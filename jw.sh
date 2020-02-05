7d4bcc195024ff7b7c0e8fa14243d1a2640da040
root <root@localhost.localdomain>
2020-02-05 13:07:48 +0900

    이미지 작업 간편화를 위한 jw.sh 추가

---
+++ jw.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z "${TAG}" ]; then
+    echo "Please provide TAG env var and try again."
+    exit 1
+fi
+
+./build.sh
+
+docker build -t 192.168.6.110:5000/hypercloud-ori:${TAG}
+
+echo "192.168.6.110:5000/hypercloud-ori:${TAG} Created"
+
