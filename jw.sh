618dfb879cb043de0840cdfddd34ba4ae042e8e2
root <root@localhost.localdomain>
2020-02-10 17:47:33 +0900

    image create 후, CK docker registry에 자동으로 푸시하는 스크립트 추가

--- jw.sh
+++ jw.sh
@@ -6,13 +6,13 @@ if [ -z "${TAG}" ]; then
 fi
 
 git pull
-
 git tag ${TAG}
-
 git push origin --tags
 
 ./build.sh
 
 docker build -t 192.168.6.110:5000/hypercloud-ori:${TAG} .
 docker tag 192.168.6.110:5000/hypercloud-ori:${TAG} 192.168.6.110:5000/hypercloud-ori:latest
+docker push 192.168.6.110:5000/hypercloud-ori:${TAG}
+
 echo "192.168.6.110:5000/hypercloud-ori:${TAG} Created"
