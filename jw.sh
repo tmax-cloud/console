90890f1cca04cef536d2e2f22350ba9e11722f79
root <root@localhost.localdomain>
2020-02-11 21:34:01 +0900

    script update

--- jw.sh
+++ jw.sh
@@ -5,6 +5,14 @@ if [ -z "${TAG}" ]; then
     exit 1
 fi
 
+
+#ARR=(`docker image ls | grep hypercloud-ori | grep v0.0.`)
+#VAR=`echo ${ARR[1]} | sed -n "s/.*v0.0.\([0-9A-Za-z,]*\).*/\1/p"`
+#TMP=`echo "${VAR} + 1" | bc`
+#TAG="v0.0.${TMP}"
+#echo Create build version is ${TAG}
+
+
 git pull
 git tag ${TAG}
 git push origin --tags
