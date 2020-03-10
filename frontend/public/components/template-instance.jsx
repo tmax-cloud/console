97e64c160cbef793e9dfc4ffb800839df148c955
이경준 <gyoungjun_lee@tmax.co.kr>
2020-03-10 10:50:34 +0900

    [feat] template-instance status column 추가 (List, Details 페이지)

--- frontend/public/components/template-instance.jsx
+++ frontend/public/components/template-instance.jsx
@@ -2,69 +2,192 @@ import * as _ from 'lodash-es';
 import * as React from 'react';
 
 import { ColHead, DetailsPage, List, ListHeader, ListPage } from './factory';
-import { Cog, navFactory, ResourceCog, SectionHeading, ResourceLink, ResourceSummary, kindObj } from './utils';
+import {
+  Cog,
+  navFactory,
+  ResourceCog,
+  SectionHeading,
+  ResourceLink,
+  ResourceSummary,
+  ScrollToTopOnMount,
+  kindObj
+} from './utils';
 import { fromNow } from './utils/datetime';
-import { referenceFor, kindForReference, referenceForModel } from '../module/k8s';
+import {
+  referenceFor,
+  kindForReference,
+  referenceForModel
+} from '../module/k8s';
 import { TemplateInstanceModel } from '../models';
 
+const menuActions = [
+  Cog.factory.ModifyLabels,
+  Cog.factory.ModifyAnnotations,
+  Cog.factory.Edit,
+  Cog.factory.Delete
+];
 
-const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete];
+const TemplateInstanceHeader = props => (
+  <ListHeader>
+    <ColHead
+      {...props}
+      className="col-lg-2 col-md-3 col-sm-4 col-xs-6"
+      sortField="metadata.name"
+    >
+      Name
+    </ColHead>
+    <ColHead
+      {...props}
+      className="col-lg-2 col-md-3 col-sm-4 col-xs-6"
+      sortField="metadata.namespace"
+    >
+      Namespace
+    </ColHead>
+    <ColHead {...props} className="col-lg-3 col-md-4 col-sm-4 hidden-xs">
+      Parameter Count
+    </ColHead>
+    <ColHead
+      {...props}
+      className="col-lg-2 col-md-2 hidden-sm hidden-xs"
+      sortField="templateInstancePhase"
+    >
+      Status
+    </ColHead>
+    <ColHead
+      {...props}
+      className="col-lg-3 hidden-md hidden-sm hidden-xs"
+      sortField="metadata.creationTimestamp"
+    >
+      Created
+    </ColHead>
+  </ListHeader>
+);
 
-const TemplateInstanceHeader = props => <ListHeader>
-    <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.name">Name</ColHead>
-    <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.namespace">Namespace</ColHead>
-    <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.namespace">Parameter Count</ColHead>
-    <ColHead {...props} className="col-sm-3 hidden-xs" sortField="metadata.creationTimestamp">Created</ColHead>
-</ListHeader>;
-
-const TemplateInstanceRow = kind => function TemplateInstanceRow({ obj }) {
-    return <div className="row co-resource-list__item">
-        <div className="col-xs-3 col-sm-3 co-resource-link-wrapper">
-            <ResourceCog actions={menuActions} kind={referenceForModel(TemplateInstanceModel)} resource={obj} />
-            <ResourceLink kind={referenceForModel(TemplateInstanceModel)} name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
-        </div>
-        <div className="col-xs-3 col-sm-3 co-break-word">
-            {obj.metadata.namespace
-                ? <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} />
-                : 'None'
-            }
-        </div>
-        <div className="col-xs-3 col-sm-3 co-break-word">
-            {
-                (obj.spec.template && obj.spec.template.parameters.length) || 'None'
-            }
-        </div>
-        <div className="col-xs-3 col-sm-3 hidden-xs">
-            {fromNow(obj.metadata.creationTimestamp)}
-        </div>
-    </div>;
+// template-instance status 값
+const templateInstancePhase = pod => {
+  let phase = '';
+  pod.status.conditions.forEach(cur => {
+    if (cur.type === 'Phase') {
+      phase = cur.status;
+    }
+  });
+  return phase;
 };
 
-const DetailsForKind = kind => function DetailsForKind_({ obj }) {
-    return <React.Fragment>
-        <div className="co-m-pane__body">
-            <SectionHeading text={`${kindForReference(kind)} Overview`} />
-            <ResourceSummary resource={obj} podSelector="spec.podSelector" showNodeSelector={false} />
+const TemplateInstanceRow = kind =>
+  function TemplateInstanceRow({ obj }) {
+    let phase = templateInstancePhase(obj);
+
+    return (
+      <div className="row co-resource-list__item">
+        <div className="col-lg-2 col-md-3 col-sm-4 col-xs-6 co-resource-link-wrapper">
+          <ResourceCog
+            actions={menuActions}
+            kind={referenceForModel(TemplateInstanceModel)}
+            resource={obj}
+          />
+          <ResourceLink
+            kind={referenceForModel(TemplateInstanceModel)}
+            name={obj.metadata.name}
+            namespace={obj.metadata.namespace}
+            title={obj.metadata.name}
+          />
         </div>
-    </React.Fragment>;
-};
+        <div className="col-lg-2 col-md-3 col-sm-4 col-xs-6 co-break-word">
+          {obj.metadata.namespace ? (
+            <ResourceLink
+              kind="Namespace"
+              name={obj.metadata.namespace}
+              title={obj.metadata.namespace}
+            />
+          ) :
+            'None'
+          }
+        </div>
+        <div className="col-lg-3 col-md-4 col-sm-4 hidden-xs co-break-word">
+          {(obj.spec.template && obj.spec.template.parameters.length) || 'None'}
+        </div>
+        <div className="col-lg-2 col-md-2 hidden-sm hidden-xs hidden-xs">
+          {phase}
+        </div>
+        <div className="col-lg-3 hidden-md hidden-sm hidden-xs hidden-xs">
+          {fromNow(obj.metadata.creationTimestamp)}
+        </div>
+      </div>
+    );
+  };
+
+const Details = ({ obj: templateinstance }) => {
+  return (
+    <React.Fragment>
+      <ScrollToTopOnMount />
+
+      <div className="co-m-pane__body">
+        <SectionHeading text="Pod Overview" />
+        <div className="row">
+          <div className="col-sm-6">
+            <ResourceSummary resource={templateinstance} />
+            {/* </ResourceSummary> */}
+          </div>
+          <div className="col-sm-6">
+            <dl className="co-m-pane__details">
+              <dt>Status</dt>
+              <dd>{templateInstancePhase(templateinstance)}</dd>
+              {/* {activeDeadlineSeconds && (
+                <React.Fragment>
+                  <dt>Active Deadline</dt>
+                  <dd>{formatDuration(activeDeadlineSeconds * 1000)}</dd>
+                </React.Fragment>
+              )} */}
+            </dl>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+// const DetailsForKind = kind =>
+//   function DetailsForKind_({ obj }) {
+//     return (
+//       <React.Fragment>
+//         <div className="co-m-pane__body">
+//           <SectionHeading text={`${kindForReference(kind)} Overview`} />
+//           <ResourceSummary
+//             resource={obj}
+//             podSelector="spec.podSelector"
+//             showNodeSelector={false}
+//           />
+//         </div>
+//       </React.Fragment>
+//     );
+//   };
 
 export const TemplateInstanceList = props => {
-    const { kinds } = props;
-    const Row = TemplateInstanceRow(kinds[0]);
-    Row.displayName = 'TemplateInstanceRow';
-    return <List {...props} Header={TemplateInstanceHeader} Row={Row} />;
+  const { kinds } = props;
+  const Row = TemplateInstanceRow(kinds[0]);
+  Row.displayName = 'TemplateInstanceRow';
+  return <List {...props} Header={TemplateInstanceHeader} Row={Row} />;
 };
 TemplateInstanceList.displayName = TemplateInstanceList;
 
-export const TemplateInstancesPage = props =>
-    <ListPage {...props} ListComponent={TemplateInstanceList} canCreate={true} kind={referenceForModel(TemplateInstanceModel)} />;
+export const TemplateInstancesPage = props => (
+  <ListPage
+    {...props}
+    ListComponent={TemplateInstanceList}
+    canCreate={true}
+    kind={referenceForModel(TemplateInstanceModel)}
+  />
+);
 TemplateInstancesPage.displayName = 'TemplateInstancesPage';
 
-
 export const TemplateInstancesDetailsPage = props => {
-    const pages = [navFactory.details(DetailsForKind(props.kind)), navFactory.editYaml()];
-    return <DetailsPage {...props} menuActions={menuActions} pages={pages} />;
+  const pages = [
+    // navFactory.details(DetailsForKind(props.kind)),
+    navFactory.details(Details),
+    navFactory.editYaml()
+  ];
+  return <DetailsPage {...props} menuActions={menuActions} pages={pages} />;
 };
 
 TemplateInstancesDetailsPage.displayName = 'TemplateInstancesDetailsPage';
--- frontend/public/components/template.jsx
+++ frontend/public/components/template.jsx
@@ -2,70 +2,151 @@ import * as _ from 'lodash-es';
 import * as React from 'react';
 
 import { ColHead, DetailsPage, List, ListHeader, ListPage } from './factory';
-import { Cog, navFactory, ResourceCog, SectionHeading, ResourceLink, ResourceSummary, kindObj } from './utils';
+import {
+  Cog,
+  navFactory,
+  ResourceCog,
+  SectionHeading,
+  ResourceLink,
+  ResourceSummary
+} from './utils';
 import { fromNow } from './utils/datetime';
-import { referenceFor, kindForReference, referenceForModel } from '../module/k8s';
+import { kindForReference, referenceForModel } from '../module/k8s';
 import { TemplateModel } from '../models';
+import { breadcrumbsForOwnerRefs } from './utils/breadcrumbs';
 
+const menuActions = [
+  Cog.factory.ModifyLabels,
+  Cog.factory.ModifyAnnotations,
+  Cog.factory.Edit,
+  Cog.factory.Delete
+];
 
-const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete];
+const TemplateHeader = props => (
+  <ListHeader>
+    <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.name">
+      Name
+    </ColHead>
+    <ColHead
+      {...props}
+      className="col-xs-3 col-sm-3"
+      sortField="metadata.namespace"
+    >
+      Namespace
+    </ColHead>
+    <ColHead
+      {...props}
+      className="col-xs-3 col-sm-3"
+      sortField="metadata.namespace"
+    >
+      Object Count
+    </ColHead>
+    <ColHead
+      {...props}
+      className="col-sm-3 hidden-xs"
+      sortField="metadata.creationTimestamp"
+    >
+      Created
+    </ColHead>
+  </ListHeader>
+);
 
-const TemplateHeader = props => <ListHeader>
-    <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.name">Name</ColHead>
-    <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.namespace">Namespace</ColHead>
-    <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.namespace">Object Count</ColHead>
-    <ColHead {...props} className="col-sm-3 hidden-xs" sortField="metadata.creationTimestamp">Created</ColHead>
-</ListHeader>;
-
-const TemplateRow = kind => function TemplateRow({ obj }) {
-    return <div className="row co-resource-list__item">
+const TemplateRow = () =>
+  // eslint-disable-next-line no-shadow
+  function TemplateRow({ obj }) {
+    return (
+      <div className="row co-resource-list__item">
         <div className="col-xs-3 col-sm-3 co-resource-link-wrapper">
-            <ResourceCog actions={menuActions} kind={referenceForModel(TemplateModel)} resource={obj} />
-            <ResourceLink kind={referenceForModel(TemplateModel)} name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
+          <ResourceCog
+            actions={menuActions}
+            kind={referenceForModel(TemplateModel)}
+            resource={obj}
+          />
+          <ResourceLink
+            kind={referenceForModel(TemplateModel)}
+            name={obj.metadata.name}
+            namespace={obj.metadata.namespace}
+            title={obj.metadata.name}
+          />
         </div>
         <div className="col-xs-3 col-sm-3 co-break-word">
-            {
-                obj.metadata.namespace
-                    ? <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} />
-                    : 'None'
-            }
+          {obj.metadata.namespace ? (
+            <ResourceLink
+              kind="Namespace"
+              name={obj.metadata.namespace}
+              title={obj.metadata.namespace}
+            />
+          ) : (
+              'None'
+            )}
         </div>
         <div className="col-xs-3 col-sm-3 co-break-word">
-            {
-                (obj.objects && obj.objects.length) || 'None'
-            }
+          {(obj.objects && obj.objects.length) || 'None'}
         </div>
         <div className="col-xs-3 col-sm-3 hidden-xs">
-            {fromNow(obj.metadata.creationTimestamp)}
+          {fromNow(obj.metadata.creationTimestamp)}
         </div>
-    </div>;
-};
+      </div>
+    );
+  };
 
-const DetailsForKind = kind => function DetailsForKind_({ obj }) {
-    return <React.Fragment>
+const DetailsForKind = kind =>
+  function DetailsForKind_({ obj }) {
+    return (
+      <React.Fragment>
         <div className="co-m-pane__body">
-            <SectionHeading text={`${kindForReference(kind)} Overview`} />
-            <ResourceSummary resource={obj} podSelector="spec.podSelector" showNodeSelector={false} />
+          <SectionHeading text={`${kindForReference(kind)} Overview`} />
+          <ResourceSummary
+            resource={obj}
+            podSelector="spec.podSelector"
+            showNodeSelector={false}
+          />
         </div>
-    </React.Fragment>;
-};
+      </React.Fragment>
+    );
+  };
 
 export const TemplateList = props => {
-    const { kinds } = props;
-    const Row = TemplateRow(kinds[0]);
-    Row.displayName = 'TemplateRow';
-    return <List {...props} Header={TemplateHeader} Row={Row} />;
+  const { kinds } = props;
+  const Row = TemplateRow(kinds[0]);
+  Row.displayName = 'TemplateRow';
+  return <List {...props} Header={TemplateHeader} Row={Row} />;
 };
 TemplateList.displayName = TemplateList;
 
-export const TemplatesPage = props =>
-    <ListPage {...props} ListComponent={TemplateList} canCreate={true} kind={referenceForModel(TemplateModel)} />;
+export const TemplatesPage = props => (
+  <ListPage
+    {...props}
+    ListComponent={TemplateList}
+    canCreate={true}
+    kind={referenceForModel(TemplateModel)}
+  />
+);
 TemplatesPage.displayName = 'TemplatesPage';
 
+// export const TemplatesDetailsPage = props => {
+//   const pages = [
+//     navFactory.details(DetailsForKind(props.kind)),
+//     navFactory.editYaml()
+//   ];
+//   return <DetailsPage {...props} menuActions={menuActions} pages={pages} />;
+// };
 
-export const TemplatesDetailsPage = props => {
-    const pages = [navFactory.details(DetailsForKind(props.kind)), navFactory.editYaml()];
-    return <DetailsPage {...props} menuActions={menuActions} pages={pages} />;
-};
+export const TemplatesDetailsPage = props => (
+  <DetailsPage
+    {...props}
+    breadcrumbsFor={obj =>
+      breadcrumbsForOwnerRefs(obj).concat({
+        name: 'Template Details',
+        path: props.match.url
+      })
+    }
+    menuActions={menuActions}
+    pages={[
+      navFactory.details(DetailsForKind(props.kind)),
+      navFactory.editYaml()
+    ]}
+  />
+);
 
 TemplatesDetailsPage.displayName = 'TemplatesDetailsPage';
