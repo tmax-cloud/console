9196e9d6f94226769cdd7c8273f4ca7a056c1a5c
dongmin_jung <dongmin_jung@tmax.co.kr>
2020-03-17 15:38:44 +0900

    [feat] login, logout, openapi 리버스 프록시 적용

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 ./bin/bridge \
-    --listen=https://192.168.0.191:9000 \
-    --base-address=https://192.168.0.191:9000 \
+    --listen=http://192.168.8.25:9000 \
+    --base-address=http://192.168.8.25:9000 \
     --tls-cert-file=tls/tls.crt \
     --tls-key-file=tls/tls.key \
     --k8s-mode=off-cluster \
@@ -10,4 +10,6 @@
     --k8s-mode-off-cluster-skip-verify-tls=true \
     --k8s-auth=bearer-token \
     --k8s-auth-bearer-token=@@ \
-    --public-dir=./frontend/public/dist
+    --public-dir=./frontend/public/dist \
+    --login-endpoint=http://192.168.6.213:28677/login \
+    --logout-endpoint=http://192.168.6.213:28677/logout
