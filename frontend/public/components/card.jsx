beb5c355de42307c74a7bdb8b979f6b38195fb96
심소영 <soyoung_shim@tmax.co.kr>
2020-03-26 12:23:43 +0900

    [feat][patch] 서비스 인스턴스 생성  step2  구현 , step1 카드 수정
    [IMS] N/A

--- frontend/public/components/card.jsx
+++ frontend/public/components/card.jsx
@@ -7,46 +7,52 @@ import * as defaultImg from '../imgs/img_catalog_default.svg';
 class ServiceClassCard extends Component {
   constructor(props) {
     super(props);
+    this.onClickCard = this.onClickCard.bind(this);
   }
-  // TODO: active props로 빼기
-  // handleActive = e => {
-  //   if (e.target.closest('.card-pf').classList.contains('active')) {
-  //     e.target.classList.remove('active');
-  //   } else {
-  //     const cardList = document.querySelectorAll('.card-pf-view-single-select');
-  //     Array.prototype.forEach.call(cardList, (card) => {
-  //       card.classList.remove('active');
-  //     });
-  //     e.target.closest('.card-pf').classList.add('active');
-  //   }
-  // };
-
+  onClickCard(uid) {
+    console.log('onClickCard');
+    this.props.onChangeClass(uid);
+  }
   render() {
-    const { onClickCard, selectedClassId } = this.props;
+    const { selectedClassId } = this.props;
     const { uid, name, imageUrl, description, providerDisplayName, isRecommended, isNew } = this.props.serviceClass;
     const isActive = selectedClassId === uid;
     return (
-      <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3" onClick={() => onClickCard(uid)} style={{ paddingLeft: '10px', paddingRight: '10px' }}>
+      <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3" onClick={() => this.onClickCard(uid)} style={{ paddingLeft: '10px', paddingRight: '10px' }}>
         <div className={`card-pf card-pf-view card-pf-view-select card-pf-view-single-select ${isActive && 'active'}`} style={{ height: '184px', margin: '0 0 20px', padding: '0 20px 20px 20px', border: `${!isActive ? '1px solid #C5C5C8' : '1px solid #39a5dc'}`, borderRadius: '2px' }}>
           <div className="card-bookmark" style={{ marginLeft: '-10px' }}>
             <img
               src={recommendImg}
               style={{
-                display: isRecommended ? 'inline-block' : 'none',
+                // display: isRecommended ? 'inline-block' : 'none',
+                display: 'inline-block',
                 marginRight: '10px',
               }}
             />
             <img
               src={newImg}
               style={{
-                visibility: isNew ? 'visible' : 'hidden',
+                visibility: 'visible',
+                // visibility: isNew ? 'visible' : 'hidden',
               }}
             />
+            <button className="pull-right" onClick={() => alert('상세정보 팝업 구현 예정')}>
+              <i className="fa fa-info" />
+            </button>
           </div>
           <div className="card-pf-body" style={{ margin: '0', padding: '0' }}>
             <div className="card-pf-items text-center" style={{ display: 'flex', marginTop: '10px' }}>
               <div className="card-logo" style={{ width: '70px', height: '70px', lineHeight: '70px' }}>
-                <img style={{ verticalAlign: 'middle' }} width="100%" height="auto" src={imageUrl || defaultImg} />
+                <img
+                  style={{ verticalAlign: 'middle' }}
+                  width="100%"
+                  height="auto"
+                  src={imageUrl || defaultImg}
+                  onError={e => {
+                    e.target.onerror = null;
+                    e.target.src = defaultImg;
+                  }}
+                />
               </div>
               <div className="card-pf-item" style={{ paddingLeft: '20px' }}>
                 <h2 className="card-pf-title text-center" style={{ fontSize: '13px', color: '#222222', fontWeight: 'bold' }}>
@@ -113,52 +119,17 @@ export const CardList0 = ({ data }) => {
 export class CardList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      classList: [],
-      cachedClassList: null,
-      selectedClassId: null,
-    };
-    //  { isNew: true, isRecommended: true }, { isNew: false, isRecommended: false }, { isNew: true, isRecommended: true }, { isNew: true, isRecommended: false }, {}, {}, {}, {}, {}, { isRecommended: true }
   }
-  static getDerivedStateFromProps(nextProps, prevState) {
-    // console.log('getDerivedStateFromProps', nextProps.classList, prevState.classList, prevState.cachedClassList);
-    // console.log('nextProps', nextProps.classList === prevState.cachedClassList);
-    // console.log('prevState', nextProps.classList === prevState.cachedClassList);
-
-    // if (prevState.classList !== prevState.cachedClassList) {
-    //   return {
-    //     cachedClassList: prevState.classList,
-    //     classList: prevState.classList,
-    //   };
-    // } else if (nextProps.classList !== prevState.cachedClassList) {
-    //   return {
-    //     cachedClassList: nextProps.classList,
-    //     classList: nextProps.classList,
-    //   };
-    // }
-    if (nextProps.classList !== prevState.classList) {
-      return {
-        classList: nextProps.classList,
-      };
-    }
-    return null;
-  }
-  onClickCard = uid => {
-    this.setState({
-      selectedClassId: uid,
-    });
-    // 서비스 플랜 리스트 get
-  };
-  // active state 관리
   render() {
-    const { classList, selectedClassId } = this.state;
+    // const { classList, selectedClassId } = this.state;
+    const { classList, selectedClassId, onChangeClass } = this.props;
     console.log('classList', classList);
     return (
       <div className="cards-pf" style={{ background: 'inherit' }}>
         <div className="container-fluid container-cards-pf" style={{ padding: '0px' }}>
           <div className="row row-cards-pf" style={{ padding: 0 }}>
             {classList.map(item => (
-              <ServiceClassCard serviceClass={item} key={item.uid} selectedClassId={selectedClassId} onClickCard={this.onClickCard} />
+              <ServiceClassCard serviceClass={item} key={item.uid} selectedClassId={selectedClassId} onChangeClass={onChangeClass} />
             ))}
           </div>
         </div>
