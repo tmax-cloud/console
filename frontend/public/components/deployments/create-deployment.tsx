ef394f2d1a2e2022addc6945798bb90e20d22ae5
신은경 <eunkyeng_shin@tmax.co.kr>
2020-03-26 12:07:37 +0900

    [feat] deployment 필수입력창 추가

--- frontend/public/components/deployments/create-deployment.tsx
+++ frontend/public/components/deployments/create-deployment.tsx
@@ -6,15 +6,13 @@ import { Link } from 'react-router-dom';
 import { k8sCreate, k8sUpdate, K8sResourceKind } from '../../module/k8s';
 import { ButtonBar, Firehose, history, kindObj, StatusBox, SelectorInput } from '../utils';
 import { formatNamespacedRouteForResource } from '../../ui/ui-actions';
-import * as k8sModels from '../../models';
-import { coFetch } from '../../co-fetch';
 
 enum CreateType {
     generic = 'generic',
     form = 'form',
 }
 const pageExplanation = {
-    [CreateType.form]: '폼 형식을 통한 템플릿 인스턴스 생성',
+    [CreateType.form]: '폼 형식을 통한 디플로이먼트 생성',
 };
 
 const determineCreateType = (data) => {
@@ -33,210 +31,173 @@ const Section = ({ label, children, id }) => <div className="row">
 // Requestform returns SubForm which is a Higher Order Component for all the types of secret forms.
 const Requestform = (SubForm) => class SecretFormComponent extends React.Component<BaseEditSecretProps_, BaseEditSecretState_> {
     constructor(props) {
-        console.log(props)
         super(props);
         const existingTemplateInstance = _.pick(props.obj, ['metadata', 'type']);
-        const templateInstance = _.defaultsDeep({}, props.fixed, existingTemplateInstance, {
-            apiVersion: 'tmax.io/v1',
-            kind: "TemplateInstance",
+        const deployment = _.defaultsDeep({}, props.fixed, existingTemplateInstance, {
+            apiVersion: 'apps/v1',
+            kind: "Deployment",
             metadata: {
                 name: '',
+                labels: { app: 'test' }
             },
             spec: {
+                replicas: 1,
+                selector: {
+                    matchLabels: { app: 'test' }
+                },
                 template: {
                     metadata: {
-                        name: ''
+                        labels: { app: 'test' }
                     },
-                    parameters: []
+                    spec: {
+                        containers: [{
+                            name: 'hello-hypercloud',
+                            image: 'hypercloud/hello-hypercloud'
+                        }],
+                        restartPolicy: 'Always'
+                    }
                 }
             }
         });
 
         this.state = {
             secretTypeAbstraction: this.props.secretTypeAbstraction,
-            templateInstance: templateInstance,
+            deployment: deployment,
             inProgress: false,
             templateList: [],
             paramList: [],
             editParamList: true,
             selectedTemplate: ''
         };
+
         this.onNameChanged = this.onNameChanged.bind(this);
-        this.onTemplateChanged = this.onTemplateChanged.bind(this);
-        this.getParams = this.getParams.bind(this);
-        this.onParamValueChanged = this.onParamValueChanged.bind(this);
+        this.onImageChanged = this.onImageChanged.bind(this);
+        this.onImagePullPolicyChanged = this.onImagePullPolicyChanged.bind(this);
+        this.onReplicasChanged = this.onReplicasChanged.bind(this);
+        this.onLabelChanged = this.onLabelChanged.bind(this);
         this.save = this.save.bind(this);
     }
     onNameChanged(event) {
-        let templateInstance = { ...this.state.templateInstance };
-        templateInstance.metadata.name = event.target.value;
-        this.setState({ templateInstance });
+        let deployment = { ...this.state.deployment };
+        deployment.metadata.name = event.target.value;
+        deployment.metadata.labels.app = event.target.value;
+        this.setState({ deployment });
     }
-    getParams() {
-        const namespace = document.location.href.split('ns/')[1].split('/')[0];
-        let templateInstance = { ...this.state.templateInstance };
-        let template = this.state.selectedTemplate;
-        coFetch('/api/kubernetes/apis/' + k8sModels.TemplateModel.apiGroup + '/' + k8sModels.TemplateModel.apiVersion + '/namespaces/' + namespace + '/templates/' + template)
-            .then(res => res.json())
-            .then((myJson) => {
-                let stringobj = JSON.stringify(myJson.objects);
-                let param = [];
-                for (let i = 0; i < stringobj.length; i++) {
-                    let word = ''
-                    if (stringobj[i] === '$') {
-                        let n = i + 2;
-                        if (stringobj[n - 1] == '{') {
-                            while (stringobj[n] !== '}') {
-                                word = word + stringobj[n];
-                                n++
-                            } param.push(word)
-                        }
-                    }
+    onImageChanged(event) {
+        let deployment = { ...this.state.deployment };
+        deployment.spec.template.spec.containers[0].image = event.target.value;
+        this.setState({ deployment });
+    }
+    onImagePullPolicyChanged(event) {
+        let deployment = { ...this.state.deployment };
+        deployment.spec.template.spec.containers[0].imagePullPolicy = event.target.value;
+        this.setState({ deployment });
+    }
+    onReplicasChanged(event) {
+        let deployment = { ...this.state.deployment };
+        deployment.spec.replicas = Number(event.target.value);
+        this.setState({ deployment });
+    }
+    onLabelChanged(event) {
+        let deployment = { ...this.state.deployment };
+        deployment.metadata.labels = { app: '' };
+        if (event.length !== 0) {
+            event.forEach(item => {
+                if (item.split('=')[1] === undefined) {
+                    document.getElementById('labelErrMsg').style.display = 'block';
+                    event.pop(item);
+                    return;
                 }
-                let paramList = Array.from(new Set(param));
-                if (paramList.length) {
-                    //paramList가 ['key1','key2']인경우 [{name: key1, value : 'value'},{name: key2, value : 'value'}]로 만들어야 함 
-                    let parameters = [];
-                    paramList.forEach((key) => {
-                        let newObj = { name: key, value: '' };
-                        parameters.push(newObj)
-                    })
-                    templateInstance.spec.template.parameters = parameters;
-                    this.setState({
-                        paramList: paramList,
-                        editParamList: false,
-                        templateInstance: templateInstance
-                    });
-                } else {
-                    this.setState({
-                        paramList: [],
-                        editParamList: false
-                    });
-                }
-            },
-                //컴포넌트의 실제 버그에서 발생하는 예외사항들을 넘기지 않도록 에러를 이 부분에서 처리
-                (error) => {
-                    this.setState({
-                        error
-                    });
-                }
-            )
-            .catch(function (myJson) {
-                console.log(myJson);
-            });
+                document.getElementById('labelErrMsg').style.display = 'none';
+                deployment.spec.selector.matchLabels.push(item.split('=')[1]);
+            })
+        }
+        this.setState({ deployment });
     }
-    onTemplateChanged(event) {
-        let templateInstance = { ...this.state.templateInstance };
-        templateInstance.spec.template.metadata.name = event.target.value;
-        this.setState({
-            selectedTemplate: event.target.value,
-            editParamList: true,
-            templateInstance: templateInstance
-        });
-    }
-    onParamValueChanged(event) {
-        console.log('onParamValueChanged불림')
-        let key = event.target.id
-        let templateInstance = { ...this.state.templateInstance };
-        templateInstance.spec.template.parameters.forEach(obj => {
-            if (obj.name === key) {
-                obj.value = event.target.value;
-            }
-        });
-        this.setState({
-            templateInstance: templateInstance
-        });
-    }
+
     save(e) {
         e.preventDefault();
-        const { kind, metadata } = this.state.templateInstance;
+        const { kind, metadata } = this.state.deployment;
         this.setState({ inProgress: true });
 
-        const newSecret = _.assign({}, this.state.templateInstance);
+        const newSecret = _.assign({}, this.state.deployment);
         const ko = kindObj(kind);
         (this.props.isCreate
             ? k8sCreate(ko, newSecret)
             : k8sUpdate(ko, newSecret, metadata.namespace, newSecret.metadata.name)
         ).then(() => {
             this.setState({ inProgress: false });
-            history.push('/k8s/ns/' + metadata.namespace + '/templateinstances');
+            history.push('/k8s/ns/' + metadata.namespace + '/deployments');
         }, err => this.setState({ error: err.message, inProgress: false }));
     }
-    getTemplateList() {
-        const namespace = document.location.href.split('ns/')[1].split('/')[0];
-        coFetch('/api/kubernetes/apis/' + k8sModels.TemplateModel.apiGroup + '/' + k8sModels.TemplateModel.apiVersion + '/namespaces/' + namespace + '/templates')
-            .then(res => res.json())
-            .then((myJson) => {
-                let templateList = myJson.items.map(function (template) {
-                    return template.metadata.name
-                });
-                this.setState({
-                    templateList: templateList,
-                    selectedTemplate: templateList[0],
-                    editParamList: true
-                });
-            },
-                (error) => {
-                    this.setState({
-                        error
-                    });
-                }
-            )
-            .catch(function (myJson) {
-                this.state.templateList = [];
-            });
-    }
     componentDidMount() {
-        this.getTemplateList();
-        this.getParams();
     }
     render() {
-        const { templateList, paramList } = this.state;
-        let options = templateList.map(function (template) {
-            return <option value={template}>{template}</option>;
-        });
-        // onchange에  getPatrams()바인딩. 초기에도 불리도록 수정 
-        if (this.state.editParamList) {
-            this.getParams();
-        }
-        let paramDivs = paramList.map((parameter) => {
-            return <Section label={parameter} id={parameter}>
-                <input onChange={this.onParamValueChanged} className="form-control" type="text" placeholder="value" id={parameter} required />
-            </Section>
-        });
-
         return <div className="co-m-pane__body">
             < Helmet >
-                <title>템플릿 인스턴스 생성</title>
+                <title>Create Deployment</title>
             </Helmet >
             <form className="co-m-pane__body-group co-create-secret-form" onSubmit={this.save}>
-                <h1 className="co-m-pane__heading">템플릿 인스턴스 생성</h1>
+                <h1 className="co-m-pane__heading">Create Deployment</h1>
                 <p className="co-m-pane__explanation">{this.props.explanation}</p>
                 <fieldset disabled={!this.props.isCreate}>
                     <div className="form-group">
-                        <label className="control-label" htmlFor="secret-name">템플릿 인스턴스 이름</label>
+                        <label className="control-label" htmlFor="secret-name">Name</label>
                         <div>
                             <input className="form-control"
                                 type="text"
                                 onChange={this.onNameChanged}
-                                value={this.state.templateInstance.metadata.name}
+                                value={this.state.deployment.metadata.name}
+                                id="deplaoyment-name"
+                                required />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label" htmlFor="secret-name">Container Image</label>
+                        <div>
+                            <input className="form-control"
+                                type="text"
+                                onChange={this.onImageChanged}
                                 id="template-instance-name"
                                 required />
                         </div>
                     </div>
                     <div className="form-group">
-                        <label className="control-label" htmlFor="secret-type" >템플릿</label>
+                        <label className="control-label" htmlFor="secret-type" >Image pull policy</label>
                         <div>
-                            <select onChange={this.onTemplateChanged} className="form-control" id="template">
-                                {options}
+                            <select onChange={this.onImagePullPolicyChanged} className="form-control" id="template">
+                                <option value='Always'>Always</option>
+                                <option value='IfNotPresent'>IfNotPresent</option>
+                                <option value='Never'>Never</option>
                             </select>
                         </div>
                     </div>
+                    <div className="form-group">
+                        <label className="control-label" htmlFor="secret-name">Replicas</label>
+                        <div>
+                            <input className="form-control"
+                                type="text"
+                                onChange={this.onReplicasChanged}
+                                id="template-instance-name"
+                                required />
+                        </div>
+                    </div>
+
                 </fieldset>
-                <label className="control-label" htmlFor="secret-name">Parameters </label>
-                <div>
-                    {paramDivs}
+                <div className="form-group">
+                    <React.Fragment>
+                        <div className="form-group">
+                            <label className="control-label" htmlFor="username">Labels</label>
+                            <div>
+                                <SelectorInput labelClassName="co-text-namespace" tags={[]} onChange={this.onLabelChanged} />
+                            </div>
+                        </div>
+                    </React.Fragment>
+                    <div id="labelErrMsg" style={{ display: 'none', color: 'red' }}>
+                        <p>Lables must be 'key=value' form.</p>
+                    </div>
                 </div>
+
                 <ButtonBar errorMessage={this.state.error} inProgress={this.state.inProgress} >
                     <button type="submit" className="btn btn-primary" id="save-changes">{this.props.saveButtonText || 'Create'}</button>
                     <Link to={formatNamespacedRouteForResource('templateinstances')} className="btn btn-default" id="cancel">Cancel</Link>
@@ -245,8 +206,6 @@ const Requestform = (SubForm) => class SecretFormComponent extends React.Compone
         </div >;
     }
 };
-
-
 class SourceSecretForm extends React.Component<SourceSecretFormProps> {
     constructor(props) {
         super(props);
@@ -301,7 +260,7 @@ export const EditSecret = ({ match: { params }, kind }) => <Firehose resources={
 
 export type BaseEditSecretState_ = {
     secretTypeAbstraction?: CreateType,
-    templateInstance: K8sResourceKind,
+    deployment: K8sResourceKind,
     inProgress: boolean,
     error?: any,
     templateList: Array<any>,
