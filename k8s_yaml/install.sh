7c9dfc815aca57abb8a5bdba79680662d0a0ddea
dongmin_jung <dongmin_jung@tmax.co.kr>
2020-04-01 15:36:53 +0900

    [feat] install script 수정

--- k8s_yaml/install.sh
+++ k8s_yaml/install.sh
@@ -3,15 +3,15 @@
 NAME_HC4="hypercloud4-operator-service"
 NAME_PROM="prometheus-k8s"
 file_initialzation="./1.initialization.yaml"
-file_svc_np="./2.svc-np.yaml"
 file_svc_lb="./2.svc-lb.yaml"
+file_svc_np="./2.svc-np.yaml"
 file_deployment_pod="./3.deployment-pod.yaml"
 file_deployment_pod_temp="./3.deployment-pod-temp.yaml"
 
 echo "==============================================================="
 echo "STEP 1. ENV Setting"
 echo "==============================================================="
-# Enter docker tag (console version) 
+# Enter docker image ver (console version) 
 echo -e "Enter the console version (default latest):"
 read version
 CONSOLE_VERSION=$version
@@ -20,7 +20,7 @@ if [ ${CONSOLE_VERSION}=="" ]; then
 else
     echo "CONSOLE_VERSION=${CONSOLE_VERSION} " 
 fi 
-# hypercloud ip addr 
+# get hypercloud ip addr 
 HC4_IP=$(kubectl get svc -A | grep ${NAME_HC4} | awk '{print $5}')
 HC4_PORT=$(kubectl get svc -A | grep ${NAME_HC4} | awk '{print $6}' | awk 'match($0, ":"){print substr($0,1,RSTART-1)}')
 
@@ -28,14 +28,14 @@ if [ -z $HC4_IP ]; then
     echo "Cannot find HC4_IP in ${NAME_HC4}. Is hypercloud4-system installed?"
     exit 1 
 fi 
-# prometheus ip addr 
+# get prometheus ip addr 
 PROM_IP=$(kubectl get svc -A | grep ${NAME_PROM} | awk '{print $5}')
 PROM_PORT=$(kubectl get svc -A | grep ${NAME_PROM} | awk '{print $6}' | awk 'match($0, ":"){print substr($0,1,RSTART-1)}')
 if [ -z $PROM_IP ]; then 
     echo "Cannot find PROMETHEUS_IP in ${NAME_PROM}. Is prometheus installed?"
     exit 1 
 fi 
-# put HC4, PROM, TAG into 3.deployment-pod-temp.yaml
+# inject HC4, PROM, VER into 3.deployment-pod-temp.yaml
 cp $file_deployment_pod $file_deployment_pod_temp
 HC4=$HC4_IP":"$HC4_PORT
 PROM=$PROM_IP":"$PROM_PORT
@@ -44,7 +44,7 @@ echo "Prometheus Addr = ${PROM}"
 echo "" 
 sed -i "s/@@HC4@@/${HC4}/g" ${file_deployment_pod_temp}
 sed -i "s/@@PROM@@/${PROM}/g" ${file_deployment_pod_temp}
-sed -i "s/@@TAG@@/${CONSOLE_VERSION}/g" ${file_deployment_pod_temp}
+sed -i "s/@@VER@@/${CONSOLE_VERSION}/g" ${file_deployment_pod_temp}
 
 
 echo "==============================================================="
@@ -67,19 +67,18 @@ else
 fi 
 echo ""
 # Create Service 
-if [ -z $(kubectl get svc -n console-system | grep console-np | awk '{print $1}') ]; then 
-    kubectl create -f ${file_svc_np}
-else
-    echo "NodePort service exists" 
-    kubectl get svc -n console-system 
-fi
-echo ""
 if [ -z $(kubectl get svc -n console-system | grep console-lb | awk '{print $1}') ]; then 
     kubectl create -f ${file_svc_lb}
 else
     echo "LoadBalancer service exists" 
-    kubectl get svc -n console-system 
 fi
+echo ""
+if [ -z $(kubectl get svc -n console-system | grep console-np | awk '{print $1}') ]; then 
+    kubectl create -f ${file_svc_np}
+else
+    echo "NodePort service exists" 
+fi
+kubectl get svc -n console-system 
 echo ""
 # Create Deployment
 if [ -z $(kubectl get deployment -n console-system | grep console | awk '{print $1}') ]; then
@@ -96,22 +95,21 @@ echo "==============================================================="
 count=0
 stop=60 
 while :
-do 
+do
+    sleep 1
     count=$(($count+1))
-    echo "Waiting for $count seconds..."
+    echo "Waiting for $count sec(s)..."
     RUNNING_FLAG=$(kubectl get po -n console-system | grep console | awk '{print $3}')
     if [ ${RUNNING_FLAG} == "Running" ]; then
-        echo "Console has been successfully deployed to console-system."
-        # rm -rf ${file_deployment_pod_temp}
+        rm -rf ${file_deployment_pod_temp}
+        kubectl get po -n console-system
+        echo "Console has been successfully deployed."
         break 
     fi
     if [ $count -eq $stop ]; then 
-        echo "Pod has a problem"
-        echo "Please check the logs" 
+        kubectl get po -n console-system 
+        echo "It seems something went wrong! Please check the log."
         kubectl logs -n console-system $(kubectl get po -n console-system | grep console | awk '{print $1}') 
-        break  
+        break
     fi
-    kubectl get po -n console-system 
-    sleep 1
-done 
-echo "Console installation completed."
+done
