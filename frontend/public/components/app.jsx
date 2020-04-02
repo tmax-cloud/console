1636de8fbe73dd7327eb2b23184b0290d0044bd7
신은경 <eunkyeng_shin@tmax.co.kr>
2020-04-02 09:50:59 +0900

    [feat]user role localstorage에 추가

--- frontend/public/components/app.jsx
+++ frontend/public/components/app.jsx
@@ -138,6 +138,7 @@ class App extends React.PureComponent {
     if (window.SERVER_FLAGS.releaseModeFlag && window.localStorage.getItem('refreshToken') && window.localStorage.getItem('accessToken')) {
       const userRole = JSON.parse(atob(window.localStorage.getItem('accessToken').split('.')[1])).role;
       console.log('userRole: ', userRole);
+      window.localStorage.setItem('role', userRole)
       if (userRole !== 'cluster-admin') {
         this.changeRole_();
       }
