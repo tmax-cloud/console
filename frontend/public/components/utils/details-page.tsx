4af778e631e8c5ef14130a4243e29266c8d83b93
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-08 21:26:57 +0900

    [feat][patch] tempate페이지 detail 페이지 빠진것 추가

--- frontend/public/components/utils/details-page.tsx
+++ frontend/public/components/utils/details-page.tsx
@@ -7,45 +7,71 @@ import { useTranslation } from 'react-i18next';
 
 export const pluralize = (i: number, singular: string, plural: string = `${singular}s`) => `${i || 0} ${i === 1 ? singular : plural}`;
 
-export const detailsPage = <T extends {}>(Component: React.ComponentType<T>) => function DetailsPage(props: T) {
-  return <Component {...props} />;
-};
+export const detailsPage = <T extends {}>(Component: React.ComponentType<T>) =>
+  function DetailsPage(props: T) {
+    return <Component {...props} />;
+  };
 
 export const ResourceSummary: React.SFC<ResourceSummaryProps> = ({ children, resource, showPodSelector = true, showNodeSelector = true, showAnnotations = true, podSelector = 'spec.selector' }) => {
   const { metadata, type } = resource;
   const { t } = useTranslation();
-  const owners = (_.get(metadata, 'ownerReferences') || [])
-    .map((o, i) => <ResourceLink key={i} kind={referenceForOwnerRef(o)} name={o.name} namespace={metadata.namespace} title={o.uid} />);
+  const owners = (_.get(metadata, 'ownerReferences') || []).map((o, i) => <ResourceLink key={i} kind={referenceForOwnerRef(o)} name={o.name} namespace={metadata.namespace} title={o.uid} />);
 
-  return <dl className="co-m-pane__details">
-    <dt>{t('CONTENT:NAME')}</dt>
-    <dd>{metadata.name || '-'}</dd>
-    {metadata.namespace ? <dt>{t('CONTENT:NAMESPACE')}</dt> : null}
-    {metadata.namespace ? <dd><ResourceLink kind="Namespace" name={metadata.namespace} title={metadata.uid} namespace={null} /></dd> : null}
-    {type ? <dt>{t('CONTENT:TYPE')}</dt> : null}
-    {type ? <dd>{type}</dd> : null}
-    <dt>{t('CONTENT:LABELS')}</dt>
-    <dd><LabelList kind={referenceFor(resource)} labels={metadata.labels} /></dd>
-    {showPodSelector && <dt>{t('CONTENT:PODSELECTOR')}</dt>}
-    {showPodSelector && <dd><Selector selector={_.get(resource, podSelector)} namespace={_.get(resource, 'metadata.namespace')} /></dd>}
-    {showNodeSelector && <dt>Node Selector</dt>}
-    {showNodeSelector && <dd><Selector kind="Node" selector={_.get(resource, 'spec.template.spec.nodeSelector')} /></dd>}
-    {showAnnotations && <dt>{t('CONTENT:ANNOTATIONS')}</dt>}
-    {showAnnotations && <dd><a className="co-m-modal-link" onClick={Cog.factory.ModifyAnnotations(kindObj(resource.kind), resource).callback}>{pluralize(_.size(metadata.annotations), 'Annotation')}</a></dd>}
-    {children}
-    <dt>Created At</dt>
-    <dd><Timestamp timestamp={metadata.creationTimestamp} /></dd>
-    {owners.length ? <dt>{pluralize(owners.length, 'Owner')}</dt> : null}
-    {owners.length ? <dd>{owners}</dd> : null}
-  </dl>;
+  return (
+    <dl className="co-m-pane__details">
+      <dt>{t('CONTENT:NAME')}</dt>
+      <dd>{metadata.name || '-'}</dd>
+      {metadata.namespace ? <dt>{t('CONTENT:NAMESPACE')}</dt> : null}
+      {metadata.namespace ? (
+        <dd>
+          <ResourceLink kind="Namespace" name={metadata.namespace} title={metadata.uid} namespace={null} />
+        </dd>
+      ) : null}
+      {type ? <dt>{t('CONTENT:TYPE')}</dt> : null}
+      {type ? <dd>{type}</dd> : null}
+      <dt>{t('CONTENT:LABELS')}</dt>
+      <dd>
+        <LabelList kind={referenceFor(resource)} labels={metadata.labels} />
+      </dd>
+      {showPodSelector && <dt>{t('CONTENT:PODSELECTOR')}</dt>}
+      {showPodSelector && (
+        <dd>
+          <Selector selector={_.get(resource, podSelector)} namespace={_.get(resource, 'metadata.namespace')} />
+        </dd>
+      )}
+      {showNodeSelector && <dt>Node Selector</dt>}
+      {showNodeSelector && (
+        <dd>
+          <Selector kind="Node" selector={_.get(resource, 'spec.template.spec.nodeSelector')} />
+        </dd>
+      )}
+      {showAnnotations && <dt>{t('CONTENT:ANNOTATIONS')}</dt>}
+      {showAnnotations && (
+        <dd>
+          <a className="co-m-modal-link" onClick={Cog.factory.ModifyAnnotations(kindObj(resource.kind), resource).callback}>
+            {pluralize(_.size(metadata.annotations), 'Annotation')}
+          </a>
+        </dd>
+      )}
+      {children}
+      <dt>{t('CONTENT:CREATEDAT')}</dt>
+      <dd>
+        <Timestamp timestamp={metadata.creationTimestamp} />
+      </dd>
+      {owners.length ? <dt>{pluralize(owners.length, 'Owner')}</dt> : null}
+      {owners.length ? <dd>{owners}</dd> : null}
+    </dl>
+  );
 };
 
-export const ResourcePodCount: React.SFC<ResourcePodCountProps> = ({ resource }) => <dl>
-  <dt>Current Count</dt>
-  <dd>{resource.status.replicas || 0}</dd>
-  <dt>Desired Count</dt>
-  <dd>{resource.spec.replicas || 0}</dd>
-</dl>;
+export const ResourcePodCount: React.SFC<ResourcePodCountProps> = ({ resource }) => (
+  <dl>
+    <dt>Current Count</dt>
+    <dd>{resource.status.replicas || 0}</dd>
+    <dt>Desired Count</dt>
+    <dd>{resource.spec.replicas || 0}</dd>
+  </dl>
+);
 
 /* eslint-disable no-undef */
 export type ResourceSummaryProps = {
