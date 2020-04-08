a2ab893e7ab157a1c4c55766c5b8b0b47f355c48
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-08 11:14:35 +0900

    [feat][patch] package.json script에 yarn install 이후에 특정 스크립트 추가

--- build-frontend.sh
+++ build-frontend.sh
@@ -2,14 +2,13 @@
 
 set -e
 
+# test -f ./frontend/node_modules/react-i18next/src/index.d.ts && mv ./frontend/node_modules/react-i18next/src/index.d.ts ./frontend/node_modules/react-i18next/src/index.ts
+
 pushd frontend
 yarn install
-test -f ./frontend/node_modules/react-i18next/src/index.d.ts && mv ./frontend/node_modules/react-i18next/src/index.d.ts ./frontend/node_modules/react-i18next/src/index.ts
-
 yarn run build
-
 # download latest swagger file
-curl -k --location --request GET 'https://192.168.6.196:6443/openapi/v2' --header 'Accept: application/json' --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiY2x1c3Rlci1hZG1pbiIsInRva2VuSWQiOiJ3b29AdG1heC5jby5rciIsImlzcyI6IlRtYXgtUHJvQXV0aC1XZWJIb29rIiwiaWQiOiJhZG1pbkB0bWF4LmNvLmtyIiwiZXhwIjoxNzQzMzAxNDM1fQ.ls9Cj1BX4NPJ3XxxHwcrGDzveaaqsauMo5L4e5BfUnE' > ../swagger/autocomplete--swagger.json
+curl -k --location --request GET 'https://192.168.6.196:6443/openapi/v2' --header 'Accept: application/json' --header 'AuthoArization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiY2x1c3Rlci1hZG1pbiIsInRva2VuSWQiOiJ3b29AdG1heC5jby5rciIsImlzcyI6IlRtYXgtUHJvQXV0aC1XZWJIb29rIiwiaWQiOiJhZG1pbkB0bWF4LmNvLmtyIiwiZXhwIjoxNzQzMzAxNDM1fQ.ls9Cj1BX4NPJ3XxxHwcrGDzveaaqsauMo5L4e5BfUnE' > ../swagger/autocomplete--swagger.json
 cp ../swagger/autocomplete--swagger.json ./public/dist/assets/
 
 popd
