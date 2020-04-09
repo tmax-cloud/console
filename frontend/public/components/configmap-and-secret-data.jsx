551064cc79787fc304f70a7c0c3a9162638a02c5
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-09 19:30:25 +0900

    [feat][patch] 시크릿 i18n 적용

--- frontend/public/components/configmap-and-secret-data.jsx
+++ frontend/public/components/configmap-and-secret-data.jsx
@@ -4,18 +4,26 @@ import * as _ from 'lodash-es';
 
 import { CopyToClipboard, EmptyBox, SectionHeading } from './utils';
 
-export const MaskedData = () => <React.Fragment>
-  <span className="sr-only">Value hidden</span>
-  <span aria-hidden="true">&bull;&bull;&bull;&bull;&bull;</span>
-</React.Fragment>;
+export const MaskedData = () => (
+  <React.Fragment>
+    <span className="sr-only">Value hidden</span>
+    <span aria-hidden="true">&bull;&bull;&bull;&bull;&bull;</span>
+  </React.Fragment>
+);
 
-export const ConfigMapData = ({data}) => {
+export const ConfigMapData = ({ data }) => {
   const dl = [];
-  Object.keys(data || {}).sort().forEach(k => {
-    const value = data[k];
-    dl.push(<dt key={`${k}-k`}>{k}</dt>);
-    dl.push(<dd key={`${k}-v`}><CopyToClipboard value={value} /></dd>);
-  });
+  Object.keys(data || {})
+    .sort()
+    .forEach(k => {
+      const value = data[k];
+      dl.push(<dt key={`${k}-k`}>{k}</dt>);
+      dl.push(
+        <dd key={`${k}-v`}>
+          <CopyToClipboard value={value} />
+        </dd>,
+      );
+    });
   return dl.length ? <dl>{dl}</dl> : <EmptyBox label="Data" />;
 };
 
@@ -42,33 +50,43 @@ export class SecretData extends React.PureComponent {
   }
 
   render() {
-    const { data } = this.props;
+    const { data, t } = this.props;
     const { showSecret } = this.state;
     const dl = [];
-    Object.keys(data || {}).sort().forEach(k => {
-      const value = this.getValue(data[k]);
-      dl.push(<dt key={`${k}-k`}>{k}</dt>);
-      dl.push(<dd key={`${k}-v`}>{value}</dd>);
-    });
-    return <React.Fragment>
-      <SectionHeading text="Data">
-        {dl.length
-          ? <button className="btn btn-link" type="button" onClick={this.toggleSecret}>
-            {showSecret
-              ? <React.Fragment><i className="fa fa-eye-slash" aria-hidden="true"></i> Hide Values</React.Fragment>
-              : <React.Fragment><i className="fa fa-eye" aria-hidden="true"></i> Reveal Values</React.Fragment>}
-          </button>
-          : null}
-      </SectionHeading>
-      {dl.length ? <dl>{dl}</dl> : <EmptyBox label="Data" />}
-    </React.Fragment>;
+    Object.keys(data || {})
+      .sort()
+      .forEach(k => {
+        const value = this.getValue(data[k]);
+        dl.push(<dt key={`${k}-k`}>{k}</dt>);
+        dl.push(<dd key={`${k}-v`}>{value}</dd>);
+      });
+    return (
+      <React.Fragment>
+        <SectionHeading text={t('CONTENT:DATA')}>
+          {dl.length ? (
+            <button className="btn btn-link" type="button" onClick={this.toggleSecret}>
+              {showSecret ? (
+                <React.Fragment>
+                  <i className="fa fa-eye-slash" aria-hidden="true"></i> {t('CONTENT:HIDEVALUES')}
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <i className="fa fa-eye" aria-hidden="true"></i> {t('CONTENT:REVEALVALUES')}
+                </React.Fragment>
+              )}
+            </button>
+          ) : null}
+        </SectionHeading>
+        {dl.length ? <dl>{dl}</dl> : <EmptyBox label="Data" />}
+      </React.Fragment>
+    );
   }
 }
 
 SecretData.propTypes = {
-  data: PropTypes.object
+  data: PropTypes.object,
 };
 
 SecretData.defaultProps = {
-  data: {}
+  data: {},
 };
