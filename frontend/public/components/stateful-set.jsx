bb9cb065972bf0edb42a55c8c0554245d4e5b5d1
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-09 10:39:32 +0900

    [feat][patch] statefulset i18n 적용

--- frontend/public/components/stateful-set.jsx
+++ frontend/public/components/stateful-set.jsx
@@ -4,40 +4,32 @@ import { DetailsPage, List, ListPage, WorkloadListHeader, WorkloadListRow } from
 import { Cog, navFactory, SectionHeading, ResourceSummary } from './utils';
 import { EnvironmentPage } from './environment';
 import { ResourceEventStream } from './events';
+import { useTranslation } from 'react-i18next';
+import { ResourcePlural } from './utils/lang/resource-plural';
 
 const menuActions = [Cog.factory.EditEnvironment, ...Cog.factory.common];
 
 const kind = 'StatefulSet';
 const Row = props => <WorkloadListRow {...props} kind={kind} actions={menuActions} />;
 
-const Details = ({obj: ss}) => <React.Fragment>
-  <div className="co-m-pane__body">
-    <SectionHeading text="StatefulSet Overview" />
-    <ResourceSummary resource={ss} showNodeSelector={false} />
-  </div>
-</React.Fragment>;
+const Details = ({ obj: ss }) => {
+  const { t } = useTranslation();
+  return (
+    <React.Fragment>
+      <div className="co-m-pane__body">
+        <SectionHeading text={t('ADDITIONAL:OVERVIEWTITLE', { something: ResourcePlural('StatefulSeet') })} />
+        <ResourceSummary resource={ss} showNodeSelector={false} />
+      </div>
+    </React.Fragment>
+  );
+};
 
-const envPath = ['spec','template','spec','containers'];
-const environmentComponent = (props) => <EnvironmentPage
-  obj={props.obj}
-  rawEnvData={props.obj.spec.template.spec.containers}
-  envPath={envPath}
-  readOnly={false}
-/>;
+const envPath = ['spec', 'template', 'spec', 'containers'];
+const environmentComponent = props => <EnvironmentPage obj={props.obj} rawEnvData={props.obj.spec.template.spec.containers} envPath={envPath} readOnly={false} />;
 
 export const StatefulSetsList = props => <List {...props} Header={WorkloadListHeader} Row={Row} />;
 export const StatefulSetsPage = props => <ListPage {...props} ListComponent={StatefulSetsList} kind={kind} canCreate={true} />;
 
-const pages = [
-  navFactory.details(Details),
-  navFactory.editYaml(),
-  navFactory.pods(),
-  navFactory.envEditor(environmentComponent),
-  navFactory.events(ResourceEventStream)
-];
+const pages = [navFactory.details(Details), navFactory.editYaml(), navFactory.pods(), navFactory.envEditor(environmentComponent), navFactory.events(ResourceEventStream)];
 
-export const StatefulSetsDetailsPage = props => <DetailsPage
-  {...props}
-  menuActions={menuActions}
-  pages={pages}
-/>;
+export const StatefulSetsDetailsPage = props => <DetailsPage {...props} menuActions={menuActions} pages={pages} />;
