a1ad8eec87f929dc0630d84abcd3ae2ce54f0399
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-10 13:40:36 +0900

    [feat][patch] 파이프라인 런 i18n 적용

--- frontend/public/components/pipeline-run.jsx
+++ frontend/public/components/pipeline-run.jsx
@@ -2,121 +2,81 @@ import * as _ from 'lodash-es';
 import * as React from 'react';
 
 import { ColHead, DetailsPage, List, ListHeader, ListPage } from './factory';
-import {
-    Cog,
-    navFactory,
-    ResourceCog,
-    SectionHeading,
-    ResourceLink,
-    ResourceSummary
-} from './utils';
+import { Cog, navFactory, ResourceCog, SectionHeading, ResourceLink, ResourceSummary } from './utils';
 import { fromNow } from './utils/datetime';
 import { kindForReference, referenceForModel } from '../module/k8s';
 import { breadcrumbsForOwnerRefs } from './utils/breadcrumbs';
 import { PipelineRunModel } from '../models';
+import { useTranslation } from 'react-i18next';
+import { ResourcePlural } from './utils/lang/resource-plural';
 
-const menuActions = [
-    Cog.factory.ModifyLabels,
-    Cog.factory.ModifyAnnotations,
-    Cog.factory.Edit,
-    Cog.factory.Delete
-];
+const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete];
 
-const PipelineRunHeader = props => (
+const PipelineRunHeader = props => {
+  const { t } = useTranslation();
+  return (
     <ListHeader>
-        <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.name">
-            Name
-    </ColHead>
-        <ColHead
-            {...props}
-            className="col-xs-4 col-sm-4"
-            sortField="metadata.namespace"
-        >
-            Namespace
-    </ColHead>
-
-        <ColHead
-            {...props}
-            className="col-sm-4 hidden-xs"
-            sortField="metadata.creationTimestamp"
-        >
-            Created
-    </ColHead>
+      <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.name">
+        {t('CONTENT:NAME')}
+      </ColHead>
+      <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.namespace">
+        {t('CONTENT:NAMESPACE')}
+      </ColHead>
+      <ColHead {...props} className="col-sm-4 hidden-xs" sortField="metadata.creationTimestamp">
+        CREATED
+      </ColHead>
     </ListHeader>
-);
+  );
+};
 
 const PipelineRunRow = () =>
-    // eslint-disable-next-line no-shadow
-    function PipelineRunRow({ obj }) {
-        return (
-            <div className="row co-resource-list__item">
-                <div className="col-xs-4 col-sm-4 co-resource-link-wrapper">
-                    <ResourceCog
-                        actions={menuActions}
-                        kind="PipelineRun"
-                        resource={obj}
-                    />
-                    <ResourceLink
-                        kind="PipelineRun"
-                        name={obj.metadata.name}
-                        namespace={obj.metadata.namespace}
-                        title={obj.metadata.name}
-                    />
-                </div>
-                <div className="col-xs-4 col-sm-4 co-break-word">
-                    {obj.metadata.namespace ? (
-                        <ResourceLink
-                            kind="Namespace"
-                            name={obj.metadata.namespace}
-                            title={obj.metadata.namespace}
-                        />
-                    ) : (
-                            'None'
-                        )}
-                </div>
-                <div className="col-xs-4 col-sm-4 hidden-xs">
-                    {fromNow(obj.metadata.creationTimestamp)}
-                </div>
-            </div>
-        );
-    };
+  // eslint-disable-next-line no-shadow
+  function PipelineRunRow({ obj }) {
+    return (
+      <div className="row co-resource-list__item">
+        <div className="col-xs-4 col-sm-4 co-resource-link-wrapper">
+          <ResourceCog actions={menuActions} kind="PipelineRun" resource={obj} />
+          <ResourceLink kind="PipelineRun" name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
+        </div>
+        <div className="col-xs-4 col-sm-4 co-break-word">{obj.metadata.namespace ? <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} /> : 'None'}</div>
+        <div className="col-xs-4 col-sm-4 hidden-xs">{fromNow(obj.metadata.creationTimestamp)}</div>
+      </div>
+    );
+  };
 
 const DetailsForKind = kind =>
-    function DetailsForKind_({ obj }) {
-        return (
-            <React.Fragment>
-                <div className="co-m-pane__body">
-                    <SectionHeading text={`Pipeline Run Overview`} />
-                    <ResourceSummary
-                        resource={obj}
-                        podSelector="spec.podSelector"
-                        showNodeSelector={false}
-                    />
-                </div>
-            </React.Fragment>
-        );
-    };
+  function DetailsForKind_({ obj }) {
+    const { t } = useTranslation();
+    return (
+      <React.Fragment>
+        <div className="co-m-pane__body">
+          <SectionHeading text={t('ADDITIONAL:OVERVIEWTITLE', { something: ResourcePlural('PIPELINERUN', t) })} />
+          <ResourceSummary resource={obj} podSelector="spec.podSelector" showNodeSelector={false} />
+        </div>
+      </React.Fragment>
+    );
+  };
 
 export const PipelineRunList = props => {
-    const { kinds } = props;
-    const Row = PipelineRunRow(kinds[0]);
-    Row.displayName = 'PipelineRun';
-    return <List {...props} Header={PipelineRunHeader} Row={Row} />;
+  const { kinds } = props;
+  const Row = PipelineRunRow(kinds[0]);
+  Row.displayName = 'PipelineRun';
+  return <List {...props} Header={PipelineRunHeader} Row={Row} />;
 };
 PipelineRunList.displayName = PipelineRunList;
 
-
 export const PipelineRunsPage = props => {
-    const createItems = {
-        form: 'Pipeline Run (Form Editor)',
-        yaml: 'Pipeline Run (YAML Editor)'
-    };
+  const { t } = useTranslation();
+  const createItems = {
+    form: t('CONTENT:FORMEDITOR'),
+    yaml: t('CONTENT:YAMLEDITOR'),
+  };
 
-    const createProps = {
-        items: createItems,
-        createLink: (type) => `/k8s/ns/${props.namespace || 'default'}/pipelineruns/new${type !== 'yaml' ? '/' + type : ''}`
-    };
-    return <ListPage ListComponent={PipelineRunList} canCreate={true} createButtonText="Create" createProps={createProps} {...props} />;
+  const createProps = {
+    items: createItems,
+    createLink: type => `/k8s/ns/${props.namespace || 'default'}/pipelineruns/new${type !== 'yaml' ? '/' + type : ''}`,
+  };
+  return <ListPage ListComponent={PipelineRunList} canCreate={true} createButtonText="Create" createProps={createProps} {...props} />;
 };
 PipelineRunsPage.displayName = 'PipelineRunsPage';
 
@@ -129,20 +89,17 @@ PipelineRunsPage.displayName = 'PipelineRunsPage';
 // };
 
 export const PipelineRunDetailsPage = props => (
-    <DetailsPage
-        {...props}
-        breadcrumbsFor={obj =>
-            breadcrumbsForOwnerRefs(obj).concat({
-                name: 'Task Details',
-                path: props.match.url
-            })
-        }
-        menuActions={menuActions}
-        pages={[
-            navFactory.details(DetailsForKind(props.kind)),
-            navFactory.editYaml()
-        ]}
-    />
+  <DetailsPage
+    {...props}
+    breadcrumbsFor={obj =>
+      breadcrumbsForOwnerRefs(obj).concat({
+        name: 'Task Details',
+        path: props.match.url,
+      })
+    }
+    menuActions={menuActions}
+    pages={[navFactory.details(DetailsForKind(props.kind)), navFactory.editYaml()]}
+  />
 );
 
 PipelineRunDetailsPage.displayName = 'PipelineRunDetailsPage';
