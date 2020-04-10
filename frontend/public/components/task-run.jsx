7e86a34f25c88224e8845b176952faba209ae160
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-10 13:41:23 +0900

    [feat][patch] task run 페이지 i18n 적용

--- frontend/public/components/task-run.jsx
+++ frontend/public/components/task-run.jsx
@@ -2,116 +2,70 @@ import * as _ from 'lodash-es';
 import * as React from 'react';
 
 import { ColHead, DetailsPage, List, ListHeader, ListPage } from './factory';
-import {
-    Cog,
-    navFactory,
-    ResourceCog,
-    SectionHeading,
-    ResourceLink,
-    ResourceSummary
-} from './utils';
+import { Cog, navFactory, ResourceCog, SectionHeading, ResourceLink, ResourceSummary } from './utils';
 import { fromNow } from './utils/datetime';
 import { kindForReference, referenceForModel } from '../module/k8s';
 import { breadcrumbsForOwnerRefs } from './utils/breadcrumbs';
 import { TaskRunModel } from '../models';
+import { useTranslation } from 'react-i18next';
+import { ResourcePlural } from './utils/lang/resource-plural';
 
-const menuActions = [
-    Cog.factory.ModifyLabels,
-    Cog.factory.ModifyAnnotations,
-    Cog.factory.Edit,
-    Cog.factory.Delete
-];
+const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete];
 
-const TaskRunHeader = props => (
+const TaskRunHeader = props => {
+  const { t } = useTranslation();
+  return (
     <ListHeader>
-        <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.name">
-            Name
-    </ColHead>
-        <ColHead
-            {...props}
-            className="col-xs-4 col-sm-4"
-            sortField="metadata.namespace"
-        >
-            Namespace
-    </ColHead>
-        <ColHead
-            {...props}
-            className="col-sm-4 hidden-xs"
-            sortField="metadata.creationTimestamp"
-        >
-            Created
-    </ColHead>
+      <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.name">
+        {t('CONTENT:NAME')}
+      </ColHead>
+      <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.namespace">
+        {t('CONTENT:NAMESPACE')}
+      </ColHead>
+      <ColHead {...props} className="col-sm-4 hidden-xs" sortField="metadata.creationTimestamp">
+        {t('CONTENT:CREATED')}
+      </ColHead>
     </ListHeader>
-);
+  );
+};
 
 const TaskRunRow = () =>
-    // eslint-disable-next-line no-shadow
-    function TaskRunRow({ obj }) {
-        return (
-            <div className="row co-resource-list__item">
-                <div className="col-xs-4 col-sm-4 co-resource-link-wrapper">
-                    <ResourceCog
-                        actions={menuActions}
-                        kind="TaskRun"
-                        resource={obj}
-                    />
-                    <ResourceLink
-                        kind="TaskRun"
-                        name={obj.metadata.name}
-                        namespace={obj.metadata.namespace}
-                        title={obj.metadata.name}
-                    />
-                </div>
-                <div className="col-xs-4 col-sm-4 co-break-word">
-                    {obj.metadata.namespace ? (
-                        <ResourceLink
-                            kind="Namespace"
-                            name={obj.metadata.namespace}
-                            title={obj.metadata.namespace}
-                        />
-                    ) : (
-                            'None'
-                        )}
-                </div>
-                <div className="col-xs-4 col-sm-4 hidden-xs">
-                    {fromNow(obj.metadata.creationTimestamp)}
-                </div>
-            </div>
-        );
-    };
+  // eslint-disable-next-line no-shadow
+  function TaskRunRow({ obj }) {
+    return (
+      <div className="row co-resource-list__item">
+        <div className="col-xs-4 col-sm-4 co-resource-link-wrapper">
+          <ResourceCog actions={menuActions} kind="TaskRun" resource={obj} />
+          <ResourceLink kind="TaskRun" name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
+        </div>
+        <div className="col-xs-4 col-sm-4 co-break-word">{obj.metadata.namespace ? <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} /> : 'None'}</div>
+        <div className="col-xs-4 col-sm-4 hidden-xs">{fromNow(obj.metadata.creationTimestamp)}</div>
+      </div>
+    );
+  };
 
 const DetailsForKind = kind =>
-    function DetailsForKind_({ obj }) {
-        return (
-            <React.Fragment>
-                <div className="co-m-pane__body">
-                    <SectionHeading text={`${kindForReference(kind)} Overview`} />
-                    <ResourceSummary
-                        resource={obj}
-                        podSelector="spec.podSelector"
-                        showNodeSelector={false}
-                    />
-                </div>
-            </React.Fragment>
-        );
-    };
+  function DetailsForKind_({ obj }) {
+    const { t } = useTranslation();
+    return (
+      <React.Fragment>
+        <div className="co-m-pane__body">
+          <SectionHeading text={t('ADDITIONAL:OVERVIEWTITLE', { something: ResourcePlural('TASKRUN', t) })} />
+          <ResourceSummary resource={obj} podSelector="spec.podSelector" showNodeSelector={false} />
+        </div>
+      </React.Fragment>
+    );
+  };
 
 export const TaskRunList = props => {
-    const { kinds } = props;
-    const Row = TaskRunRow(kinds[0]);
-    Row.displayName = 'TaskRunRow';
-    return <List {...props} Header={TaskRunHeader} Row={Row} />;
+  const { kinds } = props;
+  const Row = TaskRunRow(kinds[0]);
+  Row.displayName = 'TaskRunRow';
+  return <List {...props} Header={TaskRunHeader} Row={Row} />;
 };
 TaskRunList.displayName = TaskRunList;
 
-export const TaskRunsPage = props => (
-    <ListPage
-        {...props}
-        ListComponent={TaskRunList}
-        canCreate={true}
-        kind="TaskRun"
-    />
-);
+export const TaskRunsPage = props => <ListPage {...props} ListComponent={TaskRunList} canCreate={true} kind="TaskRun" />;
 TaskRunsPage.displayName = 'TaskRunsPage';
 
 // export const TaskDetailsPage = props => {
@@ -123,20 +77,17 @@ TaskRunsPage.displayName = 'TaskRunsPage';
 // };
 
 export const TaskRunDetailsPage = props => (
-    <DetailsPage
-        {...props}
-        breadcrumbsFor={obj =>
-            breadcrumbsForOwnerRefs(obj).concat({
-                name: 'Task Details',
-                path: props.match.url
-            })
-        }
-        menuActions={menuActions}
-        pages={[
-            navFactory.details(DetailsForKind(props.kind)),
-            navFactory.editYaml()
-        ]}
-    />
+  <DetailsPage
+    {...props}
+    breadcrumbsFor={obj =>
+      breadcrumbsForOwnerRefs(obj).concat({
+        name: 'Task Details',
+        path: props.match.url,
+      })
+    }
+    menuActions={menuActions}
+    pages={[navFactory.details(DetailsForKind(props.kind)), navFactory.editYaml()]}
+  />
 );
 
 TaskRunDetailsPage.displayName = 'TaskRunDetailsPage';
