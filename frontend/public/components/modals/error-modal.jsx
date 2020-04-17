28cc8172d41f60004e9da674bc79c44a094c0937
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-17 11:50:35 +0900

    [feat][patch] 서비스 어카운트 페이지 action 메뉴 i18n 적용

--- frontend/public/components/modals/error-modal.jsx
+++ frontend/public/components/modals/error-modal.jsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 
-import {createModalLauncher, ModalTitle, ModalBody, ModalFooter} from '../factory/modal';
+import { createModalLauncher, ModalTitle, ModalBody, ModalFooter } from '../factory/modal';
 
-export const errorModal = createModalLauncher(
-  ({error, cancel}) => {
-    return (
-      <div>
-        <ModalTitle>Error</ModalTitle>
-        <ModalBody>{error}</ModalBody>
-        <ModalFooter inProgress={false} errorMessage=""><button type="button" onClick={(e) => cancel(e)} className="btn btn-default">OK</button></ModalFooter>
-      </div>
-    );
-  }
-);
+export const errorModal = createModalLauncher(({ error, cancel, t }) => {
+  return (
+    <div>
+      <ModalTitle>{t('CONTENT:ERROR')}</ModalTitle>
+      <ModalBody>{error}</ModalBody>
+      <ModalFooter inProgress={false} errorMessage="">
+        <button type="button" onClick={e => cancel(e)} className="btn btn-default">
+          {t('CONTENT:OK')}
+        </button>
+      </ModalFooter>
+    </div>
+  );
+});
