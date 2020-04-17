d3057b653f748a2d80b9be286693aa633b378d8b
이경준 <gyoungjun_lee@tmax.co.kr>
2020-04-17 13:53:56 +0900

    [feat][patch] 세션 만료 시간 설정 팝업 i18n 적용

--- frontend/public/components/modals/extend-session-modal.jsx
+++ frontend/public/components/modals/extend-session-modal.jsx
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import * as PropTypes from 'prop-types';
 import { coFetch } from '../../co-fetch';
 import { createModalLauncher, ModalTitle, ModalBody, CustomModalSubmitFooter } from '../factory/modal';
+import { useTranslation } from 'react-i18next';
+
+const ExpiredTimeComponent = (submit, updateState, requestTime, inProgress, cancel) => {
+  const { t } = useTranslation();
+  return (
+    <form onSubmit={submit} name="form">
+      <ModalTitle>{t('CONTENT:CHANGESESSIONEXPIREDTIME')}</ModalTitle>
+      <ModalBody>
+        <div className="form-group">
+          <label className="control-label" htmlFor="extend-time">
+            {t('CONTENT:SESSIONEXPIREDTIME')}
+          </label>
+          <div>
+            <input type="text" style={{ marginRight: '5px', width: 'calc(100% - 20px)' }} placeholder={t('CONTENT:MINIMUM1,MAXIMUM60')} onChange={updateState} value={requestTime} id="input-extend-time" required />
+            {t('CONTENT:MINUTE')}
+          </div>
+          <div>
+            <p id="error-request" style={{ color: 'red', visibility: 'hidden' }}>
+              {t('CONTENT:MINIMUM1,MAXIMUM60')}
+            </p>
+          </div>
+        </div>
+      </ModalBody>
+      <CustomModalSubmitFooter inProgress={inProgress} submitText={t('CONTENT:SAVE')} cancelText={t('CONTENT:CANCEL')} cancel={cancel} />
+    </form>
+  );
+};
 
 class ExtendSessionModal extends Component {
   constructor(props) {
@@ -34,26 +61,27 @@ class ExtendSessionModal extends Component {
 
   render() {
     return (
-      <form onSubmit={this._submit} name="form">
-        <ModalTitle>Change Session Expired Time</ModalTitle>
-        <ModalBody>
-          <div className="form-group">
-            <label className="control-label" htmlFor="extend-time">
-              Session Expired Time
-            </label>
-            <div>
-              <input type="text" style={{ marginRight: '5px', width: 'calc(100% - 20px)' }} placeholder="최소 1 ~ 최대 60" onChange={this._updateState} value={this.state.requestTime} id="input-extend-time" required />
-              Minute
-            </div>
-            <div>
-              <p id="error-request" style={{ color: 'red', visibility: 'hidden' }}>
-                Minimum 1 minute, maximum 60 minutes
-              </p>
-            </div>
-          </div>
-        </ModalBody>
-        <CustomModalSubmitFooter inProgress={this.state.inProgress} submitText="Save" cancelText="Cancel" cancel={this._cancel} />
-      </form>
+      <ExpiredTimeComponent submit={this._submit} updateState={this._updateState} requestTime={this.state.requestTime} inProgress={this.state.inProgress} cancel={this._cancel} />
+      // <form onSubmit={this._submit} name="form">
+      //   <ModalTitle>Change Session Expired Time</ModalTitle>
+      //   <ModalBody>
+      //     <div className="form-group">
+      //       <label className="control-label" htmlFor="extend-time">
+      //         Session Expired Time
+      //       </label>
+      //       <div>
+      //         <input type="text" style={{ marginRight: '5px', width: 'calc(100% - 20px)' }} placeholder="최소 1 ~ 최대 60" onChange={this._updateState} value={this.state.requestTime} id="input-extend-time" required />
+      //         Minute
+      //       </div>
+      //       <div>
+      //         <p id="error-request" style={{ color: 'red', visibility: 'hidden' }}>
+      //           Minimum 1 minute, maximum 60 minutes
+      //         </p>
+      //       </div>
+      //     </div>
+      //   </ModalBody>
+      //   <CustomModalSubmitFooter inProgress={this.state.inProgress} submitText="Save" cancelText="Cancel" cancel={this._cancel} />
+      // </form>
     );
   }
 }
