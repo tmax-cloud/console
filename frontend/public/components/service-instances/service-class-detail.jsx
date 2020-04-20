563fd4407b7634d64f7c8a48db87f136d3acf982
신은경 <eunkyeng_shin@tmax.co.kr>
2020-04-20 20:13:30 +0900

    [feat] service class detail modal > reactMarkDown컴포넌트 사용

--- frontend/public/components/service-instances/service-class-detail.jsx
+++ frontend/public/components/service-instances/service-class-detail.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import * as defaultImg from '../../imgs/img_catalog_default.svg';
-
+import ReactMarkdown from 'react-markdown';
 export const ServiceClassDetail = ({ detail }) => {
   const { name, imageUrl, description, providerDisplayName, markdownDescription, urlDescription } = detail;
+
   return (
     <div>
       <div className="card-pf-items text-left" style={{ display: 'flex', marginTop: '10px' }}>
@@ -30,7 +31,8 @@ export const ServiceClassDetail = ({ detail }) => {
       </div>
       <hr />
       <div>
-        <p style={{ margin: '10px 0 0 0', fontSize: '13px', color: '#6A6A6A', textAlign: 'initial' }}>{markdownDescription}</p>
+        {/* <p style={{ margin: '10px 0 0 0', fontSize: '13px', color: '#6A6A6A', textAlign: 'initial' }}>{markdownDescription}</p> */}
+        <ReactMarkdown source={markdownDescription} />
         <a style={{ margin: '10px 0 0 0', fontSize: '13px', color: '#6A6A6A', textAlign: 'initial' }} href={urlDescription} target="_blank">{urlDescription}</a>
       </div>
     </div>
