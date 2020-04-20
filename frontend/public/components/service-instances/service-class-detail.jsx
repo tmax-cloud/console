d180af26a65659b85146178628ed0cb31017f9b2
신은경 <eunkyeng_shin@tmax.co.kr>
2020-04-20 17:10:09 +0900

    [feat][patch]서비스 인스턴스 생성 > markdownDescription,urlDescription적용

--- frontend/public/components/service-instances/service-class-detail.jsx
+++ frontend/public/components/service-instances/service-class-detail.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as defaultImg from '../../imgs/img_catalog_default.svg';
 
 export const ServiceClassDetail = ({ detail }) => {
-  const { name, imageUrl, description, providerDisplayName, longDescription } = detail;
+  const { name, imageUrl, description, providerDisplayName, markdownDescription, urlDescription } = detail;
   return (
     <div>
       <div className="card-pf-items text-left" style={{ display: 'flex', marginTop: '10px' }}>
@@ -30,7 +30,8 @@ export const ServiceClassDetail = ({ detail }) => {
       </div>
       <hr />
       <div>
-        <p style={{ margin: '10px 0 0 0', fontSize: '13px', color: '#6A6A6A', textAlign: 'initial' }}>{longDescription}</p>
+        <p style={{ margin: '10px 0 0 0', fontSize: '13px', color: '#6A6A6A', textAlign: 'initial' }}>{markdownDescription}</p>
+        <a style={{ margin: '10px 0 0 0', fontSize: '13px', color: '#6A6A6A', textAlign: 'initial' }} href={urlDescription} target="_blank">{urlDescription}</a>
       </div>
     </div>
   );
