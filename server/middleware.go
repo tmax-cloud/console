a9587b2e35e1c7619c0f049aeaf1ed0799e5e214
dongmin_jung <dongmin_jung@tmax.co.kr>
2020-04-24 14:43:39 +0900

    [feat] header에 access-control-allow-origin * 추가

--- server/middleware.go
+++ server/middleware.go
@@ -67,6 +67,8 @@ func securityHeadersMiddleware(hdlr http.Handler) http.HandlerFunc {
 		// Less information leakage about what domains we link to
 		// w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
 		w.Header().Set("Referrer-Policy", "no-referrer-when-downgrade")
+		w.Header().Set("Referrer-Policy", "no-referrer-when-downgrade")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		hdlr.ServeHTTP(w, r)
 	}
 }
