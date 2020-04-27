878bcd2ba7b5ffc74d4bb51016e15b5c9a12edcd
신은경 <eunkyeng_shin@tmax.co.kr>
2020-04-27 11:43:26 +0900

    [feat] create-ingress >  form editor생성 및 ingress-editor component추가

--- frontend/public/components/ingresses/create-ingress.tsx
+++ frontend/public/components/ingresses/create-ingress.tsx
@@ -3,332 +3,201 @@ import * as _ from 'lodash-es';
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
+import { KeyValueEditor } from '../utils/key-value-editor';
 import { k8sCreate, k8sUpdate, K8sResourceKind } from '../../module/k8s';
-import { ButtonBar, Firehose, history, kindObj, StatusBox, SelectorInput } from '../utils';
+import { ButtonBar, history, kindObj } from '../utils';
+import { useTranslation } from 'react-i18next';
+import { ResourcePlural } from '../utils/lang/resource-plural';
 import { formatNamespacedRouteForResource } from '../../ui/ui-actions';
-import * as k8sModels from '../../models';
-import { coFetch } from '../../co-fetch';
+import { IngressEditor } from '../utils/ingress-editor';
 
 enum CreateType {
     generic = 'generic',
     form = 'form',
 }
 const pageExplanation = {
-    [CreateType.form]: '폼 형식을 통한 템플릿 인스턴스 생성',
+    [CreateType.form]: 'Create resource quota claim  using Form Editor',
 };
-
-const determineCreateType = (data) => {
-    return CreateType.form;
-};
-
-const Section = ({ label, children, id }) => <div className="row">
-    <div className="col-xs-2">
-        <div>{label}</div>
-    </div>
-    <div className="col-xs-2" id={id}>
+const FirstSection = ({ label, children, id }) => <div className="form-group">
+    <label className="control-label" htmlFor="secret-type" >{label}</label>
+    <div>
         {children}
     </div>
-</div>;
+</div>
 
-// Requestform returns SubForm which is a Higher Order Component for all the types of secret forms.
-const Requestform = (SubForm) => class SecretFormComponent extends React.Component<BaseEditSecretProps_, BaseEditSecretState_> {
+class IngressFormComponent extends React.Component<IngressProps_, IngressState_>  {
     constructor(props) {
-        console.log(props)
         super(props);
-        const existingTemplateInstance = _.pick(props.obj, ['metadata', 'type']);
-        const templateInstance = _.defaultsDeep({}, props.fixed, existingTemplateInstance, {
-            apiVersion: 'tmax.io/v1',
-            data: {},
-            kind: "TemplateInstance",
+        const existingIngress = _.pick(props.obj, ['metadata', 'type']);
+        const ingress = _.defaultsDeep({}, props.fixed, existingIngress, {
+            apiVersion: 'extensions/v1beta1',
+            kind: 'Ingress',
             metadata: {
                 name: '',
+                namespace: ''
             },
             spec: {
-                template: {
-                    metadata: {
-                        name: ''
-                    },
-                    parameters: []
-                }
+                rules: [{
+                    host: '',
+                    http: {
+                        paths: [
+                            {
+                                path: '',
+                                backend: {
+                                    serviceName: '',
+                                    servicePort: ''
+                                }
+                            }
+                        ]
+                    }
+
+                }]
+
             }
         });
 
         this.state = {
-            secretTypeAbstraction: this.props.secretTypeAbstraction,
-            templateInstance: templateInstance,
+            ingressTypeAbstraction: this.props.ingressTypeAbstraction,
+            ingress: ingress,
             inProgress: false,
-            stringData: _.mapValues(_.get(props.obj, 'data'), window.atob),
-            templateList: [],
-            paramList: [],
-            editParamList: true,
-            selectedTemplate: ''
+            type: 'form',
+            paths: [['', '', '']],
+            serviceNameList: [],
+            servicePortList: [],
+            serviceNameOptions: [],
+            servicePortOptions: []
         };
-        this.onDataChanged = this.onDataChanged.bind(this);
+        this.onResourceNameChanged = this.onResourceNameChanged.bind(this);
         this.onNameChanged = this.onNameChanged.bind(this);
-        this.onTemplateChanged = this.onTemplateChanged.bind(this);
-        this.getParams = this.getParams.bind(this);
-        this.onParamValueChanged = this.onParamValueChanged.bind(this);
         this.save = this.save.bind(this);
     }
-    onDataChanged(secretsData) {
+    _updatePaths(path) {
         this.setState({
-            stringData: { ...secretsData.stringData }
+            paths: path.pathPairs,
         });
     }
+    onResourceNameChanged(event) {
+        let ingress = { ...this.state.ingress };
+        ingress['resourceName'] = String(event.target.value);
+        this.setState({ ingress });
+    }
+
     onNameChanged(event) {
-        let templateInstance = { ...this.state.templateInstance };
-        templateInstance.metadata.name = event.target.value;
-        this.setState({ templateInstance });
-    }
-    getParams() {
-        const namespace = document.location.href.split('ns/')[1].split('/')[0];
-        let template = this.state.selectedTemplate;
-        coFetch('/api/kubernetes/apis/' + k8sModels.TemplateModel.apiGroup + '/' + k8sModels.TemplateModel.apiVersion + '/namespaces/' + namespace + '/templates/' + template)
-            .then(res => res.json())
-            .then((myJson) => {
-                let stringobj = JSON.stringify(myJson.objects);
-                let param = [];
-                for (let i = 0; i < stringobj.length; i++) {
-                    let word = ''
-                    if (stringobj[i] === '$') {
-                        let n = i + 2;
-                        if (stringobj[n - 1] == '{') {
-                            while (stringobj[n] !== '}') {
-                                word = word + stringobj[n];
-                                n++
-                            } param.push(word)
-                        }
-                    }
-                }
-                let paramList = Array.from(new Set(param));
-                if (paramList.length) {
-                    this.setState({
-                        paramList: paramList,
-                        editParamList: false
-                    });
-                } else {
-                    this.setState({
-                        paramList: [],
-                        editParamList: false
-                    });
-                }
-            },
-                //컴포넌트의 실제 버그에서 발생하는 예외사항들을 넘기지 않도록 에러를 이 부분에서 처리
-                (error) => {
-                    this.setState({
-                        error
-                    });
-                }
-            )
-            .catch(function (myJson) {
-                console.log(myJson);
-            });
-    }
-    onTemplateChanged(event) {
-        let templateInstance = { ...this.state.templateInstance };
-        templateInstance.spec.template.metadata.name = event.target.value;
-        this.setState({
-            selectedTemplate: event.target.value,
-            editParamList: true,
-            templateInstance: templateInstance
-        });
-    }
-    onParamValueChanged(event) {
-        let key = event.target.id
-        let templateInstance = { ...this.state.templateInstance };
-        templateInstance.spec.template.parameters[key] = event.target.value;
-        this.setState({
-            templateInstance: templateInstance
-        });
+        let ingress = { ...this.state.ingress };
+        ingress.metadata.name = String(event.target.value);
+        this.setState({ ingress });
     }
     save(e) {
         e.preventDefault();
-        const { kind, metadata } = this.state.templateInstance;
+        const { kind, metadata } = this.state.ingress;
         this.setState({ inProgress: true });
+        const newIngress = _.assign({}, this.state.ingress);
 
-        const newSecret = _.assign({}, this.state.templateInstance, { stringData: this.state.stringData });
+
         const ko = kindObj(kind);
         (this.props.isCreate
-            ? k8sCreate(ko, newSecret)
-            : k8sUpdate(ko, newSecret, metadata.namespace, newSecret.metadata.name)
+            ? k8sCreate(ko, newIngress)
+            : k8sUpdate(ko, newIngress, metadata.namespace, newIngress.metadata.name)
         ).then(() => {
             this.setState({ inProgress: false });
-            history.push('/k8s/ns/' + metadata.namespace + '/templateinstances');
+            history.push(formatNamespacedRouteForResource('resourcequotaclaims'));
         }, err => this.setState({ error: err.message, inProgress: false }));
     }
-    getTemplateList() {
-        const namespace = document.location.href.split('ns/')[1].split('/')[0];
-        coFetch('/api/kubernetes/apis/' + k8sModels.TemplateModel.apiGroup + '/' + k8sModels.TemplateModel.apiVersion + '/namespaces/' + namespace + '/templates')
-            .then(res => res.json())
-            .then((myJson) => {
-                let templateList = myJson.items.map(function (template) {
-                    return template.metadata.name
-                });
-                this.setState({
-                    templateList: templateList,
-                    selectedTemplate: templateList[0],
-                    editParamList: true
-                });
-            },
-                (error) => {
-                    this.setState({
-                        error
-                    });
-                }
-            )
-            .catch(function (myJson) {
-                this.state.templateList = [];
-            });
-    }
-    componentDidMount() {
-        this.getTemplateList();
-        this.getParams();
-    }
-    render() {
-        const { templateList, paramList } = this.state;
-        let options = templateList.map(function (template) {
-            return <option value={template}>{template}</option>;
-        });
-        // onchange에  getPatrams()바인딩. 초기에도 불리도록 수정 
-        if (this.state.editParamList) {
-            this.getParams();
-        }
-        let paramDivs = paramList.map((parameter) => {
-            return <Section label={parameter} id={parameter}>
-                <input onChange={this.onParamValueChanged} className="form-control" type="text" placeholder="value" id={parameter} required />
-            </Section>
-        });
 
+    render() {
+        const { serviceNameOptions, servicePortOptions } = this.state;
+        const { t } = this.props;
+        let serviceNameList = serviceNameOptions.map(function (pvc) {
+            return <option value={pvc}>{pvc}</option>;
+        });
+        let servicePortList = servicePortOptions.map(function (pvc) {
+            return <option value={pvc}>{pvc}</option>;
+        });
         return <div className="co-m-pane__body">
             < Helmet >
-                <title>Create Ingress</title>
+                <title>{t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(this.state.ingress.kind, t) })}</title>
             </Helmet >
             <form className="co-m-pane__body-group co-create-secret-form" onSubmit={this.save}>
-                <h1 className="co-m-pane__heading">Create Ingress</h1>
-                <p className="co-m-pane__explanation">{this.props.explanation}</p>
+                <h1 className="co-m-pane__heading">{t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(this.state.ingress.kind, t) })}</h1>
+                {/* <p className="co-m-pane__explanation">{this.props.explanation}</p> */}
+
                 <fieldset disabled={!this.props.isCreate}>
+                    {/* Name */}
+                    <FirstSection label={t('CONTENT:NAME')} children={
+                        <input className="form-control"
+                            type="text"
+                            onChange={this.onNameChanged}
+                            value={this.state.ingress.metadata.name}
+                            required />
+                    } id="name" />
+
+                    {/* Resource Name */}
+                    <FirstSection label={t('CONTENT:HOSTNAME')} children={
+                        <input className="form-control"
+                            type="text"
+                            onChange={this.onResourceNameChanged}
+                            value={this.state.ingress['resourceName']}
+                            required />
+                    } id="resourceName" />
+
+                    {/* Resource Quota */}
                     <div className="form-group">
-                        <label className="control-label" htmlFor="secret-name">이름</label>
-                        <div>
-                            <input className="form-control"
-                                type="text"
-                                onChange={this.onNameChanged}
-                                value={this.state.templateInstance.metadata.name}
-                                id="template-instance-name"
-                                required />
-                        </div>
+                        <React.Fragment>
+                            <div className="form-group">
+                                <label className="control-label" htmlFor="username">
+                                    {t('CONTENT:PORT')}
+                                </label>
+                                <div>
+                                    <IngressEditor serviceNameOptions={serviceNameList} servicePortOptions={servicePortList} t={t} pathPairs={this.state.paths} updateParentData={this._updatePaths} />
+                                </div>
+                            </div>
+                        </React.Fragment>
                     </div>
-                    <div className="form-group">
-                        <label className="control-label" htmlFor="secret-type" >템플릿</label>
-                        <div>
-                            <select onChange={this.onTemplateChanged} className="form-control" id="template">
-                                {options}
-                            </select>
-                        </div>
-                    </div>
+                    <ButtonBar errorMessage={this.state.error} inProgress={this.state.inProgress} >
+                        <button type="submit" className="btn btn-primary" id="save-changes">{t('CONTENT:CREATE')}</button>
+                        <Link to={formatNamespacedRouteForResource('resourcequotaclaims')} className="btn btn-default" id="cancel">{t('CONTENT:CANCEL')}</Link>
+                    </ButtonBar>
                 </fieldset>
-                <label className="control-label" htmlFor="secret-name">Parameters </label>
-                <div>
-                    {paramDivs}
-                </div>
-                <React.Fragment>
-                    <div className="form-group">
-                        <label className="control-label" htmlFor="username">Label</label>
-                        <div>
-                            <SelectorInput labelClassName="co-text-namespace" tags={[]} />
-                        </div>
-                    </div>
-                </React.Fragment>
-                <ButtonBar errorMessage={this.state.error} inProgress={this.state.inProgress} >
-                    <button type="submit" className="btn btn-primary" id="save-changes">{this.props.saveButtonText || 'Create'}</button>
-                    <Link to={formatNamespacedRouteForResource('templateinstances')} className="btn btn-default" id="cancel">Cancel</Link>
-                </ButtonBar>
             </form>
+
         </div >;
     }
 };
 
-
-class SourceSecretForm extends React.Component<SourceSecretFormProps> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            stringData: this.props.stringData || {},
-        };
-        this.onDataChanged = this.onDataChanged.bind(this);
-    }
-    onDataChanged(secretsData) {
-        this.setState({
-            stringData: { ...secretsData },
-        }, () => this.props.onChange(this.state));
-    }
-    render() {
-        return <React.Fragment>
-        </React.Fragment>;
-    }
-}
-
-const secretFormFactory = secretType => {
-    return secretType === CreateType.form ? Requestform(SourceSecretForm) : Requestform(SourceSecretForm);
-};
-
-const SecretLoadingWrapper = props => {
-    const secretTypeAbstraction = determineCreateType(_.get(props.obj.data, 'data'));
-    const SecretFormComponent = secretFormFactory(secretTypeAbstraction);
-    const fixed = _.reduce(props.fixedKeys, (acc, k) => ({ ...acc, k: _.get(props.obj.data, k) }), {});
-    return <StatusBox {...props.obj}>
-        <SecretFormComponent {...props}
-            secretTypeAbstraction={secretTypeAbstraction}
-            obj={props.obj.data}
-            fixed={fixed}
-            explanation={pageExplanation[secretTypeAbstraction]}
-        />
-    </StatusBox>;
-};
-
 export const CreateIngress = ({ match: { params } }) => {
-    const SecretFormComponent = secretFormFactory(params.type);
-    return <SecretFormComponent fixed={{ metadata: { namespace: params.ns } }}
-        secretTypeAbstraction={params.type}
-        explanation={pageExplanation[params.type]}
+    const { t } = useTranslation();
+    return <IngressFormComponent
+        t={t}
+        fixed={{ metadata: { namespace: params.ns } }}
+        ingressTypeAbstraction={params.type}
+        explanation=''
+        // explanation={pageExplanation[params.type]}
         titleVerb="Create"
         isCreate={true}
     />;
 };
-
-export const EditSecret = ({ match: { params }, kind }) => <Firehose resources={[{ kind: kind, name: params.name, namespace: params.ns, isList: false, prop: 'obj' }]}>
-    <SecretLoadingWrapper fixedKeys={['kind', 'metadata']} titleVerb="Edit" saveButtonText="Save Changes" />
-</Firehose>;
-
-
-export type BaseEditSecretState_ = {
-    secretTypeAbstraction?: CreateType,
-    templateInstance: K8sResourceKind,
+export type IngressState_ = {
+    ingressTypeAbstraction?: CreateType,
+    ingress: K8sResourceKind,
     inProgress: boolean,
-    stringData: { [key: string]: string },
     error?: any,
-    templateList: Array<any>,
-    paramList: Array<any>,
-    editParamList: boolean,
-    selectedTemplate: string
+    type: string,
+    paths: Array<any>,
+    serviceNameList: Array<any>,
+    servicePortList: Array<any>,
+    serviceNameOptions: Array<any>;
+    servicePortOptions: Array<any>;
 };
 
-export type BaseEditSecretProps_ = {
+export type IngressProps_ = {
     obj?: K8sResourceKind,
     fixed: any,
     kind?: string,
     isCreate: boolean,
     titleVerb: string,
-    secretTypeAbstraction?: CreateType,
+    ingressTypeAbstraction?: CreateType,
     saveButtonText?: string,
     explanation: string,
+    t: any
 };
 
-export type SourceSecretFormProps = {
-    onChange: Function;
-    stringData: {
-        [key: string]: string
-    },
-    isCreate: boolean,
-};
-/* eslint-enable no-undef */
