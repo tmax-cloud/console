17ad12ceeabc6fcb272973456796ae9e65fca3ee
심소영 <soyoung_shim@tmax.co.kr>
2020-05-18 17:20:53 +0900

    [refactor] 파이프라인 사이드바 컴포넌트 오타 수정, 주석 제거
    [IMS] N/A

--- frontend/public/components/sidebars/pipeline-resource-sidebar.jsx
+++ frontend/public/components/sidebars/pipeline-resource-sidebar.jsx
@@ -10,13 +10,12 @@ export const PipelineResourceSidebar = ({ loadSampleYaml, downloadSampleYaml }) 
   // const { t } = useTranslation();
   const samples = [
     {
-      header: 'Creat Pipeline Resources',
+      header: 'Create Pipeline Resources',
       details: `파이프라인의 입력(예: Git 저장소) 또는 출력(예: Docker 이미지)을 정의하는 '파이프라인 리소스'를 생성할 수 있다.
       리소스 이름: 
       (1) name 파이프라인 리소스의 이름
       (2) type 파이프라인 리소스 유형
       (3) params 형상관리 저장소 및 레지스트리의 상세 명세`,
-      // TODO: yaml 추가하기
       templateName: 'pipelineresource-sample',
       kind: referenceForModel(PipelineResourceModel),
     },
--- frontend/public/components/sidebars/pipeline-sidebar.jsx
+++ frontend/public/components/sidebars/pipeline-sidebar.jsx
@@ -16,7 +16,6 @@ export const PipelineSidebar = ({ loadSampleYaml, downloadSampleYaml }) => {
       (1) serviceAccountName 네임스페이스 내부에 리소스를 생성하기 위해 권한을 가진 서비스 어카운트의 이름
       (2) pipelineRef 파이프라인 런에서 실행할 파이프라인의 이름 
       (3) resourceRef 태스크에서 실행될 컨테이너 이미지가 참조하는 파이프라인 리소스의 상세 명세`,
-      // TODO: yaml 추가하기
       templateName: 'pipeline-sample',
       kind: referenceForModel(PipelineModel),
     },
