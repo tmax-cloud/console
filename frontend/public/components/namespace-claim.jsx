6ceaab21595a94ab921a76da8f28c1e00204fe85
이경준 <gyoungjun_lee@tmax.co.kr>
2020-05-19 22:44:08 +0900

    [feat][patch] namespaceclaim resourcename 및 trial time 추가

--- frontend/public/components/namespace-claim.jsx
+++ frontend/public/components/namespace-claim.jsx
@@ -4,25 +4,36 @@ import * as React from 'react';
 import { ColHead, DetailsPage, List, ListHeader, ListPage } from './factory';
 import { Cog, navFactory, ResourceCog, SectionHeading, ResourceLink, ResourceSummary, ScrollToTopOnMount, kindObj } from './utils';
 import { fromNow } from './utils/datetime';
+import { k8sGet, k8sList } from '../module/k8s';
 import { breadcrumbsForOwnerRefs } from './utils/breadcrumbs';
 import { useTranslation } from 'react-i18next';
 import { ResourcePlural } from './utils/lang/resource-plural';
+import { NamespaceList } from './namespace';
+import { Timestamp } from './utils/timestamp';
 
 const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete, Cog.factory.EditStatus];
+let namespaceList = [];
+let flag = 0;
 
 const NamespaceClaimHeader = props => {
   const { t } = useTranslation();
   return (
     <ListHeader>
-      <ColHead {...props} className="col-xs-3 col-sm-3" sortField="metadata.name">
-        {t('CONTENT:NAME')}
-      </ColHead>
-      <ColHead {...props} className="col-xs-3 col-sm-3" sortField="status.status">
-        {t('CONTENT:STATUS')}
-      </ColHead>
       <ColHead {...props} className="col-sm-3 hidden-xs" sortField="resourceName">
         {t('CONTENT:RESOURCENAME')}
       </ColHead>
+      {/* <ColHead {...props} className="col-xs-2 col-sm-2" sortField="metadata.name">
+        {t('CONTENT:NAME')}
+      </ColHead> */}
+      <ColHead {...props} className="col-xs-3 col-sm-3" sortField="status.status">
+        {t('CONTENT:STATUS')}
+      </ColHead>
+      <ColHead {...props} className="col-xs-3 col-sm-3" sortField="status.status">
+        {t('CONTENT:TRIALTIME')}
+      </ColHead>
+      {/* <ColHead {...props} className="col-sm-3 hidden-xs" sortField="resourceName">
+        {t('CONTENT:RESOURCENAME')}
+      </ColHead> */}
       <ColHead {...props} className="col-sm-3 hidden-xs" sortField="metadata.creationTimestamp">
         {t('CONTENT:CREATED')}
       </ColHead>
@@ -30,17 +41,32 @@ const NamespaceClaimHeader = props => {
   );
 };
 
+const TrialTime = namespace => {
+  let createTime = new Date(namespace.metadata.creationTimestamp.iMillis);
+  let start = String(createTime.getFullYear()) + '.' + String(createTime.getMonth() + 1) + '.' + String(createTime.getDate());
+  let end = String(createTime.getFullYear()) + '.' + String(createTime.getMonth() + 2) + '.' + String(createTime.getDate());
+  return start + '~' + end;
+};
+
 const NamespaceClaimRow = () =>
   // eslint-disable-next-line no-shadow
   function NamespaceClaimRow({ obj }) {
+    flag = flag + 1;
+    const namespace = namespaceList.filter(cur => cur.metadata.name === obj.resourceName)[0];
+    let time = namespace && TrialTime(namespace);
     return (
       <div className="row co-resource-list__item">
-        <div className="col-xs-3 col-sm-3 co-resource-link-wrapper">
+        {/* <div className="col-xs-2 col-sm-2 co-resource-link-wrapper">
           <ResourceCog actions={menuActions} kind="NamespaceClaim" resource={obj} />
           <ResourceLink kind="NamespaceClaim" name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
+        </div> */}
+        <div className="col-md-3 col-xs-3 co-resource-link-wrapper">
+          <ResourceCog actions={menuActions} kind="NamespaceClaim" resource={obj} />
+          <ResourceLink kind="Namespace" name={obj.resourceName} title={obj.resourceName} />
         </div>
         <div className="col-xs-3 col-sm-3 hidden-xs">{obj.status && obj.status.status}</div>
-        <div className="col-xs-3 col-sm-3 hidden-xs">{obj.resourceName}</div>
+        <div className="col-xs-3 col-sm-3 hidden-xs">{time}</div>
+        {/* <div className="col-xs-3 col-sm-3 hidden-xs">{obj.resourceName}</div> */}
         <div className="col-xs-3 col-sm-3 hidden-xs">{fromNow(obj.metadata.creationTimestamp)}</div>
       </div>
     );
@@ -114,7 +140,32 @@ export const NamespaceClaimsPage = props => {
     items: createItems,
     createLink: type => `/k8s/cluster/namespaceclaims/new${type !== 'yaml' ? '/' + type : ''}`,
   };
-  return <ListPage {...props} ListComponent={NamespaceClaimList} canCreate={true} kind="NamespaceClaim" createProps={createProps}{...props} createButtonText={t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(props.kind, t) })} />;
+  React.useEffect(() => {
+    // window.setTimeout(
+    k8sList(kindObj('Namespace'))
+      .then(reponse => reponse)
+      .then(
+        data => {
+          if (flag && !namespaceList.length) {
+            // 이유는 모르겠는데
+            location.reload();
+          }
+          namespaceList = data;
+        },
+        err => {
+          console.error(err);
+        },
+      );
+    // .then(
+    //   () => {},
+    //   err => {
+    //     console.error(err);
+    //   },
+    // );
+    // );
+  }, []);
+
+  return <ListPage {...props} ListComponent={NamespaceClaimList} canCreate={true} kind="NamespaceClaim" createProps={createProps} {...props} createButtonText={t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(props.kind, t) })} />;
 };
 NamespaceClaimsPage.displayName = 'NamespaceClaimsPage';
 
