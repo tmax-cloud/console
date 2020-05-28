7919a6c6839083d5181fe53b9e1f8702c56790e6
신은경 <eunkyeng_shin@tmax.co.kr>
2020-05-28 18:02:54 +0900

    [feat] release 브랜치 설정값 변경

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 ./bin/bridge \
-    --listen=https://192.168.8.33:9000 \
-    --base-address=https://192.168.8.33:9000 \
+    --listen=https://192.168.8.23:9000 \
+    --base-address=https://192.168.8.23:9000 \
     --tls-cert-file=tls/tls.crt \
     --tls-key-file=tls/tls.key \
     --k8s-mode=off-cluster \
-    --k8s-mode-off-cluster-endpoint=https://192.168.6.196:6443 \
+    --k8s-mode-off-cluster-endpoint=https://192.168.6.145:6443 \
     --k8s-mode-off-cluster-skip-verify-tls=true \
     --k8s-auth=bearer-token \
     --k8s-auth-bearer-token=@@ \
     --public-dir=./frontend/public/dist \
-    --hypercloud-endpoint=http://192.168.6.211:28677 \
-    --prometheus-endpoint=http://192.168.6.215:9090/api \
+    --hypercloud-endpoint=http://192.168.6.149:28677 \
+    --prometheus-endpoint=http://192.168.6.196:31029/api \
     --release-mode=true \
