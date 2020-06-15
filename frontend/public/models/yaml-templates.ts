32fb556d7b7526e31457551b1baef9d6f0f17f73
신은경 <eunkyeng_shin@tmax.co.kr>
2020-06-15 10:01:37 +0900

    [feat][patch] template > create sample 1 최신 yaml반영 IMS:228383

--- frontend/public/models/yaml-templates.ts
+++ frontend/public/models/yaml-templates.ts
@@ -1233,29 +1233,52 @@ spec:
     kind: Template
     metadata:
       name: example-template
-      namespace: demo-ns
+      namespace: default
+    imageUrl: example.com/example.gif
+    provider: tmax
+    recommend: true
     objects:
-      - apiVersion: apps/v1
-        kind: Deployment
-        metadata:
-          name: example
-          labels:
-            app: example
-        spec:
-          selector:
-            matchLabels:
-              app: example
-          template:
-            metadata:
-              labels:
-                app: example
-            spec:
-              containers:
-                - name: example
-                  image: 'example/image:version'
-                  ports:
-                    - name: example
-                      containerPort: 80
+    - apiVersion: apps/v1
+      kind: Deployment
+      metadata:
+        name: \${NAME}
+        labels:
+          app: \${NAME}
+      spec:
+        selector:
+          matchLabels:
+            app: \${NAME}
+        template:
+          metadata:
+            labels:
+              app: \${NAME}
+          spec:
+            containers:
+            - name: \${NAME}
+              image: example/image:version
+              ports:
+              - name: example
+                containerPort: 80
+    plans:
+    - name: example-plan
+      metadata:
+        bullets:
+        - feat 1
+        - feat 2
+        costs:
+          amount: 100
+          unit: $
+        bindable: true
+        schemas:
+          service_instance:
+            create:
+              parameters:
+                EXAMPLE_PARAM: value
+    parameters:
+    - name: NAME
+      description: Application name
+      valueType: string
+      value: example
 `,
   )
   .setIn(
