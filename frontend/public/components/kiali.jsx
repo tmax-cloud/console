19c6d101c55fa080ac3ec253a2aed8f794e1ea6e
신은경 <eunkyeng_shin@tmax.co.kr>
2020-06-26 16:14:43 +0900

    [feat] grafana, kiali화면 추가

---
+++ frontend/public/components/kiali.jsx
@@ -0,0 +1,97 @@
+import * as _ from 'lodash-es';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
+import * as PropTypes from 'prop-types';
+import * as classNames from 'classnames';
+
+import { history, NavTitle, SelectorInput, LoadingBox } from './utils';
+import { namespaceProptype } from '../propTypes';
+import { split, selectorFromString } from '../module/k8s/selector';
+import { requirementFromString } from '../module/k8s/selector-requirement';
+import { resourceListPages } from './resource-pages';
+import { ResourceListDropdown } from './resource-dropdown';
+import { connectToModel } from '../kinds';
+import { connectToFlags, FLAGS, flagPending } from '../features';
+import { OpenShiftGettingStarted } from './start-guide';
+import { referenceForModel, kindForReference } from '../module/k8s';
+import { AsyncComponent } from './utils/async';
+import { DefaultPage } from './default-resource';
+
+const ResourceList = connectToModel(({ kindObj, kindsInFlight, namespace, selector, fake }) => {
+    if (kindsInFlight) {
+        return <LoadingBox />;
+    }
+
+    const componentLoader = resourceListPages.get(referenceForModel(kindObj), () => Promise.resolve(DefaultPage));
+    const ns = kindObj.namespaced ? namespace : undefined;
+
+    return <AsyncComponent loader={componentLoader} namespace={ns} selector={selector} kind={kindObj.crd ? referenceForModel(kindObj) : kindObj.kind} showTitle={false} autoFocus={false} fake={fake} />;
+});
+
+const updateUrlParams = (k, v) => {
+    const url = new URL(window.location);
+    const sp = new URLSearchParams(window.location.search);
+    sp.set(k, v);
+    history.push(`${url.pathname}?${sp.toString()}${url.hash}`);
+};
+
+const updateKind = kind => updateUrlParams('kind', encodeURIComponent(kind));
+const updateTags = tags => updateUrlParams('q', tags.map(encodeURIComponent).join(','));
+
+class KialiPage_ extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.setRef = ref => (this.ref = ref);
+        this.onSelectorChange = k => {
+            updateKind(k);
+            this.ref && this.ref.focus();
+        };
+    }
+
+    render() {
+        const { flags, location, namespace, t } = this.props;
+        let kind, q;
+
+        if (flagPending(flags.OPENSHIFT) || flagPending(flags.PROJECTS_AVAILABLE)) {
+            return null;
+        }
+
+        if (location.search) {
+            const sp = new URLSearchParams(window.location.search);
+            kind = sp.get('kind');
+            q = sp.get('q');
+        }
+
+        // Ensure that the "kind" route parameter is a valid resource kind ID
+        kind = kind ? decodeURIComponent(kind) : 'Service';
+
+        const tags = split(_.isString(q) ? decodeURIComponent(q) : '');
+        const validTags = _.reject(tags, tag => requirementFromString(tag) === undefined);
+        const selector = selectorFromString(validTags.join(','));
+        const labelClassName = `co-text-${_.toLower(kindForReference(kind))}`;
+        const showGettingStarted = flags.OPENSHIFT && !flags.PROJECTS_AVAILABLE;
+        let url = `${document.location.origin}/api/kiali`
+        return (
+            <React.Fragment>
+                {showGettingStarted && <OpenShiftGettingStarted />}
+                <div className={classNames({ 'co-disabled': showGettingStarted })}>
+                    <Helmet>
+                        <title>Kiali</title>
+                    </Helmet>
+                    <NavTitle title='Kiali'>
+
+                    </NavTitle>
+                    <iframe style={{ width: '100%', height: '100vh', border: 0 }} src={url} target="_blank" />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export const KialiPage = connectToFlags(FLAGS.OPENSHIFT, FLAGS.PROJECTS_AVAILABLE)(KialiPage_);
+
+KialiPage.propTypes = {
+    namespace: namespaceProptype,
+    location: PropTypes.object.isRequired,
+};
