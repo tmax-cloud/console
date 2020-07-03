7bcb138a9ce973ffaed5ee87079c5a3cb25b4c83
dongmin-jung <66112900+dongmin-jung@users.noreply.github.com>
2020-07-03 16:09:46 +0900

    [feat] 개발 편의를 위한 IP 불러오기 기능 추가

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -1,25 +1,28 @@
 #!/bin/bash -e
 
-### proxy endpoint들의 node port 정보를 받아오려면 아래 주석처리된 두 줄을 차례로 실행하세요.
+myIP=$(ipconfig | grep "IPv4" -a | head -1 | awk '{print $NF}')
+k8sIP='192.168.6.196'
+
+### proxy endpoint들의 node port 정보를 받아오려면, 터미널에서 아래의 주석 두 줄을 차례로 실행하세요.
 : <<'END'
-ssh root@192.168.6.196
-PORT_ARR=($(kubectl get svc -A | grep -E "hypercloud4-operator-service|prometheus-k8s|grafana|kiali|tracing" | grep -v "kiali-operator" | awk '{print $6}' | awk 'match($0, /:.*\//){print substr($0,RSTART+1,RLENGTH-2)}')); printf "\n\n\nhypercloud_port = ${PORT_ARR[0]}\nkiali_port = ${PORT_ARR[1]}\njaeger_port = ${PORT_ARR[2]}\ngrafana_port = ${PORT_ARR[3]}\nprometheus_port = ${PORT_ARR[4]}\n\n\n\n\n"; exit
+ssh root@$k8sIP
+printf "\n\n\n"; kubectl get svc -A | grep -E "hypercloud4-operator-service|prometheus-k8s|grafana|kiali|tracing" | grep ":" | awk -F '\t' -v OFS='\t' '{ result=gensub(/[0-9]+:([0-9]+)\/TCP/,"\\1", "g", $0); print result}' | awk '{print $1,$2,$6}' | column -t; printf "\n\n\n"; exit
 END
 
 ./bin/bridge \
-    --listen=https://192.168.8.25:9000 \
-    --base-address=https://192.168.8.25:9000 \
+    --listen=https://$myIP:9000 \
+    --base-address=https://$myIP:9000 \
     --tls-cert-file=tls/tls.crt \
     --tls-key-file=tls/tls.key \
     --k8s-mode=off-cluster \
-    --k8s-mode-off-cluster-endpoint=https://192.168.6.196:6443 \
+    --k8s-mode-off-cluster-endpoint=https://$k8sIP:6443 \
     --k8s-mode-off-cluster-skip-verify-tls=true \
     --k8s-auth=bearer-token \
     --k8s-auth-bearer-token=@@ \
     --public-dir=./frontend/public/dist \
-    --hypercloud-endpoint=http://192.168.6.211:28677 \
-    --kiali-endpoint=http://192.168.6.196:31971 \
-    --jaeger-endpoint=http://192.168.6.196:32602/api/jaeger \
-    --grafana-endpoint=http://192.168.6.196:31527 \
-    --prometheus-endpoint=http://192.168.6.196:30562/api \
+    --hypercloud-endpoint=http://$k8sIP:31092 \
+    --kiali-endpoint=http://$k8sIP:30197 \
+    --jaeger-endpoint=http://$k8sIP:30573/api/jaeger \
+    --grafana-endpoint=http://$k8sIP:31527 \
+    --prometheus-endpoint=http://$k8sIP:30562/api \
     --release-mode=true \
