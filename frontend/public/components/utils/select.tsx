a705a09330e63780be499c7998a926781286b78f
이경준 <gyoungjun_lee@tmax.co.kr>
2020-07-06 12:54:10 +0900

    [feat][patch] react-select 컴포넌트 value값 명시적으로 안주어도 선택되도록 수정, placeholder property추가

--- frontend/public/components/utils/select.tsx
+++ frontend/public/components/utils/select.tsx
@@ -9,33 +9,22 @@ type OptionType = {
 type SingleSelectProps = {
   name: string;
   options: OptionType[];
-  value: string;
+  placeholder?: string;
   className?: string;
   id?: string;
   onChange: () => {};
 };
 
-const SingleSelect = ({ options, name, value, id, onChange }: SingleSelectProps) => {
-  const newValue = value ? { value, label: value } : options[0];
+const SingleSelect = ({ options, name, id, placeholder, onChange }: SingleSelectProps) => {
   const colorStyles = {
     control: styles => ({ ...styles, backgroundColor: 'white', borderRadius: 0, height: '27px', minHeight: '27px' }),
   };
 
   return (
     <React.Fragment>
-      <Select
-        className="basic-single"
-        classNamePrefix="select"
-        id={id}
-        value={newValue}
-        name={name}
-        options={options}
-        onChange={onChange}
-        styles={colorStyles}
-        isSearchable={false}
-      />
+      <Select className="basic-single" classNamePrefix="select" id={id} defaultValue={options[0]} placeholder={placeholder} name={name} options={options} onChange={onChange} styles={colorStyles} isSearchable={false} />
     </React.Fragment>
-  )
+  );
 };
 
 export default SingleSelect;
