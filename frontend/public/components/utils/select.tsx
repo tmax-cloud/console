211c6c72c77c49790feae08ac2411e653a3b006a
이경준 <gyoungjun_lee@tmax.co.kr>
2020-07-09 12:27:35 +0900

    [feat][patch] react-select newValule property 없앴던 것 원복

--- frontend/public/components/utils/select.tsx
+++ frontend/public/components/utils/select.tsx
@@ -9,20 +9,23 @@ type OptionType = {
 type SingleSelectProps = {
   name: string;
   options: OptionType[];
+  label?: string;
+  value?: string;
   placeholder?: string;
   className?: string;
   id?: string;
   onChange: () => {};
 };
 
-const SingleSelect = ({ options, name, id, placeholder, onChange }: SingleSelectProps) => {
+const SingleSelect = ({ options, name, id, value, placeholder, label, onChange }: SingleSelectProps) => {
+  const newValue = value ? { value, label: label || value } : options.length > 0 && options[0];
   const colorStyles = {
     control: styles => ({ ...styles, backgroundColor: 'white', borderRadius: 0, height: '27px', minHeight: '27px' }),
   };
 
   return (
     <React.Fragment>
-      <Select className="basic-single" classNamePrefix="select" id={id} placeholder={placeholder} name={name} options={options} onChange={onChange} styles={colorStyles} isSearchable={false} />
+      <Select className="basic-single" classNamePrefix="select" value={newValue} id={id} placeholder={placeholder} name={name} options={options} onChange={onChange} styles={colorStyles} isSearchable={false} />
     </React.Fragment>
   );
 };
