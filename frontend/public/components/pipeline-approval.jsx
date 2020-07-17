d327c7bea9d8f2dd01e07e840be36c8bd9bc45a4
심소영 <soyoung_shim@tmax.co.kr>
2020-07-17 16:25:35 +0900

    [feat][patch] 파이프라인 승인 리스트 - 생성 버튼 제거, 상태 컬럼 추가
    [IMS] N/A

--- frontend/public/components/pipeline-approval.jsx
+++ frontend/public/components/pipeline-approval.jsx
@@ -9,19 +9,23 @@ import { breadcrumbsForOwnerRefs } from './utils/breadcrumbs';
 import { PipelineApprovalModel } from '../models';
 import { useTranslation } from 'react-i18next';
 import { ResourcePlural } from './utils/lang/resource-plural';
-const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete];
+// const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.EditStatus];
+const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit];
 
 const PipelineApprovalHeader = props => {
   const { t } = useTranslation();
   return (
     <ListHeader>
-      <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.name">
+      <ColHead {...props} className="col-sm-3 col-xs-6" sortField="metadata.name">
         {t('CONTENT:NAME')}
       </ColHead>
-      <ColHead {...props} className="col-xs-4 col-sm-4" sortField="metadata.namespace">
+      <ColHead {...props} className="col-sm-3 col-xs-6" sortField="metadata.namespace">
         {t('CONTENT:NAMESPACE')}
       </ColHead>
-      <ColHead {...props} className="col-sm-4 hidden-xs" sortField="metadata.creationTimestamp">
+      <ColHead {...props} className="col-sm-3 col-xs-6" sortField="status.result">
+        {t('CONTENT:STATUS')}
+      </ColHead>
+      <ColHead {...props} className="col-sm-3 hidden-xs" sortField="metadata.creationTimestamp">
         {t('CONTENT:CREATED')}
       </ColHead>
     </ListHeader>
@@ -33,12 +37,13 @@ const PipelineApprovalRow = () =>
   function PipelineApprovalRow({ obj }) {
     return (
       <div className="row co-resource-list__item">
-        <div className="col-xs-4 col-sm-4 co-resource-link-wrapper">
+        <div className="col-sm-3 col-xs-6 co-resource-link-wrapper">
           <ResourceCog actions={menuActions} kind="Approval" resource={obj} />
           <ResourceLink kind="Approval" name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
         </div>
-        <div className="col-xs-4 col-sm-4 co-break-word">{obj.metadata.namespace ? <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} /> : 'None'}</div>
-        <div className="col-xs-4 col-sm-4 hidden-xs">{fromNow(obj.metadata.creationTimestamp)}</div>
+        <div className="col-sm-3 col-xs-6 co-break-word">{obj.metadata.namespace ? <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} /> : 'None'}</div>
+        <div className="col-sm-3 col-xs-6 co-break-word">{obj.hasOwnProperty('status') ? obj.status.result : 'Waiting'}</div>
+        <div className="col-sm-3 col-xs-6 hidden-xs">{fromNow(obj.metadata.creationTimestamp)}</div>
       </div>
     );
   };
@@ -66,7 +71,7 @@ PipelineApprovalList.displayName = PipelineApprovalList;
 
 export const PipelineApprovalsPage = props => {
   const { t } = useTranslation();
-  return <ListPage {...props} ListComponent={PipelineApprovalList} canCreate={true} kind="Approval" createButtonText={t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(props.kind, t) })} />;
+  return <ListPage {...props} ListComponent={PipelineApprovalList} canCreate={false} kind="Approval" createButtonText={t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural(props.kind, t) })} />;
 };
 PipelineApprovalsPage.displayName = 'PipelineApprovalsPage';
 
