ea036de3c21257404c900b28d02f743e78e0776d
yeonggyulim <lyg940128@gmail.com>
2020-07-23 18:30:09 +0900

    [bugfix] 머지 에러 수정

--- frontend/public/components/inference-service.jsx
+++ frontend/public/components/inference-service.jsx
@@ -40,12 +40,8 @@ const InferenceServiceRow = () =>
   // eslint-disable-next-line no-shadow
   function InferenceServiceRow({ obj }) {
     let status = obj.status.conditions.length ? obj.status.conditions[obj.status.conditions.length - 1].type : '';
-<<<<<<< HEAD
-    let framework = Object.keys(obj.spec.default.predictor)[0];
-=======
     const frameworkList = ['tensorflow', 'onnx', 'sklearn', 'xgboost', 'pytorch', 'tensorrt'];
     let framework = frameworkList.includes(Object.keys(obj.spec.default.predictor)[0]) ? Object.keys(obj.spec.default.predictor)[0] : Object.keys(obj.spec.default.predictor)[1];
->>>>>>> def5fc51f94af9aed8cd1e64c7745438296f265b
     let storageUri = obj.spec.default.predictor[framework].storageUri;
     let canary = Object.keys(obj.status.canary).length === 0 ? 'N' : 'Y';
     return (
