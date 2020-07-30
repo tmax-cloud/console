f0f9370e16dda807be0b77acac3cee6a690ebc87
신은경 <eunkyeng_shin@tmax.co.kr>
2020-07-30 15:51:25 +0900

    [feat][patch] User Group > create sidebar기능 추가 IMS : 229213

--- frontend/public/components/sidebars/user-group-sidebar.jsx
+++ frontend/public/components/sidebars/user-group-sidebar.jsx
@@ -1,25 +1,20 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
 
-import { UsergroupModel } from '../../models';
+import { UserGroupModel } from '../../models';
 import { referenceForModel } from '../../module/k8s';
 import { SampleYaml } from './resource-sidebar';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 export const UserGroupSidebar = ({ loadSampleYaml, downloadSampleYaml }) => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const samples = [
     {
-      header: 'Example 1. Creat UserGroups',
-      details: `리소스 이름: 
-      metadata.name          사용자 그룹의 ID
-      userGroupInfo.name     사용자 그룹의 이름
-      userGroupInfo.department     사용자 그룹 부서
-      userGroupInfo.position      사용자 그룹 직위
-      userGroupInfo.description   사용자 그룹 설명`,
+      header: t('STRING:USERGROUP-SIDEBAR_0'),
+      details: t('STRING:USERGROUP-SIDEBAR_1'),
       templateName: 'usergroup-sample',
-      kind: referenceForModel(UsergroupModel),
-    }
+      kind: referenceForModel(UserGroupModel),
+    },
   ];
 
   return (
