50923344fa4604149898e9fe12e55f5072197fd2
이경준 <gyoungjun_lee@tmax.co.kr>
2020-08-10 09:52:44 +0900

    [feat] Task Form Editor 페이지 생성

--- frontend/public/components/task.jsx
+++ frontend/public/components/task.jsx
@@ -80,6 +80,14 @@ export const taskType = task => {
 
 export const TasksPage = ({ namespace, showTitle, flags }) => {
   const { t } = useTranslation();
+  const createItems = {
+    form: t('CONTENT:FORMEDITOR'),
+    yaml: t('CONTENT:YAMLEDITOR'),
+  };
+  const createProps = {
+    items: createItems,
+    createLink: type => `/k8s/cluster/tasks/new${type !== 'yaml' ? '/' + type : ''}`,
+  };
   // const isAdmin = !flagPending(flags.CAN_LIST_TASK) && flags.CAN_LIST_TASK;
   // if (!flags.CAN_LIST_NS && !flagPending(flags.CAN_LIST_TASK)) {
   //   return <Loading />;
@@ -96,7 +104,7 @@ export const TasksPage = ({ namespace, showTitle, flags }) => {
       canCreate={true}
       showTitle={showTitle}
       namespace={namespace}
-      createProps={{ to: `/k8s/ns/${namespace || 'default'}/tasks/new` }}
+      createProps={createProps}
       filterLabel="Tasks by name"
       flatten={resources => _.flatMap(resources, 'data').filter(r => !!r)}
       createButtonText={t('ADDITIONAL:CREATEBUTTON', { something: ResourcePlural('Task', t) })}
---
+++ frontend/public/components/utils/resource-editor.jsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import * as _ from 'lodash-es';
+import * as classNames from 'classnames';
+import { ResourceEditorPair } from './index';
+
+export class ResourceEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this._append = this._append.bind(this);
+    this._change = this._change.bind(this);
+    this._remove = this._remove.bind(this);
+  }
+  _append(event) {
+    const { updateParentData, values, nameValueId, allowSorting } = this.props;
+    let lastIndex = this.props.values.length - 1;
+    let lastData = this.props.values[lastIndex];
+    updateParentData({ values: allowSorting ? portPairs.concat([['', values.length]]) : values.concat([['']]) }, nameValueId);
+  }
+
+  _remove(i) {
+    const { updateParentData, nameValueId } = this.props;
+    const values = _.cloneDeep(this.props.values);
+    values.splice(i, 1);
+    updateParentData({ values: values.length ? values : [['', '']] }, nameValueId);
+  }
+
+  _change(e, i, type) {
+    const { updateParentData, nameValueId } = this.props;
+    const values = _.cloneDeep(this.props.values);
+    values[i][type] = e.target.value;
+    updateParentData({ values }, nameValueId);
+  }
+  render() {
+    const { desc, title, valueString, addString, values, allowSorting, readOnly, nameValueId, t } = this.props;
+    const portItems = values.map((pair, i) => {
+      const key = _.get(pair, [ResourceEditorPair.Index], i);
+      return <ResourceElement onChange={this._change} index={i} t={t} valueString={valueString} allowSorting={allowSorting} readOnly={readOnly} pair={pair} key={key} onRemove={this._remove} rowSourceId={nameValueId} />;
+    });
+    return (
+      <React.Fragment>
+        <div className="row">{title !== 'false' && <div className="col-md-2 col-xs-2 text-secondary">{t(`CONTENT:${valueString.toUpperCase()}`)}</div>}</div>
+        {portItems}
+        <span>{desc}</span>
+        {/* <div className="row">
+          <div className="col-md-12 col-xs-12">
+            {readOnly ? null : (
+              <React.Fragment>
+                <span className="btn-link pairs-list__btn" onClick={this._append}>
+                  <i aria-hidden="true" className="fa fa-plus-circle pairs-list__add-icon" />
+                  {t(`CONTENT:${addString.toUpperCase()}`)}
+                </span>
+              </React.Fragment>
+            )}
+          </div>
+        </div> */}
+      </React.Fragment>
+    );
+  }
+}
+ResourceEditor.defaultProps = {
+  valueString: 'Value',
+  addString: 'AddMore',
+  allowSorting: false,
+  readOnly: false,
+  nameValueId: 0,
+};
+
+class ResourceElement extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onRemove = this._onRemove.bind(this);
+    this._onChangeValue = this._onChangeValue.bind(this);
+  }
+  _onRemove() {
+    const { index, onRemove } = this.props;
+    onRemove(index);
+  }
+  _onChangeValue(e) {
+    const { index, onChange } = this.props;
+    onChange(e, index, ResourceEditorPair.Value);
+  }
+  onInputResource(e) {
+    console.log('input resource: ',e);
+  }
+  render() {
+    const { keyString, valueString, allowSorting, readOnly, pair, t } = this.props;
+    const deleteButton = (
+      <React.Fragment>
+        <i className="fa fa-minus-circle pairs-list__side-btn pairs-list__delete-icon" aria-hidden="true" onClick={this._onRemove}></i>
+        <span className="sr-only">Delete</span>
+      </React.Fragment>
+    );
+
+    return (
+      <div className={classNames('row')} ref={node => (this.node = node)}>
+        <div className="col-md-4 col-xs-4 pairs-list__protocol-field">
+          <input type="text" className="form-control" placeholder={t(`CONTENT:${valueString.toUpperCase()}`)} value={pair[ResourceEditorPair.Value] || ''} onChange={this._onChangeValue} />
+        </div>
+        {readOnly ? null : (
+          <div className="col-md-1 col-xs-2">
+            <span className={classNames(allowSorting ? 'pairs-list__span-btns' : null)}>{allowSorting ? <React.Fragment>{deleteButton}</React.Fragment> : deleteButton}</span>
+          </div>
+        )}
+        <span className="btn-link pairs-list__btn" onClick={() => {
+          import('./../modals/input-resource-modal').then(m => {
+            m.TaskModal({onInputResource: this.onInputResource});
+          });
+        }}>
+          <i aria-hidden="true" className="fa fa-plus-circle pairs-list__add-icon" />
+          {t('CONTENT:ADDMORE')}
+        </span>
+      </div>
+    );
+  }
+}
