5d17a9e2cf896ee6a8b7e5a586c3dc15e1763fd6
심소영 <soyoung_shim@tmax.co.kr>
2020-08-10 19:25:02 +0900

    [bugfix] cofetch url 에 쿼리 있을 때만 물음표 추가
    [IMS] N/A

--- frontend/public/module/k8s/resource.js
+++ frontend/public/module/k8s/resource.js
@@ -115,7 +115,7 @@ export const k8sList = (kind, params = {}, raw = false, options = {}) => {
     listURL = resourceURL(kind, { ns: params.ns });
   }
 
-  return coFetchJSON(`${listURL}?${query}`, 'GET', options).then(result => (raw ? result : result.items));
+  return coFetchJSON(`${listURL}${query && '?' + query}`, 'GET', options).then(result => (raw ? result : result.items));
 };
 
 export const k8sListPartialMetadata = (kind, params = {}, raw = false) => {
