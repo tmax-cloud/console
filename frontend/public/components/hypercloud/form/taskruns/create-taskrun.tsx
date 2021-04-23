b9e9b5e1b08f07e68ad0bdb6965586117cc0bf62
mjsso <minjin_so@tmax.co.kr>
2021-04-23 16:11:01 +0900

    [refactor] Create TaskRun에서 onChange, reset사용하지 않고 useWatch만 사용하도록 수정

--- frontend/public/components/hypercloud/form/taskruns/create-taskrun.tsx
+++ frontend/public/components/hypercloud/form/taskruns/create-taskrun.tsx
@@ -1,7 +1,7 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
 import { useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { useFormContext, useWatch } from 'react-hook-form';
 import { match as RMatch } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { WithCommonForm } from '../create-form';
@@ -52,13 +52,19 @@ const paramItemRenderer = (register, name, item, index, ListActions, ListDefault
 const CreateTaskRunComponent: React.FC<TaskRunFormProps> = props => {
   const { t } = useTranslation();
   const methods = useFormContext();
-  const [selectedTask, setSelectedTask] = useState('');
   const [inputList, setInputList] = useState([]);
   const [outputList, setOutputList] = useState([]);
   const [paramList, setParamList] = useState([]);
   const [workspaceList, setWorkspaceList] = useState([]);
+  const [paramValueListData, setParamValueListData] = useState([]);
 
   const namespace = getActiveNamespace(store.getState());
+
+  const selectedTask = useWatch({
+    control: methods.control,
+    name: 'spec.taskRef.name',
+    defaultValue: '',
+  });
 
   const getTaskDetails = async selectedTaskName => {
     const task = await k8sGet(TaskModel, selectedTaskName, getActiveNamespace(store.getState()));
@@ -97,10 +103,7 @@ const CreateTaskRunComponent: React.FC<TaskRunFormProps> = props => {
       }
     });
 
-    // MEMO : ListView의 value를 세팅해주는 방법이 reset이 최선일까..?
-    // MEMO : reset할 때 params 부분 이외에 다른 value들은 그대로 있게 하기 위해 이렇게 함.
-    const prevValues = methods.getValues();
-    methods.reset({ ...prevValues, params: paramValueListData });
+    setParamValueListData(paramValueListData || []);
 
     const paramsData = task.spec?.params?.map(param => {
       return {
@@ -122,88 +125,112 @@ const CreateTaskRunComponent: React.FC<TaskRunFormProps> = props => {
     setWorkspaceList(workspacesData || []);
   };
 
-  const inputs = inputList.map((item, index) => {
-    return (
-      <Section label={`${item.name} (${item.type})`} id={`input_${index}`} key={`input_${index}`} isRequired={item.required} description={item.description}>
-        <>
-          {/* <input ref={methods.register} type="hidden" id={`spec.resources.inputs[${index}].name`} name={`spec.resources.inputs[${index}].name`} value={item.name} /> */}
-          <ResourceDropdown name={`spec.resources.inputs.${item.name}.resourceRef.name`} placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_8')} resources={[{ kind: PipelineResourceModel.kind, namespace: namespace, prop: 'pipelineresource' }]} type="single" useHookForm />
-        </>
-      </Section>
+  const inputs =
+    inputList.length > 0 ? (
+      inputList.map((item, index) => {
+        return (
+          <Section label={`${item.name} (${item.type})`} id={`input_${index}`} key={`input_${index}`} isRequired={item.required} description={item.description}>
+            <>
+              {/* <input ref={methods.register} type="hidden" id={`spec.resources.inputs[${index}].name`} name={`spec.resources.inputs[${index}].name`} value={item.name} /> */}
+              <ResourceDropdown name={`spec.resources.inputs.${item.name}.resourceRef.name`} placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_8')} resources={[{ kind: PipelineResourceModel.kind, namespace: namespace, prop: 'pipelineresource' }]} type="single" methods={methods} useHookForm />
+            </>
+          </Section>
+        );
+      })
+    ) : (
+      <div className="help-block">{t('SINGLE:MSG_TASKRUN_CREATFORM_DIV2_4')}</div>
     );
-  });
 
-  const outputs = outputList.map((item, index) => {
-    return (
-      <Section label={`${item.name} (${item.type})`} id={`output_${index}`} key={`output_${index}`} isRequired={item.required} description={item.description}>
-        <>
-          {/* <input ref={methods.register} type="hidden" id={`spec.resources.outputs[${index}].name`} name={`spec.resources.outputs[${index}].name`} value={item.name} /> */}
-          <ResourceDropdown name={`spec.resources.outputs.${item.name}.resourceRef.name`} placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_8')} resources={[{ kind: PipelineResourceModel.kind, namespace: namespace, prop: 'pipelineresource' }]} type="single" useHookForm />
-        </>
-      </Section>
+  const outputs =
+    outputList.length > 0 ? (
+      outputList.map((item, index) => {
+        return (
+          <Section label={`${item.name} (${item.type})`} id={`output_${index}`} key={`output_${index}`} isRequired={item.required} description={item.description}>
+            <>
+              {/* <input ref={methods.register} type="hidden" id={`spec.resources.outputs[${index}].name`} name={`spec.resources.outputs[${index}].name`} value={item.name} /> */}
+              <ResourceDropdown name={`spec.resources.outputs.${item.name}.resourceRef.name`} placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_8')} resources={[{ kind: PipelineResourceModel.kind, namespace: namespace, prop: 'pipelineresource' }]} type="single" methods={methods} useHookForm />
+            </>
+          </Section>
+        );
+      })
+    ) : (
+      <div className="help-block">{t('SINGLE:MSG_TASKRUN_CREATFORM_DIV2_7')}</div>
     );
-  });
 
   // MEMO : ListView는 name에 민감해서 ListView와 item renderer의 item들의 이름은 고정시키고 name부분은 hidden input으로 넣어둠.
-  const params = paramList.map((item, index) => {
-    if (item.type === 'array') {
-      return (
-        <Section label={item.name} id={`${selectedTask}_param_${index}`} key={`${selectedTask}_param_${index}`} description={item.description}>
-          <>
-            <input ref={methods.register} type="hidden" id={`params[${index}].name`} name={`params[${index}].name`} value={item.name} />
-            <ListView name={`params[${index}].value`} methods={methods} addButtonText="추가" headerFragment={<></>} itemRenderer={paramItemRenderer} defaultItem={{ value: '' }} />
-          </>
-        </Section>
-      );
-    } else {
-      return (
-        <Section label={item.name} id={`${selectedTask}_param_${index}`} key={`${selectedTask}_param_${index}`} description={item.description}>
-          <>
-            <input ref={methods.register} type="hidden" id={`params[${index}].name`} name={`params[${index}].name`} value={item.name} />
-            <input ref={methods.register} className="pf-c-form-control" id={`params[${index}].value`} name={`params[${index}].value`} defaultValue={item.value} />
-          </>
-        </Section>
-      );
-    }
-  });
-
-  const workspaces = workspaceList.map((item, index) => {
-    return (
-      <Section label={item.name} description={item.description} key={index} id={`workspace_${index}_${item.name}`}>
-        <></>
-      </Section>
+  const params =
+    paramList.length > 0 ? (
+      paramList.map((item, index) => {
+        if (item.type === 'array') {
+          return (
+            <Section label={item.name} id={`${selectedTask}_param_${index}`} key={`${selectedTask}_param_${index}`} description={item.description}>
+              <>
+                <input ref={methods.register} type="hidden" id={`params.${index}.name`} name={`params.${index}.name`} value={item.name} />
+                <ListView name={`params.${index}.value`} methods={methods} addButtonText="추가" headerFragment={<></>} itemRenderer={paramItemRenderer} defaultItem={{ value: '' }} defaultValues={paramValueListData[index]?.value} />
+              </>
+            </Section>
+          );
+        } else {
+          return (
+            <Section label={item.name} id={`${selectedTask}_param_${index}`} key={`${selectedTask}_param_${index}`} description={item.description}>
+              <>
+                <input ref={methods.register} type="hidden" id={`params[${index}].name`} name={`params[${index}].name`} value={item.name} />
+                <input ref={methods.register} className="pf-c-form-control" id={`params[${index}].value`} name={`params[${index}].value`} defaultValue={item.value} />
+              </>
+            </Section>
+          );
+        }
+      })
+    ) : (
+      <div className="help-block">{t('SINGLE:MSG_TASKRUN_CREATFORM_DIV2_10')}</div>
     );
-  });
 
-  const onTaskSelect = selectedTaskName => {
-    setSelectedTask(selectedTaskName);
-    getTaskDetails(selectedTaskName);
-  };
+  const workspaces =
+    workspaceList.length > 0 ? (
+      workspaceList.map((item, index) => {
+        return (
+          <Section label={item.name} description={item.description} key={index} id={`workspace_${index}_${item.name}`}>
+            <></>
+          </Section>
+        );
+      })
+    ) : (
+      <div className="help-block">{t('SINGLE:MSG_TASKS_CREATFORM_DIV2_84')}</div>
+    );
+
+  React.useEffect(() => {
+    getTaskDetails(selectedTask);
+  }, [selectedTask]);
 
   return (
     <>
       <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_5')} id="task">
-        <ResourceDropdown name="spec.taskRef.name" placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_5')} resources={[{ kind: TaskModel.kind, namespace: namespace, prop: 'task' }]} onChange={onTaskSelect} type="single" useHookForm />
+        <ResourceDropdown name="spec.taskRef.name" placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_5')} resources={[{ kind: TaskModel.kind, namespace: namespace, prop: 'task' }]} defaultValue="" methods={methods} type="single" useHookForm />
+        {/* <input ref={methods.register} name="spec.taskRef.name" /> */}
       </Section>
-      <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_7')} id="inputresource">
-        <div>{inputs}</div>
-      </Section>
-      <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_9')} id="outputresource">
-        <div>{outputs}</div>
-      </Section>
-      <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_6')} id="taskparameter">
-        <div>{params}</div>
-      </Section>
-      <Section label="워크스페이스" id="workspace">
-        <div>{workspaces}</div>
-      </Section>
+      {selectedTask != '' ? (
+        <>
+          <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_7')} id="inputresource">
+            <div>{inputs}</div>
+          </Section>
+          <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_9')} id="outputresource">
+            <div>{outputs}</div>
+          </Section>
+          <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_6')} id="taskparameter">
+            <div>{params}</div>
+          </Section>
+          <Section label="워크스페이스" id="workspace">
+            <div>{workspaces}</div>
+          </Section>
+        </>
+      ) : null}
       <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_11')} id="timeout" description={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_13')}>
         <div>
           <input ref={methods.register} className="pf-c-form-control" id="time_input" name="spec.timeout" type="number" placeholder="예: 60, 120, 30" /> 분
         </div>
       </Section>
       <Section label={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_14')} id="serviceaccount" description={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_16')} isRequired={true}>
-        <ResourceDropdown name="spec.serviceAccountName" placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_15')} resources={[{ kind: ServiceAccountModel.kind, namespace: namespace, prop: 'serviceaccount' }]} type="single" useHookForm />
+        <ResourceDropdown name="spec.serviceAccountName" methods={methods} placeholder={t('SINGLE:MSG_TASKRUNS_CREATEFORM_DIV2_15')} resources={[{ kind: ServiceAccountModel.kind, namespace: namespace, prop: 'serviceaccount' }]} type="single" useHookForm />
       </Section>
     </>
   );
@@ -213,7 +240,7 @@ export const CreateTaskRun: React.FC<CreateTaskRunProps> = ({ match: { params },
   const formComponent = taskRunFormFactory(params);
   const TaskRunFormComponent = formComponent;
 
-  return <TaskRunFormComponent fixed={{ apiVersion: `${TaskRunModel.apiGroup}/${TaskRunModel.apiVersion}`, kind, metadata: { namespace: params.ns } }} explanation={''} titleVerb="Create" onSubmitCallback={onSubmitCallback} isCreate={true} />;
+  return <TaskRunFormComponent fixed={{ apiVersion: `${TaskRunModel.apiGroup}/${TaskRunModel.apiVersion}`, kind, metadata: { namespace: params.ns } }} explanation={''} titleVerb="Create" onSubmitCallback={onSubmitCallback} isCreate={true} useDefaultForm />;
 };
 
 const changeTimeoutFormat = timeout => {
