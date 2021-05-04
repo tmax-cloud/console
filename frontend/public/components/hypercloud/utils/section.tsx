b4298feb88869060dba052739dd3df943bebf707
gyoungjun_lee <injxgj@gmail.com>
2021-05-04 11:13:52 +0900

    [feat][patch] Section 컴포넌트 children 여러개일 때 로직 변경

--- frontend/public/components/hypercloud/utils/section.tsx
+++ frontend/public/components/hypercloud/utils/section.tsx
@@ -12,7 +12,7 @@ const Node = ({ className, children, description }) => (
 const CombineNodes = (id, label, description, children, isRequired) => {
   // children node 개수에 따라 가로 분할 class 적용
   let isArray = Array.isArray(children);
-  let className = isArray ? `col-md-${12 / children.length}` : 'col-md-12';
+  let className = isArray ? `col-md-${Math.floor(12 / children.length)}` : 'col-md-12';
   return isArray ? children.map(cur => <Node className={className} children={cur} description={description} />) : <Node className={className} children={children} description={description} />;
 };
 
