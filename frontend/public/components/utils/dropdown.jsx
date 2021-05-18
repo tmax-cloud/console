95b3b3ee965f218352b39f38f09bff058645a332
mjsso <minjin_so@tmax.co.kr>
2021-05-18 11:20:54 +0900

    [refactor] 259234 : Pod i18n적용 누락된 부분 적용 진행중(String발행 대기중)

--- frontend/public/components/utils/dropdown.jsx
+++ frontend/public/components/utils/dropdown.jsx
@@ -10,7 +10,7 @@ import { history } from './router';
 import { KebabItems } from './kebab';
 import { ResourceName } from './resource-icon';
 import { useSafetyFirst } from '../safety-first';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, withTranslation } from 'react-i18next';
 
 export class DropdownMixin extends React.PureComponent {
   constructor(props) {
@@ -567,35 +567,39 @@ ActionsMenu.propTypes = {
 
 const containerLabel = container => <ResourceName name={container ? container.name : ''} kind="Container" />;
 
-export class ContainerDropdown extends React.PureComponent {
-  getSpacer(container) {
-    const spacerBefore = new Set();
-    return container ? spacerBefore.add(container.name) : spacerBefore;
-  }
-
-  getHeaders(container, initContainer) {
-    return initContainer
-      ? {
-          [container.name]: 'Containers',
-          [initContainer.name]: 'Init Containers',
-        }
-      : {};
-  }
-
-  render() {
-    const { currentKey, containers, initContainers, onChange } = this.props;
-    if (_.isEmpty(containers) && _.isEmpty(initContainers)) {
-      return null;
+export const ContainerDropdown = withTranslation()(
+  class ContainerDropdown extends React.PureComponent {
+    getSpacer(container) {
+      const spacerBefore = new Set();
+      return container ? spacerBefore.add(container.name) : spacerBefore;
     }
-    const firstInitContainer = _.find(initContainers, { order: 0 });
-    const firstContainer = _.find(containers, { order: 0 });
-    const spacerBefore = this.getSpacer(firstInitContainer);
-    const headerBefore = this.getHeaders(firstContainer, firstInitContainer);
-    const dropdownItems = _.mapValues(_.merge(containers, initContainers), containerLabel);
-    const title = _.get(dropdownItems, currentKey) || containerLabel(firstContainer);
-    return <Dropdown className="btn-group" menuClassName="dropdown-menu--text-wrap" headerBefore={headerBefore} items={dropdownItems} spacerBefore={spacerBefore} title={title} onChange={onChange} selectedKey={currentKey} />;
-  }
-}
+
+    // MJ : String발행되면 적용하기
+    getHeaders(container, initContainer) {
+      const { t } = this.props;
+      return initContainer
+        ? {
+            [container.name]: t('COMMON:MSG_DETAILS_TABDETAILS_CONTAINERS_TABLEHEADER_1'),
+            [initContainer.name]: t('Init Containers'),
+          }
+        : {};
+    }
+
+    render() {
+      const { currentKey, containers, initContainers, onChange } = this.props;
+      if (_.isEmpty(containers) && _.isEmpty(initContainers)) {
+        return null;
+      }
+      const firstInitContainer = _.find(initContainers, { order: 0 });
+      const firstContainer = _.find(containers, { order: 0 });
+      const spacerBefore = this.getSpacer(firstInitContainer);
+      const headerBefore = this.getHeaders(firstContainer, firstInitContainer);
+      const dropdownItems = _.mapValues(_.merge(containers, initContainers), containerLabel);
+      const title = _.get(dropdownItems, currentKey) || containerLabel(firstContainer);
+      return <Dropdown className="btn-group" menuClassName="dropdown-menu--text-wrap" headerBefore={headerBefore} items={dropdownItems} spacerBefore={spacerBefore} title={title} onChange={onChange} selectedKey={currentKey} />;
+    }
+  },
+);
 
 ContainerDropdown.propTypes = {
   containers: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
