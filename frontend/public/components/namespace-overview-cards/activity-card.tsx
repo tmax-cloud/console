1da3857bf640fcfd52343ba0839188bc5d2f6c39
saeyoung_oh <saeyoung_oh@tmax.co.kr>
2021-05-18 14:29:56 +0900

    [refactor] 네임스페이스 개요의 activity card 수정

--- frontend/public/components/namespace-overview-cards/activity-card.tsx
+++ frontend/public/components/namespace-overview-cards/activity-card.tsx
@@ -1,4 +1,59 @@
 import * as React from 'react';
-import { ActivityCard } from '../dashboard/dashboards-page/cluster-dashboard/activity-card';
+import * as _ from 'lodash-es';
+import DashboardCard from '@console/shared/src/components/dashboard/dashboard-card/DashboardCard';
+import DashboardCardBody from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardBody';
+import DashboardCardHeader from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardHeader';
+import DashboardCardLink from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardLink';
+import DashboardCardTitle from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardTitle';
+import ActivityBody, { RecentEventsBody } from '@console/shared/src/components/dashboard/activity-card/ActivityBody';
+import { useTranslation } from 'react-i18next';
+import { EventModel } from '../../models';
+import { EventKind } from '../../module/k8s';
+import { withDashboardResources } from '../dashboard/with-dashboard-resources';
+import { FirehoseResource, FirehoseResult } from '../utils';
+
+let viewEvents = '/k8s/all-namespaces/events';
+let eventsResource: FirehoseResource;
+
+const RecentEvent = withDashboardResources(({ watchK8sResource, stopWatchK8sResource, resources }) => {
+  React.useEffect(() => {
+    watchK8sResource(eventsResource);
+    return () => {
+      stopWatchK8sResource(eventsResource);
+    };
+  }, [watchK8sResource, stopWatchK8sResource]);
+  return <RecentEventsBody events={resources.events as FirehoseResult<EventKind[]>} moreLink={viewEvents} />;
+});
+
+<<<<<<< Updated upstream
+const ActivityCard = props => {
+  const { t } = useTranslation();
+  eventsResource = { isList: true, kind: EventModel.kind, prop: 'events', ...props };
+  viewEvents = `/k8s/ns/${props.namespace}/events`;
+=======
+const ActivityCard: React.FC<{}> = React.memo(props => {
+  const { t } = useTranslation();
+  eventsResource = { isList: true, kind: EventModel.kind, prop: 'events', ...props };
+  // viewEvents = `/k8s/ns/${props.namespace}/events`;
+>>>>>>> Stashed changes
+
+  return (
+    <DashboardCard gradient data-test-id="activity-card">
+      <DashboardCardHeader>
+        <DashboardCardTitle>{t('SINGLE:MSG_OVERVIEW_MAIN_CARDACTIVITY_TITLE_1')}</DashboardCardTitle>
+        <DashboardCardLink to={viewEvents}>{t('SINGLE:MSG_OVERVIEW_MAIN_CARDACTIVITY_ALL_1')}</DashboardCardLink>
+      </DashboardCardHeader>
+      <DashboardCardBody>
+        <ActivityBody className="co-overview-dashboard__activity-body">
+          <RecentEvent />
+        </ActivityBody>
+      </DashboardCardBody>
+    </DashboardCard>
+  );
+<<<<<<< Updated upstream
+};
+=======
+});
+>>>>>>> Stashed changes
 
 export default React.memo(ActivityCard);
