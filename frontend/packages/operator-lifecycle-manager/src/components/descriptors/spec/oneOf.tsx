a1e77487300464f1fe6d417450eaf9780e99c179
gyoungjun_lee <injxgj@gmail.com>
2021-06-17 13:14:11 +0900

    [bugfix] oneOf 컴포넌트 버그 수정

--- frontend/packages/operator-lifecycle-manager/src/components/descriptors/spec/oneOf.tsx
+++ frontend/packages/operator-lifecycle-manager/src/components/descriptors/spec/oneOf.tsx
@@ -13,15 +13,12 @@ export const OneOfFields: React.FC<OneOfFieldsProps> = props => {
   });
 
   const [type, setType] = React.useState('string');
-  const [description, setDescription] = React.useState('');
   React.useEffect(() => {
     if (typeof formData === 'string') {
       setType('string');
-      setDescription(items.description?.['string']);
     } else if (typeof formData !== 'undefined') {
       // string이랑 undefined가 아니면 integer밖에 없음
       setType('integer');
-      setDescription(items.description?.['integer']);
     }
   }, []);
   return (
@@ -30,7 +27,6 @@ export const OneOfFields: React.FC<OneOfFieldsProps> = props => {
         <Dropdown id={uid} key={uid} title={items[type]} selectedKey={type} items={items.title ?? {}} onChange={setType} />
       </div>
       <label className={'control-label '}>{type}</label>
-      <p className="help-block">{description}</p>
       <div className="">{type === 'string' ? <input className="pf-c-form-control" id={uid} key={uid} onChange={({ currentTarget }) => onChange(currentTarget.value)} value={formData} type="text" /> : <input className="pf-c-form-control" id={uid} key={uid} onChange={({ currentTarget }) => onChange(currentTarget.value !== '' ? _.toNumber(currentTarget.value) : '')} value={formData} type="number" />}</div>{' '}
     </>
   );
