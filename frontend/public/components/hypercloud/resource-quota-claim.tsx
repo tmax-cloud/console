50f11d949a181edf87a690ff97f462e0a634a0f1
gyoungjun_lee <injxgj@gmail.com>
2021-06-18 10:22:30 +0900

    [bugfix][patch] IMS263323 RQC 리소스이름으로 상세페이지 접근시 버그

--- frontend/public/components/hypercloud/resource-quota-claim.tsx
+++ frontend/public/components/hypercloud/resource-quota-claim.tsx
@@ -75,7 +75,7 @@ const ResourceQuotaClaimTableRow: RowFunction<K8sClaimResourceKind> = ({ obj: re
         <ResourceLink kind={kind} name={resourcequotaclaims.metadata.name} namespace={resourcequotaclaims.metadata.namespace} title={resourcequotaclaims.metadata.uid} />
       </TableData>
       <TableData className={classNames(tableColumnClasses[1], 'co-break-word')}>
-        <ResourceLink kind="ResourceQuota" name={resourcequotaclaims.resourceName} title={resourcequotaclaims.resourceName} linkTo={resourcequotaclaims.status?.status === 'Approved'} />
+        <ResourceLink kind="ResourceQuota" name={resourcequotaclaims.resourceName} namespace={resourcequotaclaims.metadata.namespace} title={resourcequotaclaims.resourceName} linkTo={resourcequotaclaims.status?.status === 'Approved'} />
       </TableData>
       <TableData className={tableColumnClasses[2]}>
         {resourcequotaclaims?.status?.status === 'Error' ? (
