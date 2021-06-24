368c997cd3b1a3d4dfb6f31e96d8fc943e447f09
mjsso <minjin_so@tmax.co.kr>
2021-06-24 14:19:21 +0900

    [refactor] Add의 Helm Chart 카드뷰, 마스터 LNB의 오퍼레이터 제거

--- frontend/public/components/hypercloud/nav/master-nav.tsx
+++ frontend/public/components/hypercloud/nav/master-nav.tsx
@@ -33,10 +33,10 @@ const MasterNav = () => (
           <NewTabLink name={t('COMMON:MSG_LNB_MENU_99')} type="kibana" />
           <NewTabLink name={t('COMMON:GitLab')} type="git" />
         </NavSection>
-        <NavSection title={t('COMMON:MSG_LNB_MENU_7')}>
+        {/* <NavSection title={t('COMMON:MSG_LNB_MENU_7')}>
           <HrefLink href="/operatorhub" name={t('COMMON:MSG_LNB_MENU_8')} />
           <ResourceNSLink resource='clusterserviceversions' name={t('COMMON:MSG_LNB_MENU_9')} startsWith={['operators.coreos.com', 'clusterserviceversions',]} />
-        </NavSection>
+        </NavSection> */}
         <NavSection title={t('COMMON:MSG_LNB_MENU_22')}>
           <ResourceNSLink resource="pods" name={t('COMMON:MSG_LNB_MENU_23')} />
           <ResourceNSLink resource="deployments" name={t('COMMON:MSG_LNB_MENU_24')} />
