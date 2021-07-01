cd125362a77225a55ae42a3bc175da8d97a94318
jinsoo_youn <jinsoo_youn@tmax.co.kr>
2021-07-01 04:06:33 +0000

    revise build-image.sh for gs

--- build-image.sh
+++ build-image.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -e 
-DOCKER_REGISTRY="jinsnow"
-PRODUCT="console"
-MAJOR_VERSION="5"
+DOCKER_REGISTRY="tmaxcloudck"
+PRODUCT="hypercloud-console"
+MAJOR_VERSION="gs"
 MINOR_VERSION="1"
 PATCH_VERSION="0"
-HOTFIX_VERSION="0"
+HOTFIX_VERSION="4"
 
 #build docker image 
 docker build --rm=true -t ${DOCKER_REGISTRY}/${PRODUCT}:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${HOTFIX_VERSION} -f ./Dockerfile .
