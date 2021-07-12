a22cc1725ddf701f5ee1a1e9b5bd11f7c5726d01
mjsso <minjin_so@tmax.co.kr>
2021-07-12 18:53:35 +0900

    [refactor] [patch] 소유자 정보 가져오는 default path값 metadata.annotations.creator로 수정

--- frontend/public/components/utils/owner-references.tsx
+++ frontend/public/components/utils/owner-references.tsx
@@ -3,20 +3,21 @@ import * as _ from 'lodash-es';
 import { K8sResourceKind, OwnerReference, referenceForOwnerRef } from '../../module/k8s';
 import { ResourceLink } from './resource-link';
 
-export const OwnerReferences: React.FC<OwnerReferencesProps> = ({ resource }) => {
-  const owners = (_.get(resource.metadata, 'ownerReferences') || []).map((o: OwnerReference) => (
-    <ResourceLink
-      key={o.uid}
-      kind={referenceForOwnerRef(o)}
-      name={o.name}
-      namespace={resource.metadata.namespace}
-    />
-  ));
-  return owners.length ? <>{owners}</> : <span className="text-muted">No owner</span>;
+export const OwnerReferences: React.FC<OwnerReferencesProps> = ({ resource, useHcOwnerPath }) => {
+  if (useHcOwnerPath) {
+    // MEMO : HyperCloud에선 owner의 기본 path가 annotation.creator 이다.
+    const owner = _.get(resource.metadata, 'annotations.creator');
+    return !!owner ? <span>{owner}</span> : <span className="text-muted">No owner</span>;
+  } else {
+    // MEMO : 기존 Openshift에서 owner가져오는 방식.
+    const owners = (_.get(resource.metadata, 'ownerReferences') || []).map((o: OwnerReference) => <ResourceLink key={o.uid} kind={referenceForOwnerRef(o)} name={o.name} namespace={resource.metadata.namespace} />);
+    return owners.length ? <>{owners}</> : <span className="text-muted">No owner</span>;
+  }
 };
 
 type OwnerReferencesProps = {
   resource: K8sResourceKind;
+  useHcOwnerPath?: boolean;
 };
 
 OwnerReferences.displayName = 'OwnerReferences';
