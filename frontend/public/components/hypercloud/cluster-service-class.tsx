7ad936d791db01b4e1acfe19bc98c272fd9fb775
mjsso <minjin_so@tmax.co.kr>
2021-07-12 18:54:32 +0900

    [refactor] [patch] 265940 : ServiceCatalog하위메뉴 디테일페이지 소유자 항목 복구

--- frontend/public/components/hypercloud/cluster-service-class.tsx
+++ frontend/public/components/hypercloud/cluster-service-class.tsx
@@ -21,7 +21,7 @@ const ClusterServiceClassDetails: React.FC<ClusterServiceClassDetailsProps> = ({
         <SectionHeading text={t('COMMON:MSG_DETAILS_TABDETAILS_DETAILS_1', { 0: ResourceLabel(clusterServiceClass, t) })} />
         <div className="row">
           <div className="col-md-6">
-            <ResourceSummary resource={clusterServiceClass} showPodSelector={false} showNodeSelector={false} showAnnotations={false} showOwner={false}></ResourceSummary>
+            <ResourceSummary resource={clusterServiceClass} showPodSelector={false} showNodeSelector={false} showAnnotations={false}></ResourceSummary>
           </div>
           <div className="col-md-6">
             <dl className="co-m-pane__details">
