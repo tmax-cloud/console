3df225258c75a32d44b0ec769ad99519e41c3820
gyoungjun_lee <injxgj@gmail.com>
2021-07-19 12:01:51 +0900

    [feat][patch] IMS 259056 네임스페이스 삭제 모달 문구 수정

--- frontend/public/components/modals/delete-namespace-modal.jsx
+++ frontend/public/components/modals/delete-namespace-modal.jsx
@@ -1,70 +1,61 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
+import { ResourceLabel } from '@console/internal/models/hypercloud/resource-plural';
 import { k8sKill } from '../../module/k8s';
 import { createModalLauncher, ModalTitle, ModalBody, ModalSubmitFooter } from '../factory/modal';
 import { history, PromiseComponent } from '../utils';
 import { YellowExclamationTriangleIcon } from '@console/shared';
 
-class DeleteNamespaceModal extends PromiseComponent {
-  constructor(props) {
-    super(props);
-    this.state = Object.assign(this.state, { isTypedNsMatching: false });
-    this._submit = this._submit.bind(this);
-    this._close = props.close.bind(this);
-    this._cancel = props.cancel.bind(this);
-    this._matchTypedNamespace = this._matchTypedNamespace.bind(this);
-  }
+import { withTranslation, Trans } from 'react-i18next';
 
-  _matchTypedNamespace(e) {
-    this.setState({ isTypedNsMatching: e.target.value === this.props.resource.metadata.name });
-  }
+const DeleteNamespaceModal = withTranslation()(
+  class DeleteNamespaceModal extends PromiseComponent {
+    constructor(props) {
+      super(props);
+      this.state = Object.assign(this.state, { isTypedNsMatching: false });
+      this._submit = this._submit.bind(this);
+      this._close = props.close.bind(this);
+      this._cancel = props.cancel.bind(this);
+      this._matchTypedNamespace = this._matchTypedNamespace.bind(this);
+    }
 
-  _submit(event) {
-    event.preventDefault();
-    this.handlePromise(k8sKill(this.props.kind, this.props.resource)).then(() => {
-      this._close();
-      history.push(`/k8s/cluster/${this.props.kind.plural}`);
-    });
-  }
+    _matchTypedNamespace(e) {
+      this.setState({ isTypedNsMatching: e.target.value === this.props.resource.metadata.name });
+    }
 
-  render() {
-    return (
-      <form onSubmit={this._submit} name="form" className="modal-content ">
-        <ModalTitle className="modal-header">
-          <YellowExclamationTriangleIcon className="co-icon-space-r" /> Delete{' '}
-          {this.props.kind.label}?
-        </ModalTitle>
-        <ModalBody>
-          <p>
-            This action cannot be undone. It will destroy all pods, services and other objects in
-            the namespace{' '}
-            <strong className="co-break-word">{this.props.resource.metadata.name}</strong>.
-          </p>
-          <p>
-            Confirm deletion by typing{' '}
-            <strong className="co-break-word">{this.props.resource.metadata.name}</strong> below:
-          </p>
-          <input
-            type="text"
-            className="pf-c-form-control"
-            onKeyUp={this._matchTypedNamespace}
-            placeholder="Enter name"
-            autoFocus={true}
-          />
-        </ModalBody>
-        <ModalSubmitFooter
-          submitText="Delete"
-          submitDisabled={!this.state.isTypedNsMatching}
-          cancel={this._cancel}
-          errorMessage={this.state.errorMessage}
-          inProgress={this.state.inProgress}
-          submitDanger
-        />
-      </form>
-    );
-  }
-}
+    _submit(event) {
+      event.preventDefault();
+      this.handlePromise(k8sKill(this.props.kind, this.props.resource)).then(() => {
+        this._close();
+        history.push(`/k8s/cluster/${this.props.kind.plural}`);
+      });
+    }
+
+    render() {
+      const { t } = this.props;
+      const StrongTextComponent = () => <strong className="co-break-word">{this.props.resource.metadata.name}</strong>;
+      const strongString = <StrongTextComponent key="strongstring" />;
+      return (
+        <form onSubmit={this._submit} name="form" className="modal-content ">
+          <ModalTitle className="modal-header">
+            <YellowExclamationTriangleIcon className="co-icon-space-r" /> {t('COMMON:MSG_MAIN_POPUP_DESCRIPTION_5', { 0: ResourceLabel(this.props.kind, t) })}
+          </ModalTitle>
+          <ModalBody>
+            <p>
+              <Trans i18nKey="COMMON:MSG_MAIN_POPUP_18">{strongString}</Trans>
+            </p>
+            <p>
+              <Trans i18nKey="COMMON:MSG_MAIN_POPUP_20">{strongString}</Trans>
+            </p>
+            <input type="text" className="pf-c-form-control" onKeyUp={this._matchTypedNamespace} placeholder={t('COMMON:MSG_MAIN_POPUP_19')} autoFocus={true} />
+          </ModalBody>
+          <ModalSubmitFooter submitText={t('COMMON:MSG_MAIN_POPUP_COMMIT_2')} submitDisabled={!this.state.isTypedNsMatching} cancel={this._cancel} errorMessage={this.state.errorMessage} inProgress={this.state.inProgress} submitDanger />
+        </form>
+      );
+    }
+  },
+);
 
 DeleteNamespaceModal.propTypes = {
   kind: PropTypes.object,
