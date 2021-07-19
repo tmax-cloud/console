76d7f901ed3491d02639316e0478065162e11bfd
mjsso <minjin_so@tmax.co.kr>
2021-07-19 12:27:27 +0900

    [patch] 259056 : Delete Namespace모달 i18n 적용

--- frontend/public/components/modals/delete-namespace-modal.jsx
+++ frontend/public/components/modals/delete-namespace-modal.jsx
@@ -5,6 +5,7 @@ import { k8sKill } from '../../module/k8s';
 import { createModalLauncher, ModalTitle, ModalBody, ModalSubmitFooter } from '../factory/modal';
 import { history, PromiseComponent } from '../utils';
 import { YellowExclamationTriangleIcon } from '@console/shared';
+import { withTranslation, Trans } from 'react-i18next';
 
 class DeleteNamespaceModal extends PromiseComponent {
   constructor(props) {
@@ -29,38 +30,20 @@ class DeleteNamespaceModal extends PromiseComponent {
   }
 
   render() {
+    const { t } = this.props;
+    const NamespaceName = () => <strong className="co-break-word">{this.props.resource.metadata.name}</strong>;
     return (
       <form onSubmit={this._submit} name="form" className="modal-content ">
         <ModalTitle className="modal-header">
-          <YellowExclamationTriangleIcon className="co-icon-space-r" /> Delete{' '}
-          {this.props.kind.label}?
+          <YellowExclamationTriangleIcon className="co-icon-space-r" />{t('COMMON:MSG_MAIN_POPUP_17')}
         </ModalTitle>
         <ModalBody>
-          <p>
-            This action cannot be undone. It will destroy all pods, services and other objects in
-            the namespace{' '}
-            <strong className="co-break-word">{this.props.resource.metadata.name}</strong>.
+          <p style={{ whiteSpace: 'pre-wrap' }}>
+            <Trans i18nKey="COMMON:MSG_MAIN_POPUP_18">{[<NamespaceName />]}</Trans>
           </p>
-          <p>
-            Confirm deletion by typing{' '}
-            <strong className="co-break-word">{this.props.resource.metadata.name}</strong> below:
-          </p>
-          <input
-            type="text"
-            className="pf-c-form-control"
-            onKeyUp={this._matchTypedNamespace}
-            placeholder="Enter name"
-            autoFocus={true}
-          />
+          <input type="text" className="pf-c-form-control" onKeyUp={this._matchTypedNamespace} placeholder={t('COMMON:MSG_MAIN_POPUP_19')} autoFocus={true} />
         </ModalBody>
-        <ModalSubmitFooter
-          submitText="Delete"
-          submitDisabled={!this.state.isTypedNsMatching}
-          cancel={this._cancel}
-          errorMessage={this.state.errorMessage}
-          inProgress={this.state.inProgress}
-          submitDanger
-        />
+        <ModalSubmitFooter submitText={t('COMMON:MSG_MAIN_POPUP_COMMIT_2')} submitDisabled={!this.state.isTypedNsMatching} cancel={this._cancel} errorMessage={this.state.errorMessage} inProgress={this.state.inProgress} submitDanger />
       </form>
     );
   }
@@ -71,4 +54,4 @@ DeleteNamespaceModal.propTypes = {
   resource: PropTypes.object,
 };
 
-export const deleteNamespaceModal = createModalLauncher(DeleteNamespaceModal);
+export const deleteNamespaceModal = createModalLauncher(withTranslation()(DeleteNamespaceModal));
