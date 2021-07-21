503e519a0094185f35bf6da212a4e74caf3dc193
gyoungjun_lee <injxgj@gmail.com>
2021-07-21 13:58:03 +0900

    [feat] pr comment

--- frontend/packages/dev-console/src/components/topology/data-transforms/data-transformer.ts
+++ frontend/packages/dev-console/src/components/topology/data-transforms/data-transformer.ts
@@ -39,16 +39,14 @@ export const getTrafficConnectors = (trafficData: TrafficData, resources: K8sRes
 };
 
 const deploymentToService = (childServices, typedDataModel, item) => {
-  if (!!item.services) {
-    childServices.forEach(serviceItem => {
-      const { obj: deployment } = item;
-      const { obj: service } = serviceItem;
-      const serviceUid = _.get(service, ['metadata', 'uid']);
-      typedDataModel.topology[serviceUid] = createTopologyNodeData(serviceItem, getComponentType(service.kind), getImageForIconClass(`icon-hc-service`));
-      typedDataModel.graph.nodes.push(getTopologyNodeItem(service, TYPE_WORKLOAD));
-      typedDataModel.graph.edges.push({ id: `${serviceUid}_${deployment.metadata.name}`, type: TYPE_CONNECTS_TO, source: serviceUid, target: deployment.metadata.uid });
-    });
-  }
+  childServices.forEach(serviceItem => {
+    const { obj: deployment } = item;
+    const { obj: service } = serviceItem;
+    const serviceUid = _.get(service, ['metadata', 'uid']);
+    typedDataModel.topology[serviceUid] = createTopologyNodeData(serviceItem, getComponentType(service.kind), getImageForIconClass(`icon-hc-service`));
+    typedDataModel.graph.nodes.push(getTopologyNodeItem(service, TYPE_WORKLOAD));
+    typedDataModel.graph.edges.push({ id: `${serviceUid}_${deployment.metadata.name}`, type: TYPE_CONNECTS_TO, source: serviceUid, target: deployment.metadata.uid });
+  });
 };
 
 const getBaseTopologyDataModel = (resources: TopologyDataResources, allResources: K8sResourceKind[], utils: Function[], transformBy: string[]): TopologyDataModel => {
@@ -70,9 +68,13 @@ const getBaseTopologyDataModel = (resources: TopologyDataResources, allResources
         const uid = _.get(obj, ['metadata', 'uid']);
         typedDataModel.topology[uid] = createTopologyNodeData(item, getComponentType(obj.kind), getImageForIconClass(`icon-hc-pod`));
         switch (key) {
-          case 'deployments':
-            const childServices = transformResourceData['services'](item.services);
-            deploymentToService(childServices, typedDataModel, item);
+          case 'deployments': {
+            if (!!item.services) {
+              const childServices = transformResourceData['services'](item.services);
+              deploymentToService(childServices, typedDataModel, item);
+            }
+            break;
+          }
           case 'replicaSets':
           case 'daemonSets':
           case 'statefulSets': {
