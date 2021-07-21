8e2cc00d6da2a32a5f407303ca527e0a703e3c90
mjsso <minjin_so@tmax.co.kr>
2021-07-21 10:40:20 +0900

    오타 수정

--- frontend/packages/dev-console/src/components/topology/components/hypercloud/groups/Deployment.tsx
+++ frontend/packages/dev-console/src/components/topology/components/hypercloud/groups/Deployment.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Node, observer, WithDndDropProps, WithSelectionProps, WithContextMenuProps } from '@console/topology';
 import { SHOW_GROUPING_HINT_EVENT } from '../../../topology-types';
 import { RegroupHint } from '../../RegroupHint';
-import DamonSetNode from './DamonSetNode';
+import DeploymentNode from './DeploymentNode';
 import DeploymentGroup from './DeploymentGroup';
 
 import './Deployment.scss';
@@ -29,7 +29,7 @@ const ObservedDeployment: React.FC<DeploymentProps> = ({ element, selected, onSe
   }, [dropTarget, canDrop, element, dragRegroupable]);
 
   if (element.isCollapsed()) {
-    return <DamonSetNode element={element} selected={selected} onSelect={onSelect} dndDropRef={dndDropRef} canDrop={canDrop} dropTarget={dropTarget} onContextMenu={onContextMenu} contextMenuOpen={contextMenuOpen} dragging={dragging} />;
+    return <DeploymentNode element={element} selected={selected} onSelect={onSelect} dndDropRef={dndDropRef} canDrop={canDrop} dropTarget={dropTarget} onContextMenu={onContextMenu} contextMenuOpen={contextMenuOpen} dragging={dragging} />;
   }
 
   return <DeploymentGroup element={element} selected={selected} onSelect={onSelect} dndDropRef={dndDropRef} canDrop={canDrop} dropTarget={dropTarget} droppable={droppable} onContextMenu={onContextMenu} contextMenuOpen={contextMenuOpen} dragging={dragging} />;
