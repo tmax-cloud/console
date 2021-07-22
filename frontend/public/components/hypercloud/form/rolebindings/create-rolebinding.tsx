137059406deaf0ccfcd144efbb0e91cf04d8cdfd
Hyowook Park <ccsforever@gmail.com>
2021-07-22 16:01:16 +0900

    [refactor] 주석 정리

--- frontend/public/components/hypercloud/form/rolebindings/create-rolebinding.tsx
+++ frontend/public/components/hypercloud/form/rolebindings/create-rolebinding.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { EditRoleBinding } from '../../../RBAC/bindings';
-import { isCreatePage  } from '../create-form';
+import { isCreatePage } from '../create-form';
 /* MEMO : 기존에 구현된 bindings Page 재사용함. */
 export const CreateRoleBinding = props => {  
   if (!!props.obj && !isCreatePage(props.obj)) {
     return <EditRoleBinding kind={props.obj?.kind} {...props} />;
   }
-  //else return <CreateRoleBinding {...props} isCreate={true} />;
 };
