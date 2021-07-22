5cbb4acbd9a09ef1c5cdae4ac6b8d5c6fc007b6e
Hyowook Park <ccsforever@gmail.com>
2021-07-22 15:59:06 +0900

    [refactor] cluster rolebinding 수정 edit탭 아래로

--- frontend/public/components/hypercloud/form/clusterrolebindings/create-clusterrolebinding.tsx
+++ frontend/public/components/hypercloud/form/clusterrolebindings/create-clusterrolebinding.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { EditRoleBinding } from '../../../RBAC/bindings';
 import { isCreatePage  } from '../create-form';
 /* MEMO : 기존에 구현된 bindings Page 재사용함. */
-export const CreateRoleBinding = props => {  
+export const CreateClusterRoleBinding = props => {  
   if (!!props.obj && !isCreatePage(props.obj)) {
     return <EditRoleBinding kind={props.obj?.kind} {...props} />;
   }
