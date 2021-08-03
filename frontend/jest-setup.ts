ed69e2b0c00f713f530fdeb8546bf6344097e897
mjsso <minjin_so@tmax.co.kr>
2021-08-03 19:43:52 +0900

    [refactor] 테스트코드 render 전에 redux store설정하여 wrapping 해주도록 수정

--- frontend/jest-setup.ts
+++
@@ -1,2 +0,0 @@
-import '@testing-library/jest-dom';
-import 'url-search-params-polyfill';
---
+++ frontend/jest-setup.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import 'url-search-params-polyfill';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '@console/internal/redux';
+
+const render = (ui, options) => {
+  const Wrapper = ({ children }) => {
+    return <Provider store={store}></Provider>;
+  };
+  return rtlRender(ui, { wrapper: Wrapper, ...options });
+};
+
+export * from '@testing-library/react';
+export { render };
