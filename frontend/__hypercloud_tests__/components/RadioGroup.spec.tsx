79f289f86763b90cf4f9f508f7780fd5e7d1196a
mjsso <minjin_so@tmax.co.kr>
2021-08-03 12:20:40 +0900

    [refactor] RadioGroup 테스트코드 react-testing-library 사용하여 다시 작성

--- frontend/__hypercloud_tests__/components/RadioGroup.spec.tsx
+++ frontend/__hypercloud_tests__/components/RadioGroup.spec.tsx
@@ -1,45 +1,81 @@
 import * as React from 'react';
-import { RadioInput, RadioGroup } from '../../public/components/hypercloud/utils/radio';
-import { mount, ReactWrapper } from 'enzyme';
-import { act } from 'react-dom/test-utils';
+import { RadioGroup } from '../../public/components/hypercloud/utils/radio';
+import { cleanup, render, fireEvent, act, screen } from '@testing-library/react';
 import { useForm, FormProvider } from 'react-hook-form';
 
-describe('RadioGroupTestSample', () => {
-  let wrapper: ReactWrapper;
+const resources = [
+  {
+    title: 'Cpu',
+    value: 'cpu',
+  },
+  {
+    title: 'Gpu',
+    value: 'gpu',
+  },
+  {
+    title: 'Memory',
+    value: 'memory',
+  },
+];
 
-  beforeEach(async () => {
-    const resources = [
-      {
-        title: 'Cpu',
-        value: 'cpu',
-      },
-      {
-        title: 'Gpu',
-        value: 'gpu',
-      },
-      {
-        title: 'Memory',
-        value: 'memory',
-      },
-    ];
+const renderRadioGroup = () => {
+  return render(<RadioGroup name="radio-group" items={resources} inline={false} initValue="gpu" />, {
+    wrapper: ({ children }) => {
+      const methods = useForm();
+      return (
+        <FormProvider {...methods}>
+          <form>{children}</form>
+        </FormProvider>
+      );
+    },
+  });
+};
 
-    await act(async () => {
-      wrapper = mount(<RadioGroup name="radio-group" items={resources} inline={false} />, {
-        wrappingComponent: ({ children }) => {
-          const methods = useForm();
-          return (
-            <FormProvider {...methods}>
-              <form>{children}</form>
-            </FormProvider>
-          );
-        },
-      });
-    });
+describe('RadioGroupTest', () => {
+  afterEach(cleanup);
+  beforeEach(() => {});
+
+  it('초기 렌더 스냅샷 테스트입니다.', () => {
+    const { container } = renderRadioGroup();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('radio group items rendered', async () => {
-    expect(wrapper.debug()).toMatchSnapshot();
-    expect(wrapper.find(RadioInput).exists()).toBe(true);
-    expect(wrapper.find(RadioInput).length).toBe(3);
+  it('items props로 받은 item들을 RadioInput아이템으로 렌더링 되어야 합니다.', () => {
+    const { getByDisplayValue } = renderRadioGroup();
+    expect(getByDisplayValue('cpu')).toBeTruthy();
+    expect(getByDisplayValue('gpu')).toBeTruthy();
+    expect(getByDisplayValue('memory')).toBeTruthy();
+  });
+
+  it('initValue로 설정한 input은 default로 체크 돼있어야 합니다.', () => {
+    const { getByDisplayValue } = renderRadioGroup();
+    const gpuRadio = getByDisplayValue('gpu') as HTMLInputElement;
+    expect(gpuRadio.checked).toBeTruthy();
+  });
+
+  it("'Memory' Radio버튼 선택 후 submit 시 { 'radio-group': 'memory'} 형식으로 submit 되어야 합니다.", async () => {
+    const mockSubmit = jest.fn(data => console.log(data));
+
+    const RadioGroupForm = () => {
+      const methods = useForm();
+      return (
+        <FormProvider {...methods}>
+          <form
+            onSubmit={methods.handleSubmit(data => {
+              mockSubmit(data);
+            })}
+          >
+            <RadioGroup name="radio-group" items={resources} inline={false} initValue="gpu" />
+            <button type="submit">Submit</button>
+          </form>
+        </FormProvider>
+      );
+    };
+    
+    render(<RadioGroupForm />);
+    fireEvent.click(screen.getByText('Memory'));
+    await act(async () => fireEvent.submit(screen.getByText('Submit')));
+    expect(mockSubmit).toHaveBeenCalledTimes(1);
+    expect(mockSubmit).toHaveBeenCalledWith({ 'radio-group': 'memory' });
   });
 });
