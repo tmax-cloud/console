23a67a49ea27bb80e840c613a8a7a9ba8235c6d1
yunhee_shin <qlyh8@naver.com>
2021-08-20 10:32:47 +0900

    서비스메시 메뉴 - 리스트 페이지 리팩토링

--- frontend/public/components/hypercloud/sidecar.tsx
+++ frontend/public/components/hypercloud/sidecar.tsx
@@ -1,13 +1,9 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
-import * as classNames from 'classnames';
-import { sortable } from '@patternfly/react-table';
 import { useTranslation } from 'react-i18next';
-import { TFunction } from 'i18next';
-
 import { K8sResourceKind } from '../../module/k8s';
-import { DetailsPage, ListPage, Table, TableRow, TableData, RowFunction } from '../factory';
-import { Kebab, KebabAction, detailsPage, Timestamp, navFactory, ResourceKebab, ResourceLink, ResourceSummary, SectionHeading } from '../utils';
+import { DetailsPage, ListPage } from '../factory';
+import { Kebab, KebabAction, detailsPage, Timestamp, navFactory, ResourceKebab, ResourceLink, ResourceSummary, SectionHeading, TableProps } from '../utils';
 import { Status } from '@console/shared';
 import { SidecarModel } from '../../models';
 import { ResourceLabel } from '../../models/hypercloud/resource-plural';
@@ -16,92 +12,70 @@ export const menuActions: KebabAction[] = [...Kebab.getExtensionsActionsForKind(
 
 const kind = SidecarModel.kind;
 
-const tableColumnClasses = ['', '', classNames('pf-m-hidden', 'pf-m-visible-on-sm', 'pf-u-w-16-on-lg'), Kebab.columnClass];
-
-const SidecarTableHeader = (t?: TFunction) => {
-  return [
+const tableProps: TableProps = {
+  header: [
     {
-      title: t('COMMON:MSG_MAIN_TABLEHEADER_1'),
+      title: 'COMMON:MSG_MAIN_TABLEHEADER_1',
       sortField: 'metadata.name',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[0] },
     },
     {
-      title: t('COMMON:MSG_MAIN_TABLEHEADER_2'),
-      sortFunc: 'metadata.namespace',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[1] },
+      title: 'COMMON:MSG_MAIN_TABLEHEADER_2',
+      sortField: 'metadata.namespace',
     },
     {
-      title: t('COMMON:MSG_MAIN_TABLEHEADER_12'),
+      title: 'COMMON:MSG_MAIN_TABLEHEADER_12',
       sortField: 'metadata.creationTimestamp',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[2] },
     },
     {
       title: '',
-      props: { className: tableColumnClasses[3] },
+      transforms: null,
+      props: { className: Kebab.columnClass },
     },
-  ];
-};
-SidecarTableHeader.displayName = 'SidecarTableHeader';
-
-const SidecarTableRow: RowFunction<K8sResourceKind> = ({ obj: sidecar, index, key, style }) => {
-  return (
-    <TableRow id={sidecar.metadata.uid} index={index} trKey={key} style={style}>
-      <TableData className={tableColumnClasses[0]}>
-        <ResourceLink kind={kind} name={sidecar.metadata.name} namespace={sidecar.metadata.namespace} title={sidecar.metadata.uid} />
-      </TableData>
-      <TableData className={tableColumnClasses[1]}>
-        <Status status={sidecar.metadata.namespace} />
-      </TableData>
-      <TableData className={tableColumnClasses[2]}>
-        <Timestamp timestamp={sidecar.metadata.creationTimestamp} />
-      </TableData>
-      <TableData className={tableColumnClasses[3]}>
-        <ResourceKebab actions={menuActions} kind={kind} resource={sidecar} />
-      </TableData>
-    </TableRow>
-  );
+  ],
+  row: (obj: K8sResourceKind) => [
+    {
+      children: <ResourceLink kind={kind} name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.uid} />,
+    },
+    {
+      children: <Status status={obj.metadata.namespace} />,
+    },
+    {
+      children: <Timestamp timestamp={obj.metadata.creationTimestamp} />,
+    },
+    {
+      className: Kebab.columnClass,
+      children: <ResourceKebab actions={menuActions} kind={kind} resource={obj} />,
+    },
+  ],
 };
 
 const SidecarDetails: React.FC<SidecarDetailsProps> = ({ obj: sidecar }) => {
   const { t } = useTranslation();
-  return <>
-    <div className="co-m-pane__body">
-    <SectionHeading text={t('COMMON:MSG_DETAILS_TABDETAILS_DETAILS_1', {0: ResourceLabel(sidecar, t)})} />
-      <div className="row">
-        <div className="col-lg-6">
-          <ResourceSummary resource={sidecar} />
+  return (
+    <>
+      <div className="co-m-pane__body">
+        <SectionHeading text={t('COMMON:MSG_DETAILS_TABDETAILS_DETAILS_1', { 0: ResourceLabel(sidecar, t) })} />
+        <div className="row">
+          <div className="col-lg-6">
+            <ResourceSummary resource={sidecar} />
+          </div>
         </div>
       </div>
-    </div>
-    <div className="co-m-pane__body">
-    </div>
-  </>;
+      <div className="co-m-pane__body"></div>
+    </>
+  );
 };
 
 const { details, editYaml } = navFactory;
-export const Sidecars: React.FC = props => {
-  const { t } = useTranslation();
-  return <Table {...props} aria-label="Sidecars" Header={SidecarTableHeader.bind(null, t)} Row={SidecarTableRow} virtualize />;
-};
 
-export const SidecarsPage: React.FC<SidecarsPageProps> = props => {
-  const { t } = useTranslation();
-  return <ListPage title={t('COMMON:MSG_LNB_MENU_40')} createButtonText={t('COMMON:MSG_MAIN_CREATEBUTTON_1', { 0: t('COMMON:MSG_LNB_MENU_40') })} canCreate={true} ListComponent={Sidecars} kind={kind} {...props} />;
+export const SidecarsPage: React.FC = props => {
+  return <ListPage canCreate={true} tableProps={tableProps} kind={kind} {...props} />;
 };
 
 export const SidecarsDetailsPage: React.FC<SidecarsDetailsPageProps> = props => <DetailsPage {...props} kind={kind} menuActions={menuActions} pages={[details(detailsPage(SidecarDetails)), editYaml()]} />;
 
 type SidecarDetailsProps = {
   obj: K8sResourceKind;
-};
-
-type SidecarsPageProps = {
-  showTitle?: boolean;
-  namespace?: string;
-  selector?: any;
 };
 
 type SidecarsDetailsPageProps = {
