d109f457ec648465cba5fdef20a78b5ffd812e6c
gyoungjun_lee <injxgj@gmail.com>
2021-08-31 15:03:27 +0900

    [feat] 트레이닝 잡 status reducer 위치 변경 & succeeded 상태 추가

--- frontend/packages/dev-console/src/utils/hc-status-reducers.ts
+++ frontend/packages/dev-console/src/utils/hc-status-reducers.ts
@@ -63,6 +63,21 @@ export const NotebookStatusReducer = notebook => {
   return !!notebook.status ? notebook.status.conditions?.[0]?.type : NO_STATUS || '';
 };
 
+export const TrainingJobStatusReducer = tj => {
+  if (!tj.status) {
+    return NO_STATUS;
+  }
+
+  const len = tj.status.conditions.length;
+  for (let i = len - 1; i >= 0; i--) {
+    if (tj.status.conditions[i].status) {
+      return tj.status.conditions[i].type;
+    } else {
+      return NO_STATUS;
+    }
+  }
+};
+
 export const ExperimentStatusReducer = experiment => {
   if (experiment.status) {
     const conditions = experiment.status?.conditions;
