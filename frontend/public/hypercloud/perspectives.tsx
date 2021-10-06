7c03769a22a765dc12395fcaea68d85d2ce8b6fd
eunkyeng_shin@tmax.co.kr <eunkyeng_shin@tmax.co.kr>
2021-10-06 11:29:09 +0900

    [feat] perspective 타입에 selectedIcon 추가 및 선택시 해당 icon으로 적용되도록 수정

--- frontend/public/hypercloud/perspectives.tsx
+++ frontend/public/hypercloud/perspectives.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import { CogsIcon } from '@patternfly/react-icons';
 import { Perspective } from '@console/plugin-sdk';
 import { TFunction } from 'i18next';
-import * as MultiClusterIcon from '@console/internal/imgs/hypercloud/lnb/muti_cluster.svg';
+import * as MultiClusterIcon from '@console/internal/imgs/hypercloud/lnb/multi_cluster.svg';
 import * as MasterClusterIcon from '@console/internal/imgs/hypercloud/lnb/master_cluster.svg';
 import * as SingleClusterIcon from '@console/internal/imgs/hypercloud/lnb/single_cluster.svg';
 import * as DeveloperIcon from '@console/internal/imgs/hypercloud/lnb/developer.svg';
-
+import * as SelectedMultiClusterIcon from '@console/internal/imgs/hypercloud/lnb/filled/multi_cluster_filled.svg';
+import * as SelectedMasterClusterIcon from '@console/internal/imgs/hypercloud/lnb/filled/master_cluster_filled.svg';
+import * as SelectedSingleClusterIcon from '@console/internal/imgs/hypercloud/lnb/filled/single_cluster_filled.svg';
+import * as SelectedDeveloperIcon from '@console/internal/imgs/hypercloud/lnb/filled/developer_filled.svg'
 
 export enum PerspectiveType {
   MASTER = 'MASTER',
@@ -22,77 +25,83 @@ export enum PerspectiveType {
 export const getPerspectives: (t?: TFunction) => Perspective[] = (t?: TFunction) => {
   const perspectives: Perspective[] = window.SERVER_FLAGS.McMode
     ? [
-        {
-          type: 'Perspective',
-          properties: {
-            id: PerspectiveType.MULTI,
-            name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_2') : 'Multi-Cluster',
-            icon: <img src={MultiClusterIcon} className="font-icon co-console-dropdowntoggle-icon"/>,
-            default: true,
-            getLandingPageURL: flags => '/k8s/all-namespaces/clustermanagers',
-            getK8sLandingPageURL: flags => '/k8s/all-namespaces/clustermanagers',
-            getImportRedirectURL: project => `/k8s/all-namespaces/projects/${project}/workloads`,
-          },
+      {
+        type: 'Perspective',
+        properties: {
+          id: PerspectiveType.MULTI,
+          name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_2') : 'Multi-Cluster',
+          icon: <img src={MultiClusterIcon} className="font-icon co-console-dropdowntoggle-icon" />,
+          selectedIcon: <img src={SelectedMultiClusterIcon} className="font-icon" />,
+          default: true,
+          getLandingPageURL: flags => '/k8s/all-namespaces/clustermanagers',
+          getK8sLandingPageURL: flags => '/k8s/all-namespaces/clustermanagers',
+          getImportRedirectURL: project => `/k8s/all-namespaces/projects/${project}/workloads`,
         },
-        {
-          type: 'Perspective',
-          properties: {
-            id: PerspectiveType.MASTER,
-            name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_3') : 'Master-Cluster',
-            icon: <img src={MasterClusterIcon} className="font-icon co-console-dropdowntoggle-icon" />,
-            getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
-            getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
-            getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
-          },
+      },
+      {
+        type: 'Perspective',
+        properties: {
+          id: PerspectiveType.MASTER,
+          name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_3') : 'Master-Cluster',
+          icon: <img src={MasterClusterIcon} className="font-icon co-console-dropdowntoggle-icon" />,
+          selectedIcon: <img src={SelectedMasterClusterIcon} className="font-icon" />,
+          getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
+          getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
+          getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
         },
-        {
-          type: 'Perspective',
-          properties: {
-            id: PerspectiveType.SINGLE,
-            name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_1') : 'Single-Cluster',
-            icon: <img src={SingleClusterIcon} className="font-icon co-console-dropdowntoggle-icon"/>,
-            getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/single/dashboards' : '/welcome'),
-            getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/single/dashboards' : '/welcome'),
-            getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
-          },
+      },
+      {
+        type: 'Perspective',
+        properties: {
+          id: PerspectiveType.SINGLE,
+          name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_1') : 'Single-Cluster',
+          icon: <img src={SingleClusterIcon} className="font-icon co-console-dropdowntoggle-icon" />,
+          selectedIcon: <img src={SelectedSingleClusterIcon} className="font-icon" />,
+          getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/single/dashboards' : '/welcome'),
+          getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/single/dashboards' : '/welcome'),
+          getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
         },
-        {
-          type: 'Perspective',
-          properties: {
-            id: PerspectiveType.DEVELOPER,
-            name: t ? t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_RADIOBUTTON_2') : 'Developer', // 임시. 스트링 나오면 재적용 필요
-            icon: <img src={DeveloperIcon} className="font-icon co-console-dropdowntoggle-icon"/>,
-            getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
-            getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
-            getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
-          },
+      },
+      {
+        type: 'Perspective',
+        properties: {
+          id: PerspectiveType.DEVELOPER,
+          name: t ? t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_RADIOBUTTON_2') : 'Developer', // 임시. 스트링 나오면 재적용 필요
+          icon: <img src={DeveloperIcon} className="font-icon co-console-dropdowntoggle-icon" />,
+          selectedIcon: <img src={SelectedDeveloperIcon} className="font-icon" />,
+          getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
+          getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
+          getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
         },
-      ]
+      },
+    ]
     : [
-        {
-          type: 'Perspective',
-          properties: {
-            id: PerspectiveType.MASTER,
-            name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_3') : 'Master-Cluster',
-            icon: <img src={MasterClusterIcon} className="font-icon co-console-dropdowntoggle-icon"/>,
-            default: true,
-            getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
-            getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
-            getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
-          },
+      {
+        type: 'Perspective',
+        properties: {
+          id: PerspectiveType.MASTER,
+          name: t ? t('COMMON:MSG_LNB_MENU_CONSOLE_LIST_3') : 'Master-Cluster',
+          icon: <img src={MasterClusterIcon} className="font-icon co-console-dropdowntoggle-icon" />,
+          selectedIcon: <img src={SelectedMasterClusterIcon} className="font-icon" />,
+          default: true,
+          getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
+          getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/master/dashboards' : '/welcome'),
+          getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
         },
-        {
-          type: 'Perspective',
-          properties: {
-            id: PerspectiveType.DEVELOPER,
-            name: t ? t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_RADIOBUTTON_2') : 'Developer', // 임시. 스트링 나오면 재적용 필요
-            icon: <img src={DeveloperIcon} className="font-icon co-console-dropdowntoggle-icon"/>,
-            getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
-            getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
-            getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
-          },
+      },
+      {
+        type: 'Perspective',
+        properties: {
+          id: PerspectiveType.DEVELOPER,
+          name: t ? t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_RADIOBUTTON_2') : 'Developer', // 임시. 스트링 나오면 재적용 필요
+          icon: <img src={DeveloperIcon} className="font-icon co-console-dropdowntoggle-icon" />,
+          selectedIcon: <img src={SelectedDeveloperIcon} className="font-icon" />,
+          getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
+          getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/developer/add' : '/welcome'),
+          getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
         },
-      ];
+      },
+    ];
 
   if (window.SERVER_FLAGS.showCustomPerspective === undefined || window.SERVER_FLAGS.showCustomPerspective === true) {
     perspectives.push({
@@ -100,7 +109,8 @@ export const getPerspectives: (t?: TFunction) => Perspective[] = (t?: TFunction)
       properties: {
         id: PerspectiveType.CUSTOM,
         name: 'Custom', // 임시. 스트링 나오면 재적용 필요
-        icon: <CogsIcon className="font-icon co-console-dropdowntoggle-icon"/>,
+        icon: <CogsIcon className="font-icon co-console-dropdowntoggle-icon" />,
+        selectedIcon: <CogsIcon className="font-icon " />,
         getLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/custom/add' : '/welcome'),
         getK8sLandingPageURL: flags => (localStorage.getItem('flag/first-time-login') ? '/custom/add' : '/welcome'),
         getImportRedirectURL: project => `/k8s/cluster/projects/${project}/workloads`,
