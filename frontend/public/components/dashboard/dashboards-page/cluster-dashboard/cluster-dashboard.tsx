67574be7843857e8280c8fbd86108e441a944643
min9120 <a9120a@gmail.com>
2022-01-25 16:49:33 +0900

    [feat] 싱글 클러스터일때 홈/개요 상세, 주요 리소스 및 클레임 안보이도록 수정

--- frontend/public/components/dashboard/dashboards-page/cluster-dashboard/cluster-dashboard.tsx
+++ frontend/public/components/dashboard/dashboards-page/cluster-dashboard/cluster-dashboard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import Dashboard from '@console/shared/src/components/dashboard/Dashboard';
 import DashboardGrid from '@console/shared/src/components/dashboard/DashboardGrid';
+import { getActivePerspective, getActiveCluster } from '@console/internal/actions/ui';
 import { StatusCard } from './status-card';
 import { DetailsCard } from './details-card';
 import { InventoryCard } from './inventory-card';
@@ -15,11 +16,15 @@ import { AccessDenied } from '../../../utils';
 import { k8sCreate } from '@console/internal/module/k8s';
 import { SelfSubjectAccessReviewModel } from '../../../../models';
 import { useTranslation } from 'react-i18next';
+import { isSingleClusterPerspective } from '../../../../hypercloud/perspectives';
 
+
+// const isSingleCluster = isSingleClusterPerspective()
+/*
 const mainCards = [{ Card: StatusCard }, { Card: UtilizationCard }];
 const leftCards = [{ Card: DetailsCard }];
-const rightCards = [{ Card: InventoryCard }, { Card: ActivityCard }];
-
+const rightCards = isSingleClusterPerspective() === true ? [{ Card: ActivityCard }] : [{ Card: InventoryCard }, { Card: ActivityCard }];
+*/
 // const mainCards = [];
 // const leftCards = [];
 // const rightCards = [{ Card: ActivityCard }];
@@ -36,6 +41,7 @@ export const ClusterDashboard: React.FC<{}> = () => {
   const { t } = useTranslation();
 
   const [response, setResponse] = React.useState();
+  const [isSingleCluster, setSingleCluster] = React.useState(isSingleClusterPerspective());
   const payload = {
     spec: {
       resourceAttributes: {
@@ -52,8 +58,15 @@ export const ClusterDashboard: React.FC<{}> = () => {
     };
     checkAuth();
   }, []);
+  React.useEffect(() => {
+    setSingleCluster(isSingleClusterPerspective());
+  }, [getActivePerspective()]);
 
-  const dashboard = <DashboardGrid mainCards={mainCards} leftCards={leftCards} rightCards={rightCards} />;
+  const mainCards = [{ Card: StatusCard }, { Card: UtilizationCard }];
+  const leftCards = [{ Card: DetailsCard }];
+  const rightCards = isSingleCluster ? [{ Card: ActivityCard }] : [{ Card: InventoryCard }, { Card: ActivityCard }];
+
+  const dashboard = <DashboardGrid mainCards={mainCards} leftCards={leftCards} rightCards={rightCards} isSingleCluster={isSingleCluster} />;
   const loading = <div></div>;
   const error = <AccessDenied message={t('COMMON:MSG_COMMON_ERROR_MESSAGE_27')} />;
   let content;
