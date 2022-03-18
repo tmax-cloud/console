fa9e3486540e1354ba73f86d22c937973e359365
gyoungjun_lee <injxgj@gmail.com>
2022-03-18 11:26:25 +0900

    [feat] Topology 관련 코드 정리

--- frontend/packages/console-shared/src/utils/transformResourceData.ts
+++ frontend/packages/console-shared/src/utils/transformResourceData.ts
@@ -24,15 +24,15 @@ export class TransformResourceData {
 
   public createDaemonSetItems = (daemonSets: K8sResourceKind[], operatorsFilter?: boolean): OverviewItem[] => createDaemonSetItems(daemonSets, this.resources, this.installedOperators, this.utils, operatorsFilter);
 
-  public createStatefulSetItems = (statefulSets: K8sResourceKind[], operatorsFilter?: boolean): OverviewItem[] => createStatefulSetItems(statefulSets, this.resources, this.installedOperators, this.utils, operatorsFilter);
+  public createStatefulSetItems = (statefulSets: K8sResourceKind[]): OverviewItem[] => createStatefulSetItems(statefulSets, this.resources, this.installedOperators);
 
-  public createReplicaSetItems = (replicaSets: K8sResourceKind[], operatorsFilter?: boolean): OverviewItem[] => createReplicaSetItems(replicaSets, this.resources, this.installedOperators, this.utils, operatorsFilter);
+  public createReplicaSetItems = (replicaSets: K8sResourceKind[]): OverviewItem[] => createReplicaSetItems(replicaSets, this.resources, this.installedOperators);
 
-  public createServiceItems = (services: K8sResourceKind[], operatorsFilter?: boolean): OverviewItem[] => createServiceItems(services, this.resources, this.installedOperators, this.utils, operatorsFilter);
+  public createServiceItems = (services: K8sResourceKind[]): OverviewItem[] => createServiceItems(services);
 
   public createIngressItems = (ingresses: K8sResourceKind[]): OverviewItem[] => createIngressItems(ingresses);
 
-  public createPersistentVolumeClaimItems = (persistentVolumeClaims: K8sResourceKind[], operatorsFilter?: boolean): OverviewItem[] => createPersistentVolumeClaimItems(persistentVolumeClaims, this.resources, this.installedOperators, this.utils, operatorsFilter);
+  public createPersistentVolumeClaimItems = (persistentVolumeClaims: K8sResourceKind[]): OverviewItem[] => createPersistentVolumeClaimItems(persistentVolumeClaims);
 
   public createPodItems = (): OverviewItem[] => createPodItems(this.resources);
 
