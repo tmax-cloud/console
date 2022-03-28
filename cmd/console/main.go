21e0d72e18d643876c2b7f38b01d0018aa285a80
jinsoo-youn <jinsoo_youn@tmax.co.kr>
2022-03-28 22:19:21 +0900

    [feat] consider backward compatability

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -41,7 +41,7 @@ func main() {
 
 var (
 	servingInfo = &ServingInfo{}
-	app = server.NewAppConfig()
+	app         = server.NewAppConfig()
 
 	rootCmd = &cobra.Command{
 		Use:   "console",
@@ -56,23 +56,49 @@ Finally, we provide a proxy function for querying the kubernetes resource API`,
 		Run: func(cmd *cobra.Command, args []string) {
 			fs := cmd.Flags()
 
+			// backward compatibility
+			if fBaseAddress, _ := fs.GetString("base-address"); fBaseAddress != "" {
+				servingInfo.BaseAddress = fBaseAddress
+			}
+			if fListen, _ := fs.GetString("listen"); fListen != "" {
+				servingInfo.Listen = fListen
+			}
+			if fKeycloakRealm, _ := fs.GetString("keycloak-realm"); fKeycloakRealm != "" {
+				app.KeycloakRealm = fKeycloakRealm
+			}
+			if fKeycloakAuthURL, _ := fs.GetString("keycloak-auth-url"); fKeycloakAuthURL != "" {
+				app.KeycloakAuthURL = fKeycloakAuthURL
+			}
+			if fKeycloakClientId, _ := fs.GetString("keycloak-client-id"); fKeycloakClientId != "" {
+				app.KeycloakClientId = fKeycloakClientId
+			}
+			if fPublicDir, _ := fs.GetString("public-dir"); fPublicDir != "" {
+				app.PublicDir = fPublicDir
+			}
+			if fMcMode, _ := fs.GetBool("mc-mode"); fMcMode == true {
+				app.McMode = fMcMode
+			}
+			if fCustomProductName, _ := fs.GetString("custom-product-name"); fCustomProductName != "" {
+				app.CustomProductName = fCustomProductName
+			}
+
 			var (
 				k8sApi, _ = fs.GetString("clusterInfo.kubeAPIServerURL")
-				token, _ = fs.GetString("clusterInfo.kubeToken")
+				token, _  = fs.GetString("clusterInfo.kubeToken")
 			)
-			k8sHandler := server.NewK8sHandlerConfig(k8sApi,token)
+			k8sHandler := server.NewK8sHandlerConfig(k8sApi, token)
 
 			var logger kitlog.Logger
 			logger = kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(os.Stderr))
 			logger = kitlog.With(logger, "ts", kitlog.DefaultTimestampUTC)
 
-			app.AddLogger(kitlog.With(logger, "component","App"))
-			k8sHandler.AddLogger(kitlog.With(logger,"component","k8sHandler"))
+			app.AddLogger(kitlog.With(logger, "component", "App"))
+			k8sHandler.AddLogger(kitlog.With(logger, "component", "k8sHandler"))
 
-			httpHandler := server.NewServer(app,k8sHandler)
+			httpHandler := server.NewServer(app, k8sHandler)
 
-			listenURL := console.ValidateFlagIsURL("listen",servingInfo.Listen)
-			baseURL := console.ValidateFlagIsURL("baseAddress",servingInfo.BaseAddress)
+			listenURL := console.ValidateFlagIsURL("listen", servingInfo.Listen)
+			baseURL := console.ValidateFlagIsURL("baseAddress", servingInfo.BaseAddress)
 			switch listenURL.Scheme {
 			case "http":
 			case "https":
@@ -82,7 +108,7 @@ Finally, we provide a proxy function for querying the kubernetes resource API`,
 				console.FlagFatalf("listen", "scheme must be one of: http, https")
 			}
 			httpsrv := &http.Server{
-				Addr: listenURL.Host,
+				Addr:    listenURL.Host,
 				Handler: httpHandler,
 				// Disable HTTP/2, which breaks WebSockets.
 				TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
@@ -91,15 +117,15 @@ Finally, we provide a proxy function for querying the kubernetes resource API`,
 			//Run the server
 			go func() {
 				if listenURL.Scheme == "https" {
-					err := httpsrv.ListenAndServeTLS(servingInfo.CertFile,servingInfo.KeyFile)
+					err := httpsrv.ListenAndServeTLS(servingInfo.CertFile, servingInfo.KeyFile)
 					if err != nil && err != http.ErrServerClosed {
-						logger.Log("Error",err)
+						logger.Log("Error", err)
 						os.Exit(1)
 					}
 				} else {
 					err := httpsrv.ListenAndServe()
 					if err != nil && err != http.ErrServerClosed {
-						logger.Log("Error",err)
+						logger.Log("Error", err)
 						os.Exit(1)
 					}
 				}
@@ -108,24 +134,24 @@ Finally, we provide a proxy function for querying the kubernetes resource API`,
 			redirectServer := &http.Server{}
 			if servingInfo.RedirectPort != 0 {
 				mux := http.NewServeMux()
-				mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+				mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 					redirectURL := &url.URL{
-						Scheme: baseURL.Scheme,
-						Host: baseURL.Host,
+						Scheme:   baseURL.Scheme,
+						Host:     baseURL.Host,
 						RawQuery: r.URL.RawQuery,
-						Path: r.URL.Path,
+						Path:     r.URL.Path,
 					}
 					http.Redirect(w, r, redirectURL.String(), http.StatusMovedPermanently)
 				})
 				redirectPort := fmt.Sprintf(":%d", servingInfo.RedirectPort)
 				redirectServer = &http.Server{
-					Addr: redirectPort,
+					Addr:    redirectPort,
 					Handler: mux,
 				}
 				go func() {
 					err := redirectServer.ListenAndServe()
 					if err != nil && err != http.ErrServerClosed {
-						logger.Log("Error",err)
+						logger.Log("Error", err)
 						os.Exit(1)
 					}
 				}()
@@ -137,30 +163,30 @@ Finally, we provide a proxy function for querying the kubernetes resource API`,
 			go func() {
 				<-sig
 
-				logger.Log("msg","Shutdown signal received")
+				logger.Log("msg", "Shutdown signal received")
 				// Shutdown signal with grace period of 30 seconds
 				shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
 
 				go func() {
 					<-shutdownCtx.Done()
 					if shutdownCtx.Err() == context.DeadlineExceeded {
-						logger.Log("Error","graceful shutdown timed out.. forcing exit.")
+						logger.Log("Error", "graceful shutdown timed out.. forcing exit.")
 						os.Exit(1)
 					}
 				}()
 
 				// Trigger graceful shutdown
-				logger.Log("msg","graceful Shutdown httpsrv")
+				logger.Log("msg", "graceful Shutdown httpsrv")
 				err := httpsrv.Shutdown(shutdownCtx)
 				if err != nil {
-					logger.Log("Error",err)
+					logger.Log("Error", err)
 					os.Exit(1)
 				}
-				logger.Log("msg","graceful Shutdown redirectServer")
+				logger.Log("msg", "graceful Shutdown redirectServer")
 				if servingInfo.RedirectPort != 0 {
 					err := redirectServer.Shutdown(shutdownCtx)
 					if err != nil {
-						logger.Log("Error",err)
+						logger.Log("Error", err)
 						os.Exit(1)
 					}
 				}
@@ -194,8 +220,18 @@ func NewConsoleCommand() *cobra.Command {
 	rootCmd.PersistentFlags().StringVar(&app.CustomProductName, "app.customProductName", "hypercloud", "prduct name for console | default hypercloud")
 
 	//rootCmd.PersistentFlags().StringVar(&k8sHandler.KubeAPIServerURL, "k8sHandler.kubeAPIServerURL", "kubernetes.default.svc", "kube api server hostname")
-	rootCmd.PersistentFlags().String("clusterInfo.kubeAPIServerURL","https://172.23.4.201:6443", "kube api server hostname")
-	rootCmd.PersistentFlags().String("clusterInfo.kubeToken","","kubernetes token for API")
+	rootCmd.PersistentFlags().String("clusterInfo.kubeAPIServerURL", "https://kubernetes.default.svc", "kube api server hostname")
+	rootCmd.PersistentFlags().String("clusterInfo.kubeToken", "", "kubernetes token for API")
+
+	rootCmd.PersistentFlags().String("base-address", "", "backward compatibility for servingInfo.baseAddress")
+	rootCmd.PersistentFlags().String("listen", "", "backward compatibility for servingInfo.listen")
+	rootCmd.PersistentFlags().String("keycloak-realm", "", "backward compatibility for app.keycloakRealm")
+	rootCmd.PersistentFlags().String("keycloak-auth-url", "", "backward compatibility for app.keycloakAuthUrl")
+	rootCmd.PersistentFlags().String("keycloak-client-id", "", "backward compatibility for app.keycloakClientId")
+	rootCmd.PersistentFlags().Bool("mc-mode", false, "backward compatibility for app.mcMode")
+	rootCmd.PersistentFlags().String("public-dir", "", "backward compatibility for app.publicDir")
+	rootCmd.PersistentFlags().String("custom-product-name", "", "backward compatibility for app.customProductName")
+
 	return rootCmd
 }
 
