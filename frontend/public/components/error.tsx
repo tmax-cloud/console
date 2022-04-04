79782ab17d95c01908d467da8b69c8b3efddf937
yunhee_shin <qlyh8@naver.com>
2022-04-04 18:47:05 +0900

    [feat][patch] 404 에러 페이지 국문화 적용 및 클러스터 접근 제한 에러 페이지 추가

--- frontend/public/components/error.tsx
+++ frontend/public/components/error.tsx
@@ -1,35 +1,39 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 import { ErrorBoundaryFallbackProps } from '@console/shared/src/components/error/error-boundary';
 import { CopyToClipboard, getQueryArgument, PageHeading, ExpandCollapse } from './utils';
+import * as img404 from '../imgs/hypercloud/img_404.svg';
 
-// User messages for error_types returned in auth.go
-const messages = {
-  auth: {
-    /* eslint-disable camelcase */
-    oauth_error: 'There was an error generating OAuth client from OIDC client.',
-    login_state_error: 'There was an error generating login state.',
-    cookie_error: 'There was an error setting login state cookie',
-    missing_code: 'Auth code is missing in query param.',
-    missing_state: 'There was an error parsing your state cookie',
-    invalid_code: 'There was an error logging you in. Please log out and try again.',
-    invalid_state: 'There was an error verifying your session. Please log out and try again.',
-    logout_error: 'There was an error logging you out. Please try again.',
-    /* eslint-enable camelcase */
-    default:
-      'There was an authentication error with the system. Please try again or contact support.',
-  },
+const getMessage = (type: string, id: string): string => {
+  // User messages for error_types returned in auth.go
+  const messages = {
+    auth: {
+      /* eslint-disable camelcase */
+      oauth_error: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_9'),
+      login_state_error: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_8'),
+      cookie_error: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_13'),
+      missing_code: 'Auth code is missing in query param.',
+      missing_state: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_12'),
+      invalid_code: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_10'),
+      invalid_state: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_14'),
+      logout_error: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_11'),
+      /* eslint-enable camelcase */
+      default: i18next.t('COMMON:MSG_COMMON_ERROR_MESSAGE_1'),
+    },
+  };
+  return _.get(messages, `${type}.${id}`) || _.get(messages, `${type}.default`) || '';
 };
 
-const getMessage = (type, id) =>
-  _.get(messages, `${type}.${id}`) || _.get(messages, `${type}.default`) || '';
 const urlMessage = () => {
   const type = getQueryArgument('error_type');
   const error = getQueryArgument('error');
   return type && error ? getMessage(type, error) : '';
 };
+
 const getErrMessage = () => {
   const msg = getQueryArgument('error_msg');
   if (msg) {
@@ -43,44 +47,58 @@ const getErrMessage = () => {
   return '';
 };
 
-const ErrorComponent: React.SFC<ErrorComponentProps> = ({ title, message, errMessage }) => (
-  <>
-    <PageHeading detail={true} title="Error" />
-    <div className="co-m-pane__body" data-test-id="error-page">
-      <h1 className="co-m-pane__heading co-m-pane__heading--center">{title}</h1>
-      {message && <div className="text-center">{message}</div>}
-      {errMessage && <div className="text-center text-muted">{errMessage}</div>}
+const ErrorComponent: React.SFC<ErrorComponentProps> = ({ title, message, errMessage, img }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <PageHeading title={t('COMMON:MSG_COMMON_ERROR_MESSAGE_5')} detail />
+      <div className="co-m-pane__body" data-test-id="error-page">
+        <img className="co-m-pane__heading-img" src={img} />
+        <h1 className="co-m-pane__heading co-m-pane__heading--center co-m-pane__heading-error-h1">{title}</h1>
+        {message && <div className="text-center">{message}</div>}
+        {errMessage && <div className="text-center text-muted">{errMessage}</div>}
+      </div>
+    </>
+  );
+};
+
+export const ErrorPage: React.SFC<ErrorPageProps> = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <Helmet>
+        <title>{t('COMMON:MSG_COMMON_ERROR_MESSAGE_5')}</title>
+      </Helmet>
+      <ErrorComponent title={t('COMMON:MSG_COMMON_ERROR_MESSAGE_6')} message={urlMessage()} errMessage={getErrMessage()} />
     </div>
-  </>
-);
+  );
+};
 
-export const ErrorPage: React.SFC<ErrorPageProps> = () => (
-  <div>
-    <Helmet>
-      <title>Error</title>
-    </Helmet>
-    <ErrorComponent
-      title="Oh no! Something went wrong."
-      message={urlMessage()}
-      errMessage={getErrMessage()}
-    />
-  </div>
-);
+export const ErrorPage404: React.SFC<ErrorPage404Props> = props => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <Helmet>
+        <title>{t('COMMON:MSG_COMMON_ERROR_MESSAGE_7')}</title>
+      </Helmet>
+      <ErrorComponent title={t('COMMON:MSG_COMMON_ERROR_MESSAGE_45')} message={props.message || t('COMMON:MSG_COMMON_ERROR_MESSAGE_46')} errMessage={props.errMessage} img={img404} />
+    </div>
+  );
+};
 
-export const ErrorPage404: React.SFC<ErrorPage404Props> = (props) => (
-  <div>
-    <Helmet>
-      <title>Page Not Found (404)</title>
-    </Helmet>
-    <ErrorComponent
-      title="404: Page Not Found"
-      message={props.message}
-      errMessage={props.errMessage}
-    />
-  </div>
-);
+export const ErrorPageClusterPermissionDenied: React.SFC<ErrorPageClusterPermissionDeniedProps> = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <Helmet>
+        <title>{t('COMMON:MSG_COMMON_ERROR_MESSAGE_5')}</title>
+      </Helmet>
+      <ErrorComponent title={t('COMMON:MSG_COMMON_ERROR_MESSAGE_24')} message={t('COMMON:MSG_COMMON_ERROR_MESSAGE_44')} img={img404} />
+    </div>
+  );
+};
 
-export const ErrorBoundaryFallback: React.SFC<ErrorBoundaryFallbackProps> = (props) => (
+export const ErrorBoundaryFallback: React.SFC<ErrorBoundaryFallbackProps> = props => (
   <div className="co-m-pane__body">
     <h1 className="co-m-pane__heading co-m-pane__heading--center">Oh no! Something went wrong.</h1>
     <ExpandCollapse textCollapsed="Show Details" textExpanded="Hide Details">
@@ -109,7 +127,9 @@ export type ErrorComponentProps = {
   title: string;
   message?: string;
   errMessage?: string;
+  img?: any;
 };
 
 export type ErrorPageProps = {};
 export type ErrorPage404Props = Omit<ErrorComponentProps, 'title'>;
+export type ErrorPageClusterPermissionDeniedProps = {};
