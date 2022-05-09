1ddc25f458f72bafa1d6ac23fdbb77389b6ddb99
Hyowook Park <hyowook_park@tmax.co.kr>
2022-05-09 15:05:39 +0900

    [feat][patch][IMS]281954 호스트 > 노드 탭에서 데이터 정렬 불가 현상

--- frontend/public/actions/ui.ts
+++ frontend/public/actions/ui.ts
@@ -122,7 +122,8 @@ export const getPodMetric = (pod: PodKind, metric: string): number => {
 
 export const getNodeMetric = (node: NodeKind, metric: string): number => {
   const metrics = store.getState().UI.getIn(['metrics', 'node']);
-  return metrics?.[metric]?.[node.metadata.name] ?? 0;
+  const nodeAddress = (node as any).status?.addresses?.find(addr => addr.type === 'InternalIP')?.address;
+  return metrics?.[metric]?.[node.metadata.name] ?? metrics?.[metric]?.[`${nodeAddress}:9100`] ?? 0;
 };
 
 export const formatNamespaceRoute = (activeNamespace, originalPath, location?) => {
