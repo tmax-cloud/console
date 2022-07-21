3ca476b4489136c35ff8abc895fd02305a3425c0
Hyowook Park <hyowook_park@tmax.co.kr>
2022-07-21 15:55:16 +0900

    [feat] create-helmrepo refact

--- frontend/public/components/hypercloud/utils/text-input.tsx
+++ frontend/public/components/hypercloud/utils/text-input.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import { useFormContext } from 'react-hook-form';
 
-export const TextInput: React.FC<TextInputProps> = ({ id, methods, style, inputClassName, defaultValue, placeholder, type, isDisabled = false, valid = true, validation = {} }) => {
+export const TextInput: React.FC<TextInputProps> = ({ id, methods, style, inputClassName, defaultValue, placeholder, type, isDisabled = false, valid = true, validation = {}, hidden= false }) => {
   const { register } = methods ? methods : useFormContext();
-  return <input className={classNames(inputClassName, { ['error-text']: !valid })} style={style} placeholder={placeholder} defaultValue={defaultValue} name={id} ref={register(validation)} disabled={isDisabled} type={type}></input>;
+  return <input className={classNames(inputClassName, { ['error-text']: !valid })} style={style} placeholder={placeholder} defaultValue={defaultValue} name={id} ref={register(validation)} disabled={isDisabled} type={type} hidden={hidden}></input>;
 };
 
 type TextInputProps = {
