32390bea8a775a98935ee36760f122a332dba9ed
jinsoo_youn <jinsoo_youn@tmax.co.kr>
2022-09-22 00:57:43 +0000

    Added changelog and update history for 5.2.0.0(oauth2-proxy)

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 set -e 
-DOCKER_REGISTRY="jinsnow"
-DOCKER_IMAGE="tmax-cloud/hypercloud-console"
+DOCKER_REGISTRY="docker.io"
+DOCKER_IMAGE="tmaxcloudck/hypercloud-console"
 MAJOR_VERSION="5"
-MINOR_VERSION="1"
+MINOR_VERSION="2"
 PATCH_VERSION="0"
 HOTFIX_VERSION="0"
 
 #build docker image 
 docker build --rm=true -t ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${HOTFIX_VERSION} -f ./Dockerfile .
 
-# docker push ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${HOTFIX_VERSION}
+docker push ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${HOTFIX_VERSION}
