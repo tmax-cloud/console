3fe5b65ac20cf74e0a652ab0a01cfec9fa7845e8
yunhee_shin <qlyh8@naver.com>
2022-10-25 13:27:39 +0900

    [feat] 메모 수정 아이콘 추가

--- frontend/public/components/hypercloud/utils/memo.tsx
+++ frontend/public/components/hypercloud/utils/memo.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { PlusCircleIcon, EditIcon } from '@patternfly/react-icons';
+import { PlusCircleIcon } from '@patternfly/react-icons';
 import { K8sKind, K8sResourceKind } from '@console/internal/module/k8s';
+import * as MemoIcon from '../../../imgs/hypercloud/memo.svg';
 import { memoModal } from '../modals/memo-modal';
 
 const Memo = (props: MemoProps) => {
   const { model, resource } = props;
   const memo = resource.metadata?.annotations?.memo;
   const handleClick = () => memoModal({ isCreate: !memo, memo, model, resource });
-  return <div className="co-memo">{memo ? <EditIcon className="co-memo-edit-icon" onClick={handleClick} /> : <PlusCircleIcon className="co-memo-new-icon" onClick={handleClick} />}</div>;
+  return <div className="co-memo">{memo ? <img className="co-memo-edit-icon" onClick={handleClick} src={MemoIcon} /> : <PlusCircleIcon className="co-memo-new-icon" onClick={handleClick} />}</div>;
 };
 
 export const memoColumnClass = 'col-lg-1 co-text-center';
