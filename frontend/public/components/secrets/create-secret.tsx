9100fde3e07907f17dd9a34a87f7a7477003ffb3
inth9198 <inth2474@naver.com>
2022-11-16 12:55:15 +0900

    [feat][IMS] 290316 시크릿 키/값 상세 보기시 데이터값 표시에 토큰값을 64 인코딩하여 적용 파일 수정 실수

--- frontend/public/components/secrets/create-secret.tsx
+++ frontend/public/components/secrets/create-secret.tsx
@@ -976,7 +976,6 @@ const KeyValueEntryForm = withTranslation()(
           <div className="form-group">
             <div>
               <DroppableFileInput onChange={this.onValueChange} inputFileData={this.state.value} id={`${this.props.id}-value`} textareaFieldHelpText={t('SINGLE:MSG_SECRETS_CREATEFORM_DIV2_1')} label={t('SINGLE:MSG_SECRETS_CREATEKEYVALUESECRET_DIV2_4')} inputFieldHelpText={t('SINGLE:MSG_SECRETS_CREATEKEYVALUESECRET_DIV2_6')} />
-              <p className="co-m-pane__explanation">{t('SINGLE:MSG_SECRETS_CREATEFORM_DIV2_1')}</p>
             </div>
           </div>
         </div>
