e17e168f14c8afe4e6939f4aeecaafeb601b4379
inth9198 <inth2474@naver.com>
2022-11-29 16:38:54 +0900

    [feat]개인 개발환경이 잘못들어갔습니다

--- scripts/run-console.sh
+++ scripts/run-console.sh
@@ -3,8 +3,7 @@
 set -exuo pipefail
 
 #myIP=$(hostname -I | awk '{print $1}')
-myIP='192.168.8.106'
-# myIP=$(ipconfig getifaddr en0)
+myIP=$(ipconfig getifaddr en0)
 # myIP=localhost
 ## Default K8S Endpoint is public POC environment
 # k8sIP='220.90.208.100'
