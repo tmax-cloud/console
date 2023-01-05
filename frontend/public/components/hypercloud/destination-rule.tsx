a15dc75667c091fb6ed161c039057a2487fd17f6
Hyowook Park <hyowook_park@tmax.co.kr>
2023-01-05 13:43:10 +0900

    [bugfix] 서비스 메시  namespace 리소스 링크 수정

--- frontend/public/components/hypercloud/destination-rule.tsx
+++ frontend/public/components/hypercloud/destination-rule.tsx
@@ -44,7 +44,7 @@ const tableProps: TableProps = {
       children: <ResourceLink kind={kind} name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.uid} />,
     },
     {
-      children: <Status status={obj.metadata.namespace} />,
+      children: <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} />,
     },
     {
       className: classNames('pf-m-hidden', 'pf-m-visible-on-sm', 'co-break-word'),
