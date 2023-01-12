99107df89a07fe8e419356ace529b5c6fc7344e7
Hyowook Park <hyowook_park@tmax.co.kr>
2023-01-12 10:59:06 +0900

    [refact] 삼항연산자를 if 문으로 수정

--- frontend/public/components/edit-yaml.jsx
+++ frontend/public/components/edit-yaml.jsx
@@ -182,14 +182,19 @@ export const EditYAML_ = connect(stateToProps)(
       const isCustomResourceType = !isResourceSchemaBasedMenu(model.kind);
       const url = getResourceSchemaUrl(model, isCustomResourceType);
       url &&
-        coFetchJSON(url).then(template => {          
-          const currentDefinition = isCustomResourceType
-            ? template?.spec?.validation?.openAPIV3Schema
-              ? template?.spec?.validation?.openAPIV3Schema
-              : template?.spec?.versions.filter(version => {
-                  if (version.name === model.apiVersion) return true;
-                })[0]?.schema?.openAPIV3Schema
-            : JSON.parse(template);
+        coFetchJSON(url).then(template => {
+          let currentDefinition;
+          if (isCustomResourceType) {
+            if (template?.spec?.validation?.openAPIV3Schema) {
+              currentDefinition = template?.spec?.validation?.openAPIV3Schema;
+            } else {
+              currentDefinition = template?.spec?.versions.filter(version => {
+                if (version.name === model.apiVersion) return true;
+              })[0]?.schema?.openAPIV3Schema;
+            }
+          } else {
+            currentDefinition = JSON.parse(template);
+          }
           const currentProperties = _.get(currentDefinition, 'properties') || _.get(currentDefinition, 'items.properties');
           this.setState({ definition: currentProperties ? currentDefinition : null });
         });
