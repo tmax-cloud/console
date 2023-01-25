5e8f7b5fede582ca57c1ce0cd5fbc883f96cc67a
inth9198 <inth2474@naver.com>
2023-01-25 09:31:31 +0900

    [feat][patch] 테스크런 생성페이지 리팩토링

--- frontend/public/components/hypercloud/form/pipelineruns/sync-form-data.ts
+++ frontend/public/components/hypercloud/form/pipelineruns/sync-form-data.ts
@@ -18,19 +18,24 @@ export const onSubmitCallback = data => {
   _.forEach(data.spec.resources, (obj, name) => {
     resources.push({ name: name, resourceRef: obj.resourceRef });
   });
-
   _.forEach(data.spec.workspaces, workspace => {
     if (workspace && workspace.type) {
-      if (workspace.type == 'EmptyDirectory') {
-        workspace.emptyDir = {};
-      } else if (workspace.type == 'VolumeClaimTemplate') {
-        workspace.volumeClaimTemplate.spec.accessModes = [workspace.volumeClaimTemplate.spec.accessModes[0]];
-      } else if (workspace.type == 'Secret') {
-        workspace.secret.secretName = workspace?.secret?.secretName;
-      } else if (workspace.type == 'ConfigMap') {
-        workspace.configmap.name = workspace?.configmap?.name;
-      } else if (workspace.type == 'PVC') {
-        workspace.persistentVolumeClaim.claimName = workspace?.persistentVolumeClaim?.claimName;
+      switch (workspace.type) {
+        case 'EmptyDirectory':
+          workspace.emptyDir = {};
+          break;
+        case 'VolumeClaimTemplate':
+          workspace.volumeClaimTemplate.spec.accessModes = [workspace.volumeClaimTemplate.spec.accessModes[0]];
+          break;
+        case 'Secret':
+          workspace.secret.secretName = workspace?.secret?.secretName;
+          break;
+        case 'ConfigMap':
+          workspace.configmap.name = workspace?.configmap?.name;
+          break;
+        case 'PVC':
+          workspace.persistentVolumeClaim.claimName = workspace?.persistentVolumeClaim?.claimName;
+          break;
       }
       delete workspace.type;
     }
@@ -53,7 +58,6 @@ export const convertToForm = (data: any) => {
     return data;
   }
   const _data = _.cloneDeep(data);
-
   const labels = Object.entries(_.get(_data, 'metadata.labels', {}))?.map(([key, value]) => `${key}=${value}`);
 
   let params = {};
@@ -72,21 +76,26 @@ export const convertToForm = (data: any) => {
       let newWorkspace = { name: workspace.name };
       const type = 'secret' in workspace ? 'Secret' : 'configmap' in workspace ? 'ConfigMap' : 'emptyDir' in workspace ? 'EmptyDirectory' : 'persistentVolumeClaim' in workspace ? 'PVC' : 'volumeClaimTemplate' in workspace ? 'VolumeClaimTemplate' : '';
       newWorkspace = _.merge(newWorkspace, { type: type });
-      if (type === 'EmptyDirectory') {
-        newWorkspace = _.merge(newWorkspace, { emptyDir: {} });
-      } else if (type === 'VolumeClaimTemplate') {
-        const accessMode = workspace?.volumeClaimTemplate?.spec?.accessModes[0];
-        const storage = workspace?.volumeClaimTemplate?.spec?.resources?.requests?.storage;
-        const storageClassName = workspace?.volumeClaimTemplate?.spec?.storageClassName;
-        newWorkspace = _.merge(newWorkspace, { volumeClaimTemplate: { spec: { accessModes: [accessMode], resources: { requests: { storage: storage } }, storageClassName: storageClassName } } });
-      } else if (type === 'Secret') {
-        newWorkspace = _.merge(newWorkspace, { secret: { secretName: workspace?.secret?.secretName } });
-      } else if (type === 'ConfigMap') {
-        newWorkspace = _.merge(newWorkspace, { configmap: { name: workspace?.configmap?.name } });
-      } else if (type === 'PVC') {
-        newWorkspace = _.merge(newWorkspace, { persistentVolumeClaim: { claimName: workspace?.persistentVolumeClaim?.claimName } });
+      switch (type) {
+        case 'EmptyDirectory':
+          newWorkspace = _.merge(newWorkspace, { emptyDir: {} });
+          break;
+        case 'VolumeClaimTemplate':
+          const accessMode = workspace?.volumeClaimTemplate?.spec?.accessModes[0];
+          const storage = workspace?.volumeClaimTemplate?.spec?.resources?.requests?.storage;
+          const storageClassName = workspace?.volumeClaimTemplate?.spec?.storageClassName;
+          newWorkspace = _.merge(newWorkspace, { volumeClaimTemplate: { spec: { accessModes: [accessMode], resources: { requests: { storage: storage } }, storageClassName: storageClassName } } });
+          break;
+        case 'Secret':
+          newWorkspace = _.merge(newWorkspace, { secret: { secretName: workspace?.secret?.secretName } });
+          break;
+        case 'ConfigMap':
+          newWorkspace = _.merge(newWorkspace, { configmap: { name: workspace?.configmap?.name } });
+          break;
+        case 'PVC':
+          newWorkspace = _.merge(newWorkspace, { persistentVolumeClaim: { claimName: workspace?.persistentVolumeClaim?.claimName } });
+          break;
       }
-
       workspaces.push(newWorkspace);
     }
   });
