2d253a2c85f459ecdcce148743b00cead96dc50f
eunkyeng_shin@tmax.co.kr <eunkyeng_shin@tmax.co.kr>
2023-02-06 20:40:04 +0900

    [feat] expandable table에서 sort와 tooltip가능하도록 기능 확장

--- frontend/public/components/hypercloud/utils/expandable-table.tsx
+++ frontend/public/components/hypercloud/utils/expandable-table.tsx
@@ -1,27 +1,43 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
 import * as _ from 'lodash-es';
-import { Table as PfTable, TableHeader as PfTableHeader, TableBody as PfTableBody, ICell } from '@patternfly/react-table';
+import { Table as PfTable, TableHeader as PfTableHeader, TableBody as PfTableBody, ICell, sortable } from '@patternfly/react-table';
+import { TFunction } from 'i18next';
+import { sortableHelp } from './sortTip';
+import { useTranslation } from 'react-i18next';
+
+const makeTableHeader = (header: Header[], t: TFunction) => {
+  const _header: Header[] = header.map(value => ({
+    ...value,
+    title: t(value.title),
+    transforms: _.isUndefined(value.tooltip) ? (_.isUndefined(value.transforms) ? [sortable] : value.transforms) : [sortableHelp({ tooltip: t(value.tooltip) })],
+    props: value.props,
+  }));
+  return () => {
+    return _header;
+  };
+};
 
 export const SingleExpandableTable: React.FC<SingleExpandableTableProps> = ({ header, itemList, rowRenderer, innerRenderer, compoundParent }) => {
-  const [tableRows, setTableRows] = useState([]);
-  useEffect(() => {
+  const [tableRows, setTableRows] = React.useState([]);
+  const { t } = useTranslation();
+  const headerFunc = makeTableHeader(header, t);
+  React.useEffect(() => {
     const preData = [];
     itemList
       .reduce((result, item, index: number) => {
         return result.then(async () => {
           const innerTable = await innerRenderer(item);
-          if (!!innerTable) {
+          if (innerTable) {
             preData.push({
               isOpen: false,
               cells: rowRenderer(index, item, innerTable.props?.data?.length),
             });
 
             if (innerTable.props?.data?.length > 0) {
-              let parentValue = index * 2;
+              const parentValue = index * 2;
               preData.push({
                 parent: parentValue,
-                compoundParent: compoundParent,
+                compoundParent,
                 cells: [
                   {
                     title: innerTable,
@@ -30,10 +46,10 @@ export const SingleExpandableTable: React.FC<SingleExpandableTableProps> = ({ he
                 ],
               });
             } else {
-              let parentValue = index * 2;
+              const parentValue = index * 2;
               preData.push({
                 parent: parentValue,
-                compoundParent: compoundParent,
+                compoundParent,
                 cells: [
                   {
                     title: <div>...No Data...</div>,
@@ -48,17 +64,19 @@ export const SingleExpandableTable: React.FC<SingleExpandableTableProps> = ({ he
       .then(() => {
         setTableRows(_.cloneDeep(preData));
       });
-  }, [itemList]);
+  }, [compoundParent, header.length, innerRenderer, itemList, rowRenderer]);
 
   // function delay() {
   //   return new Promise(resolve => setTimeout(resolve, 300));
   // }
 
-  const onExpand = (event, rowIndex, colIndex, isOpen, rowData, extraData) => {
-    let rows = _.cloneDeep(tableRows);
+  const onExpand = (rowIndex, colIndex, isOpen) => {
+    const rows = _.cloneDeep(tableRows);
     if (!isOpen) {
       rows[rowIndex].cells.forEach((cell: ICell) => {
-        if (cell.props) cell.props.isOpen = false;
+        if (cell.props) {
+          cell.props.isOpen = false;
+        }
       });
       (rows[rowIndex].cells[colIndex] as ICell).props.isOpen = true;
       rows[rowIndex].isOpen = true;
@@ -70,17 +88,27 @@ export const SingleExpandableTable: React.FC<SingleExpandableTableProps> = ({ he
   };
 
   return (
-    <PfTable aria-label="Compound expandable table" onExpand={onExpand} rows={tableRows} cells={header}>
+    <PfTable aria-label="Compound expandable table" onExpand={onExpand} rows={tableRows} cells={headerFunc()}>
       <PfTableHeader />
       <PfTableBody />
     </PfTable>
   );
 };
 
+type Header = {
+  title: string;
+  sortField?: string;
+  sortFunc?: string;
+  transforms?: any;
+  props?: { className: string };
+  tooltip?: string;
+};
+
 type SingleExpandableTableProps = {
   itemList: any[]; // outer table의 itemList
   rowRenderer: (index, obj, itemCount: number) => any[]; // outer table의 row 한줄에 들어갈 요소들을 배열 형태로 return하는 renderer 함수
   innerRenderer: (parentItem) => any; // inner table을 render하는 함수(ExpandableInnerTable 컴포넌트 사용해야됨)
-  header: (ICell | string)[]; // header column들의 배열. 펼침 기능을 사용할 column object에는 cellTransforms: [compoundExpand] 속성 넣어줘야 함.
+  header: Header[]; // header column들의 배열. 펼침 기능을 사용할 column object에는 cellTransforms: [compoundExpand] 속성 넣어줘야 함.
   compoundParent: number; // table 펼칠 수 있는 column의 index
+  customSorts?: { [key: string]: any };
 };
