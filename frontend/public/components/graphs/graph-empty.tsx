0eeb838d828131b59cee53e586912e572f3dd1cd
hyeongmin-park1 <lg3319@gmail.com>
2023-03-10 14:13:20 +0900

    [refactor] 사용하지 않는 컴포넌트 삭제

--- frontend/public/components/graphs/graph-empty.tsx
+++ frontend/public/components/graphs/graph-empty.tsx
@@ -23,23 +23,6 @@ export const GraphEmpty: React.FC<GraphEmptyProps> = ({ height = 180, loading = 
   );
 };
 
-export const FsGraphEmpty: React.FC<GraphEmptyProps> = ({ height = 180, loading = false }) => {
-  const { t } = useTranslation();
-  return (
-    <div
-      style={{
-        alignItems: 'center',
-        display: 'flex',
-        height,
-        justifyContent: 'center',
-        padding: '5px',
-        width: '100%',
-      }}
-    >
-      {loading ? <div className="skeleton-chart" /> : <div className="text-secondary">{t('COMMON:MSG_COMMON_ERROR_MESSAGE_28')}</div>}
-    </div>
-  );
-};
 type GraphEmptyProps = {
   height?: number | string;
   loading?: boolean;
