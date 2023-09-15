e7b2f5965dc5be3a13279dfbb5531bbd9b53bdcc
Hyowook Park <hyowook_park@tmax.co.kr>
2023-09-15 16:36:09 +0900

    [bugfix][patch][IMS 309017] 콘솔 ci/cd 메뉴 관련 ui 수정 요청

--- frontend/public/components/hypercloud/form/taskruns/sync-form-data.ts
+++ frontend/public/components/hypercloud/form/taskruns/sync-form-data.ts
@@ -1,6 +1,5 @@
 import * as _ from 'lodash-es';
 import { TaskRunModel } from '@console/internal/models/hypercloud';
-import { workspaceTypeKeys } from '../utils/workspaces';
 
 const changeTimeoutFormat = timeout => {
   timeout = Number(timeout);
@@ -87,16 +86,26 @@ export const onSubmitCallback = data => {
 
   // MEMO : workspaces data
   _.forEach(data.spec.workspaces, workspace => {
-    _.forEach(workspace.name, (type, name) => {
-      workspace.name = name;
-      if (type === 'EmptyDirectory') {
-        workspace.emptyDir = {};
-      } else if (type === 'VolumeClaimTemplate') {
-        if (workspace?.volumeClaimTemplate?.spec?.accessModes) {
-          workspace.volumeClaimTemplate.spec.accessModes = [workspace.volumeClaimTemplate.spec.accessModes];
-        }
+    if (workspace && workspace.type) {
+      switch (workspace.type) {
+        case 'EmptyDirectory':
+          workspace.emptyDir = {};
+          break;
+        case 'VolumeClaimTemplate':
+          workspace.volumeClaimTemplate.spec.accessModes = [workspace.volumeClaimTemplate.spec.accessModes[0]];
+          break;
+        case 'Secret':
+          workspace.secret.secretName = workspace?.secret?.secretName;
+          break;
+        case 'ConfigMap':
+          workspace.configmap.name = workspace?.configmap?.name;
+          break;
+        case 'PVC':
+          workspace.persistentVolumeClaim.claimName = workspace?.persistentVolumeClaim?.claimName;
+          break;
       }
-    });
+      delete workspace.type;
+    }
   });
 
   data = _.defaultsDeep({ kind: TaskRunModel.kind, spec: { taskRef, params: formattedParams, timeout: formattedTimeout, resources: { inputs: formattedInputs, outputs: formattedOutputs } } }, data);
@@ -136,15 +145,34 @@ export const convertToForm = (data: any) => {
   });
 
   // MEMO : workspaces data
-  const workspaces = _.get(_data, 'spec.workspaces', [])?.map(workspace => {
-    const type = Object.keys(workspaceTypeKeys).find(key => !!_.get(workspace, key));
-    const _workspace = _.merge({ [workspace.name]: workspaceTypeKeys[type] }, { [type]: _.cloneDeep(workspace[type]) });
-    if (workspaceTypeKeys[type] === 'EmptyDirectory') {
-      delete _workspace.emptyDir;
-    } else if (workspaceTypeKeys[type] === 'VolumeClaimTemplate') {
-      _workspace[type].spec.accessModes = workspace[type].spec.accessModes?.[0] || '';
+  const workspaces = [];
+  _.forEach(_data.spec?.workspaces, workspace => {
+    if (workspace) {
+      let newWorkspace = { name: workspace.name };
+      const type = 'secret' in workspace ? 'Secret' : 'configmap' in workspace ? 'ConfigMap' : 'emptyDir' in workspace ? 'EmptyDirectory' : 'persistentVolumeClaim' in workspace ? 'PVC' : 'volumeClaimTemplate' in workspace ? 'VolumeClaimTemplate' : '';
+      newWorkspace = _.merge(newWorkspace, { type: type });
+      switch (type) {
+        case 'EmptyDirectory':
+          newWorkspace = _.merge(newWorkspace, { emptyDir: {} });
+          break;
+        case 'VolumeClaimTemplate':
+          const accessMode = workspace?.volumeClaimTemplate?.spec?.accessModes[0];
+          const storage = workspace?.volumeClaimTemplate?.spec?.resources?.requests?.storage;
+          const storageClassName = workspace?.volumeClaimTemplate?.spec?.storageClassName;
+          newWorkspace = _.merge(newWorkspace, { volumeClaimTemplate: { spec: { accessModes: [accessMode], resources: { requests: { storage: storage } }, storageClassName: storageClassName } } });
+          break;
+        case 'Secret':
+          newWorkspace = _.merge(newWorkspace, { secret: { secretName: workspace?.secret?.secretName } });
+          break;
+        case 'ConfigMap':
+          newWorkspace = _.merge(newWorkspace, { configmap: { name: workspace?.configmap?.name } });
+          break;
+        case 'PVC':
+          newWorkspace = _.merge(newWorkspace, { persistentVolumeClaim: { claimName: workspace?.persistentVolumeClaim?.claimName } });
+          break;
+      }
+      workspaces.push(newWorkspace);
     }
-    return _workspace;
   });
 
   const omittedData = _.omit(_data, ['spec.taskRef', 'spec.timeout', 'spec.resources.inputs', 'spec.resources.outputs', 'spec.params', 'spec.workspaces']);
